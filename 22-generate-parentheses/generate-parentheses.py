51c3e4c3626a3b049aa988e8428c409de2a2db98
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-04-20 23:08:18 -0700

    Time: 37 ms (60.76%) | Memory: 16.8 MB (42.46%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.py
+++ 22-generate-parentheses/generate-parentheses.py
@@ -1,26 +1,16 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        # dfs backtracking
         ans, temp = [], []
-        def dfs(open_p):
-            if len(temp) == n * 2:
-                if open_p == 0:
-                    ans.append(''.join(temp))
+        def dfs(op, cl):
+            if cl < op or op < 0 or cl < 0: return
+            if op == cl == 0:
+                ans.append(''.join(temp))
                 return
-            if open_p < 0 or len(temp) > n * 2: return
             temp.append('(')
-            dfs(open_p + 1)
+            dfs(op - 1, cl)
             temp.pop()
             temp.append(')')
-            dfs(open_p - 1)
+            dfs(op, cl - 1)
             temp.pop()
-        dfs(0)
+        dfs(n, n)
         return ans
-"""
-n1    / \
-     o1 o-1
-   /\   
-o2( o0) 
-  /\     /\
-o3( o1) o1 o-1
-"""
