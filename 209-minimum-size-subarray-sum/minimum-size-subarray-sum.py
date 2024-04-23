b18b46d2856664e5fae837e531bf8ee2b39f88a6
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-04-23 04:37:51 -0700

    Time: 225 ms (6.54%) | Memory: 30.3 MB (36.63%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.py
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.py
@@ -1,23 +1,21 @@
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
-        # 1) sliding window variable
-            # left + right pointers and keep track window sum == target
-            # time: O(n)
-            # space: O(1)
-        N = len(nums)
-        left = 0
-        window_sum = 0
-        min_window_length = float('inf')
-        for right in range(N):
-            window_sum += nums[right]
-            while window_sum >= target and left <= right:
-                min_window_length = min(min_window_length, right - left + 1)
-                window_sum -= nums[left]
-                left += 1
-        return min_window_length if min_window_length != float('inf') else 0
         # 2) prefix sum + binary search weight left
             # time: O(n log n)
             # space: O(n)
-        # 3) prefix sum + hashmap
-            # time: O(n)
-            # space: O(n)
+        N = len(nums)
+        prefix_sums = []
+        curr_sum = 0
+        min_window_length = float('inf')
+        for i in range(N):
+            curr_sum += nums[i]
+            if curr_sum >= target:
+                min_window_length = min(min_window_length, i + 1)
+            prefix_sums.append(curr_sum)
+
+        for i in range(N):
+            to_find = prefix_sums[i] + target
+            idx = bisect.bisect_left(prefix_sums, to_find)
+            if idx == N: continue
+            min_window_length = min(min_window_length, idx - i)
+        return 0 if min_window_length == float('inf') else min_window_length
