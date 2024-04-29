38d1b5b777ab36f88420628cdfcfb64e7815ed9e
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-04-29 02:32:38 -0700

    Time: 53 ms (29.93%) | Memory: 16.6 MB (96.80%) - LeetSync

--- 223-rectangle-area/rectangle-area.py
+++ 223-rectangle-area/rectangle-area.py
@@ -7,10 +7,11 @@ class Solution:
         x2 = min(ax2, bx2)
         y1 = max(ay1, by1)
         y2 = min(ay2, by2)
-        is_overlap = x2 - x1 >= 0 and y2 - y1 >= 0
-        intersection = 0
+        is_overlap = x2 - x1 > 0 and y2 - y1 > 0
         if is_overlap:
             intersection = self.cal_area(x1, x2, y1, y2)
+        else:
+            intersection = 0
         a_area = self.cal_area(ax1, ax2, ay1, ay2)
         b_area = self.cal_area(bx1, bx2, by1, by2)
         return a_area + b_area - intersection
