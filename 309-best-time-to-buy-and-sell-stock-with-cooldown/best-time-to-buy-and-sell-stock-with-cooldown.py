149de44d92b92f34f9bad54d6a78885e26e0bfdf
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-05-02 00:19:08 -0700

    Time: 1065 ms (6.61%) | Memory: 16.7 MB (96.95%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -1,18 +1,11 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         N = len(prices)
-        memo = {} # (i, is_buying): max_profit
-        def dfs(i, is_buying):
-            # base case: if i reaches end of N: return 0
-            if i >= N: return 0
-            # if params in memo: return memo[params]
-            if (i, is_buying) in memo: return memo[i, is_buying]
-            if is_buying:
-                # next level dfs - prices[i] or cooldown
-                res = max(dfs(i + 1, False) - prices[i], dfs(i + 1, is_buying))
-            else:
-                # sell or cooldown
-                res = max(dfs(i + 2, True) + prices[i], dfs(i + 1, is_buying))
-            memo[i, is_buying] = res
-            return res
-        return dfs(0, True)
+        dp = [0] * (N + 2)
+        for i in range(N - 1, -1, -1):
+            buy_sell = 0
+            for j in range(i + 1, N):
+                buy_sell = max(buy_sell, dp[j + 2] + prices[j] - prices[i])
+            skip = dp[i + 1]
+            dp[i] = max(buy_sell, skip)
+        return dp[0]
