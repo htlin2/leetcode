3050fca3a439ad044e5c49a1ef4052700e69ecc5
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-05-08 22:58:42 -0700

    Time: 34 ms (99.94%) | Memory: 17.8 MB (16.21%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,8 +1,23 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        h = {}
-        for i in range(len(nums)):
-            delta = target - nums[i]
+        h = {} # {num: index}
+        for i, n in enumerate(nums):
+            delta = target - n
             if delta in h:
                 return [h[delta], i]
-            h[nums[i]] = i
+            h[n] = i
+"""
+1) brute force
+Time: O(n^2)
+Space: O(1)
+
+2) hashmap = {num: index}
+Time: O(n)
+Space: O(n)
+
+3) two pointers + sort
+(num, index)
+[(2,1), (3,0), (4, 2)]
+Time: O(n log n)
+Space: O(n)
+"""
