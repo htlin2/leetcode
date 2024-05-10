d241bab39eec946cdc4e5df735b463429e3a560c
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-05-09 21:06:50 -0700

    Time: 32 ms (98.18%) | Memory: 17 MB (63.27%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,10 +1,14 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        new_s = ''
-        for char in s:
-            if char.isalnum():
-                new_s += char.lower()
-        return new_s == new_s[::-1]
+        L, R = 0, len(s) - 1
+        while L < R:
+            while L < R and not (s[L].isalpha() or s[L].isdigit()):
+                L += 1
+            while L < R and not s[R].isalnum():
+                R -= 1
+            if s[L].lower() != s[R].lower(): return False
+            L, R = L + 1, R - 1
+        return True
 
 """
 1) brute force
