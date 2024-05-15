c777ca54ef4830d12ac65a64a1a89d33e2cfeed7
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-05-14 19:00:28 -0700

    Time: 403 ms (89.62%) | Memory: 18.8 MB (97.44%) - LeetSync

---
+++ 542-01-matrix/01-matrix.py
@@ -0,0 +1,20 @@
+class Solution:
+    def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+        directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+        ROWS, COLS = len(mat), len(mat[0])
+        q = collections.deque([]) # [r, c]
+        for r in range(ROWS):
+            for c in range(COLS):
+                if mat[r][c] == 0:
+                    q.append([r, c])
+                else:
+                    mat[r][c] = '#'
+        while q:
+            r, c = q.popleft()
+            for dr, dc in directions:
+                rr, cc = r + dr, c + dc
+                if rr < 0 or cc < 0 or rr >= ROWS or cc >= COLS: continue
+                if mat[rr][cc] != '#': continue
+                mat[rr][cc] = mat[r][c] + 1
+                q.append([rr, cc])
+        return mat
