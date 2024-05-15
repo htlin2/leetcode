6963de0a0d1c8853813d1369cb44892208282658
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-05-14 21:38:11 -0700

    Time: 410 ms (86.29%) | Memory: 18.9 MB (94.14%) - LeetSync

--- 542-01-matrix/01-matrix.py
+++ 542-01-matrix/01-matrix.py
@@ -1,8 +1,8 @@
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
-        directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+        DIR = [[0, 1], [0, -1], [-1, 0], [1, 0]]
         ROWS, COLS = len(mat), len(mat[0])
-        q = collections.deque([]) # [r, c]
+        q = collections.deque([])
         for r in range(ROWS):
             for c in range(COLS):
                 if mat[r][c] == 0:
@@ -11,10 +11,15 @@ class Solution:
                     mat[r][c] = '#'
         while q:
             r, c = q.popleft()
-            for dr, dc in directions:
+            for dr, dc in DIR:
                 rr, cc = r + dr, c + dc
                 if rr < 0 or cc < 0 or rr >= ROWS or cc >= COLS: continue
                 if mat[rr][cc] != '#': continue
                 mat[rr][cc] = mat[r][c] + 1
                 q.append([rr, cc])
         return mat
+"""
+1. BFS
+
+2. dp
+"""
