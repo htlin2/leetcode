0657056802f1f2472eb871009aa5a7814b269229
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-05-20 00:30:31 -0700

    Time: 44 ms (29.19%) | Memory: 17.1 MB (9.36%) - LeetSync

--- 133-clone-graph/clone-graph.py
+++ 133-clone-graph/clone-graph.py
@@ -11,39 +11,13 @@ class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
         if not node: return node
         hashmap = {}
-        def dfs(node):
-            if node in hashmap: return hashmap[node]
-            hashmap[node] = Node(node.val)
-            for nei in node.neighbors:
-                cloned_node = dfs(nei)
-                hashmap[node].neighbors.append(cloned_node)
-            return hashmap[node]
-        dfs(node)
+        hashmap[node] = Node(node.val)
+        q = collections.deque([node])
+        while q:
+            first = q.popleft()
+            for nei in first.neighbors:
+                if nei not in hashmap:
+                    hashmap[nei] = Node(nei.val)
+                    q.append(nei)
+                hashmap[first].neighbors.append(hashmap[nei])
         return hashmap[node]
-"""
-linkedlist + hashmap
-1. bfs - iteratively
-hashmap = {node: copy_node}
-q = deque([node])
-while q:
-    first = q.pop()
-    if first == node: break
-    cloned_node = Node(first)
-    if first not in hashmap:
-        hashmap[first] = cloned_node
-    else:
-        cloned_node = hashmap[first]
-    iterate through first's neighbors
-        cloned_nei_node = Node(nei)
-        if neighbor not in hashmap:
-            hashmap[nei] = cloned_nei_node
-        else;
-            cloned_nei_node = hashmap[nei]
-        cloned_node.neighbors.append(cloned_nei_node)
-return hashmap[node]
-
-2. dfs- recurrsion
-    def dfs(node):
-        base case: if node in hashmap: return node
-    
-"""
