72520e8654c739032efc557294d3ce017e3edd30
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-05-24 21:18:09 -0700

    Time: 41 ms (13.34%) | Memory: 16.5 MB (83.73%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,19 +1,23 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        prev, curr = 0, 1
-        for i in range(n):
-            temp = curr
-            curr = curr + prev
-            prev = temp
-        return curr
-
+        dp = collections.defaultdict(int)
+        ans = 0
+        def dfs(n):
+            nonlocal ans
+            if n <= 2: return n
+            if n in dp: return dp[n]
+            ans += dfs(n - 1) + dfs(n - 2)
+            dp[n] = ans
+            return ans
+        return dfs(n)
 """
 n = 2
 0 1 2
 1 1 2   
 
 n = 3
-0 1 2 3
-1 1 2 3
+-1  0  1  2  3
+ 0  1  1  2  3
+ p  p  p  c
 
 """
