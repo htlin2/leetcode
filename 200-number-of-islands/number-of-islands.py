464cd636c90a85842d440ee645138f003d0dbbd8
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-05-28 20:51:56 -0700

    Time: 304 ms (13.16%) | Memory: 22.8 MB (35.57%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,27 +1,48 @@
+class UnionFind:
+    def __init__(self, N):
+        self.parents = [i for i in range(N)]
+        self.ranks = [1] * N
+
+    def find(self, n):
+        if n == self.parents[n]: return n
+        self.parents[n] = self.find(self.parents[self.parents[n]])
+        return self.parents[n]
+
+    def union(self, n1, n2):
+        p1, p2 = self.find(n1), self.find(n2)
+        if p1 == p2: return False
+        if self.ranks[p1] >= self.ranks[p2]:
+            self.ranks[p1] += self.ranks[p2]
+            self.parents[p2] = self.parents[p1]
+        else:
+            self.ranks[p2] += self.ranks[p1]
+            self.parents[p1] = self.parents[p2]
+        return True
+
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
         ROWS, COLS = len(grid), len(grid[0])
         visited = set()
-        count = 0
-        def bfs(r, c):
-            q = collections.deque([[r, c]])
-            while q:
-                qr, qc = q.popleft()
-                for dr, dc in directions:
-                    rr, cc = qr + dr, qc + dc
-                    if rr < 0 or cc < 0 or rr >= ROWS or cc >= COLS: continue
-                    if grid[rr][cc] == '0' or (rr, cc) in visited: continue
-                    visited.add((rr, cc))
-                    q.append([rr, cc])
-            return
+        N = ROWS * COLS
+        uf = UnionFind(N)
+        success_count = 0
+        ones_count = 0
         for r in range(ROWS):
             for c in range(COLS):
                 if grid[r][c] == '0' or (r, c) in visited: continue
-                visited.add((r, c))
-                count += 1
-                bfs(r, c)
-        return count
+                ones_count += 1
+                # visited.add((r, c))
+                for dr, dc in directions:
+                    rr, cc = r + dr, c + dc
+                    if rr < 0 or cc < 0 or rr >= ROWS or cc >= COLS: continue
+                    if (rr, cc) in visited or grid[rr][cc] == '0': continue
+                    n1 = r * COLS + c
+                    n2 = rr * COLS + cc
+                    if uf.union(n1, n2):
+                        success_count += 1
+                    # visited.add((rr, cc))
+        return ones_count - success_count
 
 """
 1 DFS
