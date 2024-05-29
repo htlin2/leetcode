d534b43215af006d9e936991bb1dde637b3fe69d
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-05-28 20:05:58 -0700

    Time: 230 ms (78.12%) | Memory: 19.1 MB (40.44%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,40 +1,47 @@
-class UF:
-    def __init__(self, N):
-        self.parents = [i for i in range(N)]
-        self.ranks = [1] * N
-    
-    def find(self, n):
-        if n == self.parents[n]: return n
-        self.parents[n] = self.find(self.parents[self.parents[n]])
-        return self.parents[n]
-
-    def union(self, n1, n2):
-        p1, p2 = self.find(n1), self.find(n2)
-        if p1 == p2: return False
-        if self.ranks[p1] >= self.ranks[p2]:
-            self.ranks[p1] += self.ranks[p2]
-            self.parents[p2] = self.parents[p1]
-        else:
-            self.ranks[p2] += self.ranks[p1]
-            self.parents[p1] = self.parents[p2]
-        return True
-
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        count = 0
         directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
         ROWS, COLS = len(grid), len(grid[0])
-        uf = UF(ROWS * COLS)
+        count = 0
+        def dfs(r, c):
+            if r < 0 or c < 0 or r >= ROWS or c >= COLS: return
+            if grid[r][c] == '0': return
+            grid[r][c] = '0'
+            for dr, dc in directions:
+                dfs(r + dr, c + dc)
+            return
         for r in range(ROWS):
             for c in range(COLS):
                 if grid[r][c] == '0': continue
                 count += 1
-                n1 = r * COLS + c
-                for dr, dc in directions:
-                    rr, cc = dr + r, dc + c
-                    if rr < 0 or cc < 0 or rr >= ROWS or cc >= COLS: continue
-                    if grid[rr][cc] == '0': continue
-                    n2 = rr * COLS + cc
-                    if uf.union(n1, n2):
-                        count -= 1
+                dfs(r, c)
         return count
+
+"""
+1 DFS
+iterate through grid
+run dfs on 1
+    increment count
+    iterate through 4 directions
+    check for out of bounds
+    mark 1 to 0
+    call dfs on 4 directions
+return count
+
+2 BFS
+iterate through grid
+when hitting 1 
+increment count
+run BFS
+    iterate through 4 directions
+    mark 1's to 0's
+return count
+
+3 Union Find
+iterate through grid
+    when hittin 1
+    iterate through 4 directions
+        union with original 1 from grid
+        count how many successful unions
+return R*C - successful unions
+"""
