5482be8118b0702bb5f8e34a54cd4d4c0dc9611e
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-06-04 00:37:59 -0700

    Time: 162 ms (89.21%) | Memory: 17.8 MB (99.33%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,11 +1,18 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        counter = collections.Counter(nums)
-        max_count, max_key = 0, 0
-        for k, v in counter.items():
-            if v > max_count:
-                max_key = k
-                max_count = v
-        return max_key
-        # Time: O(n)
-        # Space: O(n)
+        count, res = 0, 0
+        for n in nums:
+            if count == 0:
+                res = n
+            if n == res:
+                count += 1
+            else:
+                count -= 1
+        return res
+
+"""
+count
+nums = [2,2,1,1,1,2,2]
+res  =  2 2 2 2 1 1 2
+count=  1 2 1 0 1 0 1
+"""
