58a9451f03932f999b2002b63b8e892c51ec3691
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-06-05 02:42:00 -0700

    Time: 75 ms (75.63%) | Memory: 61.1 MB (70.56%) - LeetSync

---
+++ 341-flatten-nested-list-iterator/flatten-nested-list-iterator.js
@@ -0,0 +1,71 @@
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * function NestedInteger() {
+ *
+ *     Return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     @return {boolean}
+ *     this.isInteger = function() {
+ *         ...
+ *     };
+ *
+ *     Return the single integer that this NestedInteger holds, if it holds a single integer
+ *     Return null if this NestedInteger holds a nested list
+ *     @return {integer}
+ *     this.getInteger = function() {
+ *         ...
+ *     };
+ *
+ *     Return the nested list that this NestedInteger holds, if it holds a nested list
+ *     Return null if this NestedInteger holds a single integer
+ *     @return {NestedInteger[]}
+ *     this.getList = function() {
+ *         ...
+ *     };
+ * };
+ */
+/**
+ * @constructor
+ * @param {NestedInteger[]} nestedList
+ */
+var NestedIterator = function(nestedList) {
+    this.stack = []
+    this.dfs(nestedList)
+    this.stack.reverse()
+};
+
+
+/**
+ * @this NestedIterator
+ * @returns {boolean}
+ */
+NestedIterator.prototype.hasNext = function() {
+    return this.stack.length ? true : false
+};
+
+/**
+ * @this NestedIterator
+ * @returns {integer}
+ */
+NestedIterator.prototype.next = function() {
+    const val = this.stack.pop()
+    return val.getInteger()
+};
+
+NestedIterator.prototype.dfs = function(nestedList) {
+    const N = nestedList.length
+    for (let i = 0; i < N; i++) {
+        const num = nestedList[i]
+        if (num.isInteger()) {
+            this.stack.push(num)
+        } else {
+            this.dfs(num.getList())
+        }
+    }
+}
+
+/**
+ * Your NestedIterator will be called like this:
+ * var i = new NestedIterator(nestedList), a = [];
+ * while (i.hasNext()) a.push(i.next());
+*/
