70ee0b74855e91085537f8bef3e21e4486284b5f
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-06-09 08:59:23 -0700

    Time: 40 ms (91.62%) | Memory: 16.8 MB (86.10%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,27 +1,11 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t): return False
-        h = collections.defaultdict(int)
+        hashmap = collections.defaultdict(int)
         for char in s:
-            h[char] += 1
+            hashmap[char] += 1
         for char in t:
-            h[char] -= 1
-            if h[char] < 0: return False
-            if h[char] == 0: del h[char]
-        return len(h) == 0
-"""
-1. brute force
-for loops with s
-pop char in t
-Time: O(n^2)
-Space: O(n)
-
-2. hashmap
-Time: O(n)
-Space: O(n)
-
-3. sort
-Time: O(n log n)
-Space: O(n)
-
-"""
+            if char not in hashmap: return False
+            hashmap[char] -= 1
+            if hashmap[char] < 0: return False
+        return True
