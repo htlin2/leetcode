cca025160e8f1a80f0b706e69f5acd96f49e6611
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-06-10 01:29:37 -0700

    Time: 54 ms (46.10%) | Memory: 49 MB (11.26%) - LeetSync

--- 2733-sleep/sleep.js
+++ 2733-sleep/sleep.js
@@ -3,7 +3,11 @@
  * @return {Promise}
  */
 async function sleep(millis) {
-    return new Promise((resolve, reject) => setTimeout(() => {resolve('')}, millis))
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve()
+        }, millis)
+    })
 }
 
 /** 
