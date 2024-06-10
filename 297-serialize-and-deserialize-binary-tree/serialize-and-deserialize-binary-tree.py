96ded2b8df80c5fc9028fac862c53282c8576900
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-06-10 00:17:42 -0700

    Time: 82 ms (60.47%) | Memory: 20.7 MB (27.15%) - LeetSync

---
+++ 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.py
@@ -0,0 +1,53 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Codec:
+
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+        
+        :type root: TreeNode
+        :rtype: str
+        """
+        if not root: return ''
+        res = []
+        def dfs(node):
+            if not node:
+                res.append('N')
+                return
+            res.append(str(node.val))
+            dfs(node.left)
+            dfs(node.right)
+        dfs(root)
+        return ','.join(res)
+        
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+        
+        :type data: str
+        :rtype: TreeNode
+        """
+        if not data: return []
+        arr = data.split(',')
+        i = -1
+        def dfs():
+            nonlocal i
+            i += 1
+            if arr[i] == 'N': return None
+            node = TreeNode(int(arr[i]))
+            node.left = dfs()
+            node.right = dfs()
+            return node
+        return dfs()
+
+        
+
+# Your Codec object will be instantiated and called as such:
+# ser = Codec()
+# deser = Codec()
+# ans = deser.deserialize(ser.serialize(root))
