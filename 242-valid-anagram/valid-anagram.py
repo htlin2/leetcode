49e90dd89c686d8ba6c95e44fbd1e851b91dd8c0
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-06-09 22:49:57 -0700

    Time: 40 ms (91.61%) | Memory: 16.8 MB (97.62%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,11 +1,5 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t): return False
-        hashmap = collections.defaultdict(int)
-        for char in s:
-            hashmap[char] += 1
-        for char in t:
-            if char not in hashmap: return False
-            hashmap[char] -= 1
-            if hashmap[char] < 0: return False
-        return True
+        s_counter = collections.Counter(s)
+        t_counter = collections.Counter(t)
+        return s_counter == t_counter
