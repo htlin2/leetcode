474ff95eaea33003a27455ac88e0ade281b00e76
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-06-13 10:06:27 -0700

    Time: 67 ms (34.70%) | Memory: 52.3 MB (37.47%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.js
+++ 42-trapping-rain-water/trapping-rain-water.js
@@ -3,35 +3,22 @@
  * @return {number}
  */
 var trap = function(height) {
-    const prefix = []
-    let max_border = 0
-    for (let i = 0; i < height.length; i++) {
-        prefix.push(max_border)
-        max_border = Math.max(height[i], max_border)
-    }
-    const postfix = []
-    max_border = 0
-    for (let i = height.length - 1; i >= 0; i--) {
-        postfix.push(max_border)
-        max_border = Math.max(height[i], max_border)
-    }
-    postfix.reverse()
+    const stack = []
     let res = 0
     for (let i = 0; i < height.length; i++) {
-        const min_border = Math.min(prefix[i], postfix[i])
-        res += Math.max(0, min_border - height[i])
+        while (stack.length && height[stack[stack.length - 1]] < height[i]) {
+            const top = stack.pop()
+            if (!stack.length) break
+            const h = Math.min(height[stack[stack.length - 1]], height[i]) - height[top]
+            const base = i - [stack[stack.length - 1]] - 1
+            res += base * h
+        }
+        stack.push(i)
     }
     return res
 };
 
 /*
-prefix / postfix?
-[0,1,0,2,1,0,1,3,2,1,2,1]
-left > right
-prefix = [0,0,1,1,2,2,2,2,3,3,3,3]
-postfix= [3,3,3,3,3,3,3,2,2,2,1,0]
-min    = [0,0,1,1,2,2,2,2,2,2,1,0]
-min - h  [0,1,0,2,1,0,1,3,2,1,2,1]
-water  = [0,0,1,0,1,2,1,0,0,1,0,0]
-
+monotonic stack decreasing
+height = [0,1,0,2,1,0,1,3,2,1,2,1]
 */
