ac217aa61191f9313ba911457c7b2ad13ca36c01
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-06-26 16:24:14 +0800

    Time: 124 ms (58.93%) | Memory: 62.3 MB (65.01%) - LeetSync

--- 721-accounts-merge/accounts-merge.js
+++ 721-accounts-merge/accounts-merge.js
@@ -2,62 +2,39 @@
  * @param {string[][]} accounts
  * @return {string[][]}
  */
-class UnionFind {
-    constructor(N) {
-        this.parents = []
-        this.ranks = []
-        for (let i = 0; i < N; i++) {
-            this.parents.push(i)
-            this.ranks.push(1)
-        }
-    }
-    find(n) {
-        if (n === this.parents[n]) return n
-        this.parents[n] = this.find(this.parents[this.parents[n]])
-        return this.parents[n]
-    }
-    union(n1, n2) {
-        const p1 = this.find(n1)
-        const p2 = this.find(n2)
-        if (p1 === p2) return false
-        if (this.ranks[p1] >= this.ranks[p2]) {
-            this.ranks[p1] += this.ranks[p2]
-            this.parents[p2] = p1
-        } else {
-            this.ranks[p2] += this.ranks[p1]
-            this.parents[p1] = p2
-        }
-        return true
-    }
-}
 var accountsMerge = function(accounts) {
-    const UF = new UnionFind(accounts.length)
-    const email_index = {}
+    const emailIndex = {} // email: [1, 2, 3]
     accounts.forEach((a, i) => {
-        for (let j = 1; j < a.length; j++) {
-            const email = a[j]
-            if (email in email_index) {
-                UF.union(i, email_index[email])
+        for (const email of a.slice(1)) {
+            if (email in emailIndex) {
+                emailIndex[email].push(i)
             } else {
-                email_index[email] = i
+                emailIndex[email] = [i]
             }
         }
     })
-    const index_emails = {}
-    for (const [email, index] of Object.entries(email_index)) {
-        parent = UF.find(index)
-        if (!index_emails[parent]) {
-            index_emails[parent] = [email]
-            continue
+    const visited = new Set()
+    function dfs(i, emailSet) {
+        if (i in visited) return
+        visited.add(i)
+        const account = accounts[i]
+        for (const email of account.slice(1)) {
+            emailSet.add(email)
+            for (const nei of emailIndex[email]) {
+                if (visited.has(nei)) continue
+                dfs(nei, emailSet)
+            }
         }
-        index_emails[parent].push(email)
     }
+
     const res = []
-    for (const [index, emails] of Object.entries(index_emails)) {
-        const name = accounts[index][0]
-        console.log(emails)
-        emails.sort()
+    accounts.forEach((a, i) => {
+        if (visited.has(i)) return
+        const name = a[0]
+        const emailSet = new Set()
+        dfs(i, emailSet)
+        const emails = [...emailSet].sort()
         res.push([name, ...emails])
-    }
+    })
     return res
 };
