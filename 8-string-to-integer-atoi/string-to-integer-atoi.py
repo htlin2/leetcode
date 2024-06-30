842318b6c5b449e2c030c9377ab0fe032b387103
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-06-30 15:29:35 +0800

    Time: 37 ms (65.76%) | Memory: 16.6 MB (20.45%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.py
+++ 8-string-to-integer-atoi/string-to-integer-atoi.py
@@ -1,25 +1,23 @@
 class Solution:
     def myAtoi(self, s: str) -> int:
-        s = s.lstrip()
-        if not s: return 0
         i = 0
-        is_positive = True
-        if s[i] == '-':
-            is_positive = False
+        while i < len(s) and s[i] == ' ':
             i += 1
-        elif s[i] == '+':
-            is_positive = True
+        if i == len(s): return 0
+        sign = 1
+        if s[i] == '+':
+            i += 1
+        elif s[i] == '-':
+            sign = -1
             i += 1
         ans = 0
         while i < len(s) and s[i].isdigit():
             ans *= 10
             ans += int(s[i])
             i += 1
-        if not is_positive:
-            ans *= -1
-        if -2 ** 31 <= ans <= 2 ** 31 -1:
-            return ans
-        elif ans < -2 ** 31:
+        ans *= sign
+        if ans < -2 ** 31:
             return -2 ** 31
-        else:
+        elif 2 ** 31 - 1 < ans:
             return 2 ** 31 - 1
+        return ans
