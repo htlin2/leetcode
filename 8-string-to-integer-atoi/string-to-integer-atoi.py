705b490187b8edd8b3825ecd590e40ad8b62a616
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-06-30 15:30:33 +0800

    Time: 38 ms (59.45%) | Memory: 16.5 MB (56.25%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.py
+++ 8-string-to-integer-atoi/string-to-integer-atoi.py
@@ -1,21 +1,28 @@
 class Solution:
     def myAtoi(self, s: str) -> int:
+        # strip out leading spaces (can also use s = s.lstrip())
         i = 0
         while i < len(s) and s[i] == ' ':
             i += 1
         if i == len(s): return 0
+
+        # check leading sign
         sign = 1
         if s[i] == '+':
             i += 1
         elif s[i] == '-':
             sign = -1
             i += 1
+        
+        # add int to ans
         ans = 0
         while i < len(s) and s[i].isdigit():
             ans *= 10
             ans += int(s[i])
             i += 1
         ans *= sign
+
+        # check out of bound
         if ans < -2 ** 31:
             return -2 ** 31
         elif 2 ** 31 - 1 < ans:
