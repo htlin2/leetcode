9d90e60f431a94927df841e6556f09d2cdb41fb4
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-07-01 16:19:21 +0800

    Time: 91 ms (61.60%) | Memory: 16.5 MB (81.28%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -1,18 +1,19 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        rows = collections.defaultdict(set)
-        cols = collections.defaultdict(set)
-        sqrs = collections.defaultdict(set)
-        for r in range(9):
-            for c in range(9):
-                cell = board[r][c]
-                if cell == '.': continue
-                if cell in rows[r]: return False
-                if cell in cols[c]: return False
-                rows[r].add(cell)
-                cols[c].add(cell)
-
-                sqr_tuple = (r // 3, c // 3)
-                if cell in sqrs[sqr_tuple]: return False
-                sqrs[sqr_tuple].add(cell)
+        ROWS, COLS = len(board), len(board[0])
+        r_map = collections.defaultdict(set)
+        c_map = collections.defaultdict(set)
+        d_map = collections.defaultdict(set)
+        for r in range(ROWS):
+            for c in range(COLS):
+                if board[r][c] == '.': continue
+                if board[r][c] in r_map[r]: return False
+                if board[r][c] in c_map[c]: return False
+                r_map[r].add(board[r][c])
+                c_map[c].add(board[r][c])
+                
+                d_r = r // 3
+                d_c = c // 3
+                if board[r][c] in d_map[d_r, d_c]: return False
+                d_map[d_r, d_c].add(board[r][c])
         return True
