273305371d86b53b1a4ce2c46cfb41e11468e7f4
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-07-05 15:07:05 +0800

    Time: 119 ms (11.19%) | Memory: 18.2 MB (31.87%) - LeetSync

---
+++ 224-basic-calculator/basic-calculator.py
@@ -0,0 +1,32 @@
+class Solution:
+    def calculate(self, s: str) -> int:
+        s = s.replace(' ', '')
+        res, cur = 0, 0
+        sign = 1
+        stack = []
+        for char in s:
+            if char.isdigit():
+                cur = cur * 10 + int(char)
+            elif char in ['+', '-']:
+                print(res, cur, sign)
+                res += cur * sign
+                sign = 1 if char == '+' else -1
+                cur = 0
+            elif char == '(':
+                stack.append(res)
+                stack.append(sign)
+                sign = 1
+                res = 0
+            elif char == ')':
+                res += cur * sign
+                res *= stack.pop()
+                res += stack.pop()
+                sign = 1
+                cur = 0
+        return res + sign * cur
+        
+        
+"""
+Input: s = "(1+(4+5+2)-3)+(6+8)"
+Output: 23
+"""
