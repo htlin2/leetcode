becc46301bb92bbe47895045421f8d7a427c4e20
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-07-05 19:24:21 +0800

    Time: 912 ms (37.76%) | Memory: 30.9 MB (42.41%) - LeetSync

--- 892-shortest-subarray-with-sum-at-least-k/shortest-subarray-with-sum-at-least-k.py
+++ 892-shortest-subarray-with-sum-at-least-k/shortest-subarray-with-sum-at-least-k.py
@@ -1,29 +1,36 @@
 class Solution:
     def shortestSubarray(self, nums: List[int], k: int) -> int:
-        N = len(nums)
+        if not nums: return -1
         prefix_sums = []
         curr_sum = 0
-        ans = float('inf')
-        for i in range(N):
-            curr_sum += nums[i]
+        res = float('inf')
+        # prefix sums
+        for i, n in enumerate(nums):
+            curr_sum += n
             if curr_sum >= k:
-                ans = min(ans, i + 1)
+                res = min(res, i + 1)
             prefix_sums.append(curr_sum)
-        q = collections.deque([])
+        # monotonic q
+        N = len(nums)
+        q = collections.deque()
         for i in range(N):
-            prefix = prefix_sums[i]
-            while q and prefix - prefix_sums[q[0]] >= k:
+            while q and prefix_sums[i] - prefix_sums[q[0]] >= k:
                 first = q.popleft()
-                ans = min(ans, i - first)
+                res = min(res, i - first)
             while q and prefix_sums[q[-1]] >= prefix_sums[i]:
                 q.pop()
             q.append(i)
-        return ans if ans != float('inf') else -1
+        return -1 if res == float('inf') else res
+
 """
-prefix sums + monotonic q increasing
-[2,-1,2,-10,4,5
-prefix_sums = -7,-3,2 k=3
-              
-[48,99,37,4,-31]
-48,
+monotonic queue increasing + prefix sums
+k = 22
+queue= [2, 5, 11, 18]
+nums = [2, -1, -2, -3, 4, 5, 6, 7]
+prefx=  2,  1, -1, -4, 0, 5, 11,18
+         /
+        /
+\      /
+ \    /
+  \__/
 """
