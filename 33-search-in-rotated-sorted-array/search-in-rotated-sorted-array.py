3e78eaed02af2832849e6eb770bca4679e9d29c8
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-07-24 01:56:43 +0800

    Time: 47 ms (30.24%) | Memory: 16.9 MB (68.64%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -1,16 +1,17 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        N = len(nums)
-        l, r = 0, N - 1
+        l, r = 0, len(nums) - 1
         while l <= r:
             m = (l + r) // 2
             if nums[m] == target: return m
             if nums[l] <= nums[m]:
+                # left sorted
                 if nums[l] <= target <= nums[m]:
                     r = m - 1
                 else:
                     l = m + 1
             else:
+                # right sorted
                 if nums[m] <= target <= nums[r]:
                     l = m + 1
                 else:
@@ -18,27 +19,11 @@ class Solution:
         return -1
 """
 binary search
- 0 1 2 3 4 5 6
-[0,1,2,4,5,6,7]
-[4,5,6,7,0,1,2], target = 0
- l     m     r
-
-left - Mid is sorted
-1. target is in left - mid
-    search left
-2. target is outside of left - mid
-    search right
-
-       mid - right is sorted
-3. target is in mid - right
-    search right
-4. target is outside of mid - right
-    search mid
-
-Time: O(log n)
-Space: O(1)
-
-brute force is go through nums with for loop
-Time: O(n)
-Space: O(1)
+find left or right side is sorted
+    if target in sorted side:
+        r = m - 1
+    else:
+        l = m + 1
+    if target in not sorted side:
+        
 """
