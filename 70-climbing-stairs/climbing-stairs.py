9897ac41f3e089a616e1c7718828c8909a0f4099
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-07-24 02:53:48 +0800

    Time: 33 ms (68.02%) | Memory: 16.5 MB (50.63%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,9 +1,37 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
+        memo = {}
+        def dfs(n):
+            if n <= 2: return n
+            if n in memo: return memo[n]
+            res = dfs(n - 1) + dfs(n - 2)
+            memo[n] = res
+            return res
+        return dfs(n)
+
+"""
+1. recurrsion + memo
+    memo = {}
+    def dfs(n):
         if n <= 2: return n
-        dp = [0] * (n + 1)
-        dp[0] = 1
-        dp[1] = 1
-        for i in range(1, n + 1):
-            dp[i] = dp[i - 1] + dp[i - 2]
-        return dp[-1]
+        if n in memo: return memo[n]
+        res = dfs(n - 1) + dfs(n - 2)
+        memo[n] = res
+        return res
+    time: O(n)
+    space: O(n)
+2. tabulation
+    dp = [1] * (n + 1)
+    for i in range(2, n):
+        dp[i] = dp[i - 1] + dp[i - 2]
+    return dp[-1]
+    time: O(n)
+    space: O(n)
+3. greedy
+    prev, curr = 1, 1
+    for i in range(n):
+        temp = curr
+        curr = prev + curr
+        prev = temp
+    return curr
+"""
