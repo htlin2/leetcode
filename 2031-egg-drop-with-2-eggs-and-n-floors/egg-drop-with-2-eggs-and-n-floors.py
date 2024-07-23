44ae312ffa38cff0a20262264818e4c57328c8a8
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-07-24 01:16:29 +0800

    Time: 38 ms (64.61%) | Memory: 16.5 MB (73.99%) - LeetSync

--- 2031-egg-drop-with-2-eggs-and-n-floors/egg-drop-with-2-eggs-and-n-floors.py
+++ 2031-egg-drop-with-2-eggs-and-n-floors/egg-drop-with-2-eggs-and-n-floors.py
@@ -1,15 +1,8 @@
 class Solution:
     def twoEggDrop(self, n: int) -> int:
-        # Initialize a DP table with (n+1) entries, since floors are 0-indexed
-        dp = [0] * (n + 1)
-        
-        # The worst-case attempts can be at most the number of floors
-        for m in range(1, n + 1):
-            dp[m] = m
-
-        # We iterate through the number of floors
-        for m in range(1, n + 1):
-            for f in range(1, m + 1):
-                dp[m] = min(dp[m], 1 + max(f - 1, dp[m - f]))
-        
-        return dp[n]
+        ans = 0
+        while(n > 0):
+            ans += 1
+            n -= ans
+            
+        return ans
