eaa1efb8236c2ad50ef0871a2aba44656eb59a38
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-07-26 03:13:32 +0800

    Time: 352 ms (88.75%) | Memory: 38.7 MB (24.11%) - LeetSync

--- 295-find-median-from-data-stream/find-median-from-data-stream.py
+++ 295-find-median-from-data-stream/find-median-from-data-stream.py
@@ -1,51 +1,30 @@
 class MedianFinder:
-
     def __init__(self):
-        self.left = [] # max_heap
-        self.right = [] # min_heap
+        self.min_heap = [] # right
+        self.max_heap = [] # left
 
     def addNum(self, num: int) -> None:
-        if self.right and self.right[0] < num:
-            heapq.heappush(self.right, num)
+        min_heap, max_heap = self.min_heap, self.max_heap
+        if min_heap and num < min_heap[0]:
+            heapq.heappush(max_heap, -num)
         else:
-            heapq.heappush(self.left, -num)
-        
-        # balance length
-        if len(self.right) - len(self.left) > 1:
-            n = heapq.heappop(self.right)
-            heapq.heappush(self.left, -n)
-        elif len(self.left) - len(self.right) > 1:
-            n = heapq.heappop(self.left)
-            heapq.heappush(self.right, -n)
+            heapq.heappush(min_heap, num)
+
+        if len(min_heap) - len(max_heap) >= 2:
+            heapq.heappush(max_heap, -heapq.heappop(min_heap))
+        elif len(max_heap) - len(min_heap) >= 2:
+            heapq.heappush(min_heap, -heapq.heappop(max_heap))
 
     def findMedian(self) -> float:
-        N = len(self.left) + len(self.right)
-        if N % 2 == 0:
-            # even
-            total = self.right[0] - self.left[0]
-            return float(total / 2)
-        # odd
-        if len(self.right) - len(self.left) > 0:
-            return self.right[0]
-        return -self.left[0]
+        min_heap, max_heap = self.min_heap, self.max_heap
+        if len(min_heap) > len(max_heap):
+            return min_heap[0] * 1.0
+        if len(max_heap) > len(min_heap):
+            return max_heap[0] * -1.0
+        return (min_heap[0] - max_heap[0]) / 2
 
 
 # Your MedianFinder object will be instantiated and called as such:
 # obj = MedianFinder()
 # obj.addNum(num)
 # param_2 = obj.findMedian()
-
-"""
-1, 2, 3, 4, 5, 6
-left = max_heap = [-1]
-righ = min_heap = [2,3]
-
-
-
-# findMedian
-even
-min_heap[0] + max_heap[0] / 2
-odd
-longest left or right
-return first element
-"""
