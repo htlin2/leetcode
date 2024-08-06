4a21667eb8d363332627d6c52233281077837dd3
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-08-06 12:12:30 -0700

    Time: 532 ms (89.54%) | Memory: 74.4 MB (20.59%) - LeetSync

--- 1023-time-based-key-value-store/time-based-key-value-store.py
+++ 1023-time-based-key-value-store/time-based-key-value-store.py
@@ -1,32 +1,23 @@
 class TimeMap:
+
     def __init__(self):
-        # key: [(time1, value1), (t2, v2)]
-        self.cache = collections.defaultdict(list)
-        # Time: O(1)
-        # Space: O(n)
+        self.cache = collections.defaultdict(list) #(timestamp, value)
+
     def set(self, key: str, value: str, timestamp: int) -> None:
-        self.cache[key].append((timestamp, value))
-        # Time: O(1)
-        # Space: O(1)
+        self.cache[key].append([timestamp, value])
 
     def get(self, key: str, timestamp: int) -> str:
-        if key not in self.cache: return ''
-        values = self.cache[key]
-        index = bisect.bisect_left(values, (timestamp,))
-        if index < len(values) and values[index][0] == timestamp:
-            return values[index][-1]
-        if index == 0: return ''
-        return values[index - 1][-1]
-        # Time: O(log n)
-        # Space: O(1)
-        
-"""
-hashmap + binary search weight left
-#cache =  key: [(time1, value1), (t2, v2)]
-foo: [(1, bar)]
-get(foo, 0) return "" binary search weight left
+        timestamp_values = self.cache[key]
+        if not timestamp_values:
+            return ''
+        to_find = timestamp
+        idx = bisect.bisect_left(timestamp_values, [to_find, ])
+        if 0 <= idx < len(timestamp_values) and timestamp_values[idx][0] == timestamp:
+            return timestamp_values[idx][1]
+        if idx == 0:
+            return ''
+        return timestamp_values[idx - 1][1]
 
-"""
 
 # Your TimeMap object will be instantiated and called as such:
 # obj = TimeMap()
