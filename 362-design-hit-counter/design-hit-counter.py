e1878f055b29b0f300770695d27f16bf2bd4db26
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-08-09 11:27:49 -0700

    Time: 35 ms (57.51%) | Memory: 16.6 MB (62.93%) - LeetSync

--- 362-design-hit-counter/design-hit-counter.py
+++ 362-design-hit-counter/design-hit-counter.py
@@ -11,15 +11,6 @@ class HitCounter:
         to_find = timestamp - 300
         idx = bisect.bisect_right(self.cache, to_find)
         return N - idx
-        # while l <= r:
-        #     m = (l + r) // 2
-        #     if self.cache[m] <= timestamp:
-        #         l = m + 1
-        #     else:
-        #         r = m - 1
-        # if l - 1 >= 0 and self.cache[l - 1] == timestamp:
-        #     return l - 1
-        # return l
 
 
 # Your HitCounter object will be instantiated and called as such:
@@ -27,4 +18,4 @@ class HitCounter:
 # obj.hit(timestamp)
 # param_2 = obj.getHits(timestamp)
 
-# 1, 2, 3, 300
+# 1, 2, 3, 300, 301
