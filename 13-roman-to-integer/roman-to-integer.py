a2450f0070160a5328a1d3e14af1c798e1888ea0
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-08-14 18:55:42 -0700

    Time: 51 ms (32.35%) | Memory: 16.5 MB (85.84%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -2,29 +2,17 @@ class Solution:
     def romanToInt(self, s: str) -> int:
         h = {
             'I': 1,
-            'IV': 4,
             'V': 5,
-            'IX': 9,
             'X': 10,
-            'XL': 40,
             'L': 50,
-            'XC': 90,
             'C': 100,
-            'CD': 400,
             'D': 500,
-            'CM': 900,
             'M': 1000,
         }
         res = 0
-        N = len(s)
-
-        for i in range(N):
-            char = s[i]
-            res += h[char]
-            if i - 1 >= 0:
-                prev_char = s[i - 1] + s[i]
-                if prev_char in h:
-                    res += h[prev_char]
-                    res -= h[char]
-                    res -= h[s[i - 1]]
+        for i, char in enumerate(s):
+            if i + 1 < len(s) and h[char] < h[s[i + 1]]:
+                res -= h[char]
+            else:
+                res += h[char]
         return res
