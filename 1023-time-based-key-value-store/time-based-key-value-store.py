b55671bbd8f2aa87967246e991c877c32f18d762
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-08-16 22:14:06 -0700

    Time: 582 ms (63.59%) | Memory: 73.5 MB (86.02%) - LeetSync

--- 1023-time-based-key-value-store/time-based-key-value-store.py
+++ 1023-time-based-key-value-store/time-based-key-value-store.py
@@ -1,26 +1,30 @@
 class TimeMap:
 
     def __init__(self):
-        self.cache = collections.defaultdict(list) #(timestamp, value)
+        self.cache = collections.defaultdict(list) # key: [(time, value)]
 
     def set(self, key: str, value: str, timestamp: int) -> None:
-        self.cache[key].append([timestamp, value])
+        self.cache[key].append((timestamp, value))
 
     def get(self, key: str, timestamp: int) -> str:
-        timestamp_values = self.cache[key]
-        to_find = timestamp
-        l, r = 0, len(timestamp_values) - 1
+        if not key in self.cache: return ''
+        time_values = self.cache[key]
+        N = len(time_values)
+        l, r = 0, N - 1
         while l <= r:
             m = (l + r) // 2
-            if timestamp_values[m][0] == timestamp:
-                return timestamp_values[m][1]
-            if timestamp_values[m][0] < timestamp:
+            # binary search weight left
+            if time_values[m][0] == timestamp:
+                r = m - 1
+            elif time_values[m][0] < timestamp:
                 l = m + 1
             else:
                 r = m - 1
-        if 0 <= l - 1 < len(timestamp_values):
-            return timestamp_values[l - 1][1]
-        return ''
+        if 0 <= l < N and time_values[l][0] == timestamp:
+            return time_values[l][1]
+        if 0 == l:
+            return ''
+        return time_values[l - 1][1]
 
 
 # Your TimeMap object will be instantiated and called as such:
