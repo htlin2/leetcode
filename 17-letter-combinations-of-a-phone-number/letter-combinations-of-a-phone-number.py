08bdc6d1df60d7261c5289340982d182fda6c1b4
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-08-18 09:08:59 -0700

    Time: 37 ms (44.41%) | Memory: 16.5 MB (41.89%) - LeetSync

---
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -0,0 +1,30 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        adj = {
+            '2': 'abc',
+            '3': 'def',
+            '4': 'ghi',
+            '5': 'jkl',
+            '6': 'mno',
+            '7': 'pqrs',
+            '8': 'tuv',
+            '9': 'wxyz',
+        }
+        if not digits: return []
+        N = len(digits)
+        res = []
+        def dfs(temp, i):
+            nonlocal res
+            if len(temp) == N:
+                res.append(temp[:])
+                return
+            d = digits[i]
+            for char in adj[d]:
+                dfs(temp + char, i + 1)
+        dfs('', 0)
+        return res
+
+"""
+backtracking
+
+"""
