b912a5f2ede590ad74bc4d72e97e91041c60eb99
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-08-18 16:25:03 -0700

    Time: 170 ms (74.15%) | Memory: 20.1 MB (91.59%) - LeetSync

--- 721-accounts-merge/accounts-merge.py
+++ 721-accounts-merge/accounts-merge.py
@@ -1,38 +1,56 @@
+class UnionFind:
+    def __init__(self, N):
+        self.parents = [i for i in range(N)]
+        self.ranks = [1] * N
+
+    def find(self, n):
+        if self.parents[n] == n: return n
+        self.parents[n] = self.find(self.parents[self.parents[n]])
+        return self.parents[n]
+
+    def union(self, n1, n2):
+        p1, p2 = self.find(n1), self.find(n2)
+        if p1 == p2: return False
+        if self.ranks[p1] >= self.ranks[p2]:
+            self.ranks[p1] += self.ranks[p2]
+            self.parents[p2] = p1
+        else:
+            self.ranks[p2] += self.ranks[p1]
+            self.parents[p1] = p2
+        return True
+
 class Solution:
     def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
-        email_index = collections.defaultdict(list)
+        N = len(accounts)
+        uf = UnionFind(N)
+        email_idx = {}
         for i, a in enumerate(accounts):
             for email in a[1:]:
-                email_index[email].append(i)
-        
-        visited = set()
-        def dfs(i, emails):
-            if i in visited: return
-            visited.add(i)
-            for j in range(1, len(accounts[i])):
-                email = accounts[i][j]
-                emails.add(email)
-                for nei in email_index[email]:
-                    if nei in visited: continue
-                    dfs(nei, emails)
-
+                if email in email_idx:
+                    uf.union(i, email_idx[email])
+                else:
+                    email_idx[email] = i
+        parent_emails = collections.defaultdict(list)
+        for e, i in email_idx.items():
+            parent = uf.find(i)
+            parent_emails[parent].append(e)
         res = []
-        for i, a in enumerate(accounts):
-            if i in visited: continue
-            name = a[0]
-            emails = set()
-            dfs(i, emails)
-            res.append([name] + sorted(emails))
+        for i, emails in parent_emails.items():
+            name = accounts[i][0]
+            name_emails = [name] + sorted(emails)
+            res.append(name_emails)
         return res
+            
 """
-[
-    ["John","johnsmith@mail.com","john_newyork@mail.com"],
-    ["John","johnsmith@mail.com","john00@mail.com"],
-    ["Mary","mary@mail.com"],
-    ["John","johnnybravo@mail.com"]
-]
-johnsmith@mail.com: [0, 1]
-john_newyork@mail.com: [0]
-john00@mail.com: [1]
+dfs
+johnsmith@mail.com: 0
+john_newyork@mail.com: 0
+john00@mail.com: 1
+mary@mail.com: 2
+johnnybravo@mail.com: 3
+
+0: [johnsmith@mail.com, john_newyork@mail.com]
+1: [john00@mail.com]
+
 
 """
