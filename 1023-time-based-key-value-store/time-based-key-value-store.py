6902a0c21f46aa276cb8807abfaf60a5b1864532
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-08-19 12:51:11 -0700

    Time: 587 ms (55.56%) | Memory: 73.5 MB (74.69%) - LeetSync

--- 1023-time-based-key-value-store/time-based-key-value-store.py
+++ 1023-time-based-key-value-store/time-based-key-value-store.py
@@ -11,14 +11,21 @@ class TimeMap:
             return ''
         time_values = self.cache[key]
         N = len(time_values)
-        to_find = (timestamp,)
-        idx = bisect.bisect_left(time_values, to_find)
-        if 0 <= idx < N:
-            if time_values[idx][0] == timestamp:
-                return time_values[idx][1]
-        if 0 == idx:
+        l, r = 0, N - 1
+        while l <= r:
+            m = (l + r) // 2
+            if time_values[m][0] == timestamp:
+                r = m - 1
+            elif time_values[m][0] < timestamp:
+                l = m + 1
+            else:
+                r = m - 1
+        if 0 <= l < N:
+            if time_values[l][0] == timestamp:
+                return time_values[l][-1]
+        if 0 == l:
             return ''
-        return time_values[idx - 1][1]
+        return time_values[l - 1][-1]
 
 # Your TimeMap object will be instantiated and called as such:
 # obj = TimeMap()
