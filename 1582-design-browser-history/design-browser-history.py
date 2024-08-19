caf40082fc0a1a9bb81261246c8186290f0163b2
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-08-19 12:28:59 -0700

    Time: 197 ms (48.72%) | Memory: 19.1 MB (68.91%) - LeetSync

--- 1582-design-browser-history/design-browser-history.py
+++ 1582-design-browser-history/design-browser-history.py
@@ -1,28 +1,36 @@
+class Node:
+
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+        self.prev = None
+
 class BrowserHistory:
 
     def __init__(self, homepage: str):
-        self.prev = []
-        self.next = []
-        self.curr = homepage
+        self.head = Node(-1)
+        self.tail = Node(-1)
+        self.curr = Node(homepage)
+        self.head.next, self.tail.prev = self.curr, self.curr
+        self.curr.next, self.curr.prev = self.tail, self.head
 
     def visit(self, url: str) -> None:
-        self.prev.append(self.curr[:])
-        self.curr = url
-        self.next = []
+        node = Node(url)
+        self.curr.next, self.tail.prev = node, node
+        node.prev, node.next = self.curr, self.tail
+        self.curr = self.curr.next
 
     def back(self, steps: int) -> str:
-        while self.prev and steps:
-            self.next.append(self.curr[:])
-            self.curr = self.prev.pop()
-            steps -= 1
-        return self.curr
+        while self.curr.prev != self.head and steps:
+            self.curr = self.curr.prev
+            steps -= 1        
+        return self.curr.val
 
     def forward(self, steps: int) -> str:
-        while self.next and steps:
-            self.prev.append(self.curr[:])
-            self.curr = self.next.pop()
-            steps -= 1
-        return self.curr
+        while self.curr.next != self.tail and steps:
+            self.curr = self.curr.next
+            steps -= 1        
+        return self.curr.val
 
 
 # Your BrowserHistory object will be instantiated and called as such:
