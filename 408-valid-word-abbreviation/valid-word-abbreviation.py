3cdc3dae15730cb09872db203624b6df2ca33771
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-08-23 10:50:36 -0700

    Time: 46 ms (5.24%) | Memory: 16.6 MB (40.97%) - LeetSync

--- 408-valid-word-abbreviation/valid-word-abbreviation.py
+++ 408-valid-word-abbreviation/valid-word-abbreviation.py
@@ -1,44 +1,17 @@
 class Solution:
     def validWordAbbreviation(self, word: str, abbr: str) -> bool:
-        char_int_list = []
-        digits = 0
-        for char in abbr:
-            if char.isdigit():
-                digits *= 10
-                digits += int(char)
-                if digits == 0: return False
-                continue
-            if digits:
-                char_int_list.append(digits)
-                digits = 0
-            char_int_list.append(char)
-        if digits:
-            char_int_list.append(digits)
         i, j = 0, 0
-        while i < len(word) and j < len(char_int_list):
-            if isinstance(char_int_list[j], int):
-                i += char_int_list[j]
+        while i < len(word) and j < len(abbr):
+            if not abbr[j].isdigit():
+                if word[i] != abbr[j]: return False
+                i += 1
                 j += 1
-                continue
-            if char_int_list[j] != word[i]:
-                return False
-            i += 1
-            j += 1
-        return i == len(word) and j == len(char_int_list)
-
-"""
-Input: word = "internationalization", abbr = "i12iz4n"
-
-i 12 i z 4 n
-j
-i = 0
-while i < N:
-    if abbr[j] is digit:
-        i += abbr[j]
-        j += 1
-        continue
-    else:
-        match word[i] == abbr[j]
-        i += 1
-        j += 1
-"""
+            elif abbr[j] == '0': return False
+            else:
+                k = j
+                while k < len(abbr) and abbr[k].isdigit():
+                    k += 1
+                num = int(abbr[j:k])
+                i += num
+                j = k
+        return i == len(word) and j == len(abbr)
