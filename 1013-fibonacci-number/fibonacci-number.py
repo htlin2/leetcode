2667b344f8f4e17727c4d1191b40efa41d97459f
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-08-23 12:49:36 -0700

    Time: 32 ms (83.92%) | Memory: 16.5 MB (11.82%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -1,11 +1,10 @@
 class Solution:
     def fib(self, n: int) -> int:
-        prev, curr = 0, 1
-        for _ in range(n):
-            temp = curr + prev
-            prev = curr
-            curr = temp
-        return prev
+        dp = collections.defaultdict(int)
+        dp[1] = 1
+        for i in range(2, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
+        return dp[n]
 """
 1. recurrsion
     dfs
