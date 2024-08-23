dd1cdb7b333a1ba6d244bb156f55c296206ef96a
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-08-22 23:48:57 -0700

    Time: 555 ms (22.96%) | Memory: 31.1 MB (39.01%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -1,19 +1,21 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        def helper(l, r):
-            if l > r: return float('-inf')
-            left_sum = right_sum = total_sum = 0
-            mid = (l + r) // 2
-            curr_sum = 0
-            for i in range(mid - 1, l - 1, -1):
-                curr_sum += nums[i]
-                left_sum = max(left_sum, curr_sum)
-            curr_sum = 0
-            for i in range(mid + 1, r + 1):
-                curr_sum += nums[i]
-                right_sum = max(right_sum, curr_sum)
-            combined_sum = left_sum + nums[mid] + right_sum
-            left_max = helper(l, mid - 1)
-            right_max = helper(mid + 1, r)
-            return max(left_max, right_max, combined_sum)
-        return helper(0, len(nums) - 1)
+        res = curr_sum = float('-inf')
+        for n in nums:
+            curr_sum = max(curr_sum + n, n)
+            res = max(curr_sum, res)
+        return res
+
+
+"""
+prefix sum
+sliding window x b/c it has negative
+[-2, 1,-3,4,-1, 2, 1,-5, 4
+greedy
+        -2, 1 -2,4,-3, 5, 6,
+        -2, 1,-1,4, 1, 6         max(curr_sum, curr)
+res     -2, 1, 1,4, 4, 6
+res = 0
+ -2,-1,-4,0,-1,-1, 0,-5,-1 = prefix_sum
+ -2,
+"""
