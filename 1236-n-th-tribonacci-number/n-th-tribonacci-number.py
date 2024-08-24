77afd8de5e43f16d8d5e6574bf5d956ed370fb6f
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-08-23 18:26:24 -0700

    Time: 44 ms (7.84%) | Memory: 16.5 MB (59.63%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
@@ -1,14 +1,13 @@
 class Solution:
     def tribonacci(self, n: int) -> int:
-        memo = {0: 0, 1: 1, 2: 1}
+        if n < 2: return n
+        t0, t1, t2 = 0, 1, 1
         for i in range(3, n + 1):
-            memo[i] = memo[i - 1] + memo[i - 2] + memo[i - 3]
-        return memo[n]
+            temp = t0 + t1 + t2
+            t0 = t1
+            t1 = t2
+            t2 = temp
+        return t2
 
 """
-dfs + memo
-generate all fibonacci numbers 
-store in memo
-for loop through n
-    look up in memo and sum up
 """
