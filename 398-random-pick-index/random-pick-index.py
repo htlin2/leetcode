5e2eb1b56fb52114b96c63e0f55c6c0ff7c1efdf
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-08-25 11:24:37 -0700

    Time: 302 ms (14.74%) | Memory: 26.6 MB (21.64%) - LeetSync

--- 398-random-pick-index/random-pick-index.py
+++ 398-random-pick-index/random-pick-index.py
@@ -1,20 +1,13 @@
 class Solution:
 
     def __init__(self, nums: List[int]):
-        self.nums = nums
-        self.hashmap = collections.defaultdict(int)
-        for n in nums:
-            self.hashmap[n] += 1
+        self.hashmap = collections.defaultdict(list)
+        for i, n in enumerate(nums):
+            self.hashmap[n].append(i)
 
     def pick(self, target: int) -> int:
-        count = self.hashmap[target]
-        pick = random.randrange(1, count + 1)
-        curr_count = 0
-        for i, n in enumerate(self.nums):
-            if n == target:
-                curr_count += 1
-            if curr_count == pick:
-                return i
+        indices = self.hashmap[target]
+        return random.choice(indices)
 # Your Solution object will be instantiated and called as such:
 # obj = Solution(nums)
 # param_1 = obj.pick(target)
