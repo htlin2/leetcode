9e63c553cf4fd9b0b589fd7f7a5664c8ce758b9c
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-09-01 11:41:06 -0700

    Time: 87 ms (82.53%) | Memory: 59.8 MB (32.56%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.js
@@ -0,0 +1,37 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number[][]}
+ */
+var merge = function(intervals) {
+    if (!intervals.length) return []
+    intervals.sort((a, b) => {
+        if (a[0] < b[0]) return -1
+        return 1
+    })
+    const stack = []
+    for (let i = 0; i < intervals.length; i++) {
+        if (i === 0) {
+            stack.push(intervals[i])
+            continue
+        }
+        const [sBegin, sEnd] = stack[stack.length - 1]
+        const [iBegin, iEnd] = intervals[i]
+        const isOverlap = (sBegin <= iBegin && iBegin <= sEnd) || (iBegin <= sBegin && sBegin <= iEnd)
+        if (isOverlap) {
+            stack.pop()
+            stack.push([Math.min(sBegin, iBegin), Math.max(sEnd, iEnd)])
+        } else {
+            stack.push(intervals[i])
+        }
+    }
+    return stack
+};
+
+`
+input = [[1,3],[2,6],[8,10],[15,18]]
+output =[[1,6],[8,10],[15,18]]
+[1,      6   
+            8    10
+                    15   18
+stack = [[1, 6], [8, 10], ]
+`
