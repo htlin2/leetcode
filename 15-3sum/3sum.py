442e93841ba38a0cd379e1bab019ff2e0caf38eb
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-09-08 19:36:36 -0700

    Time: 2174 ms (5.01%) | Memory: 19.9 MB (99.06%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,44 +1,45 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        N = len(nums)
         nums.sort()
         res = set()
         def two_sum(i, left, right):
-            nonlocal res
             while left < right:
-                total = nums[i] + nums[left] + nums[right]
-                if total == 0:
+                three_sum = nums[i] + nums[left] + nums[right]
+                if three_sum == 0:
                     res.add((nums[i], nums[left], nums[right]))
-                if total <= 0:
+                if three_sum <= 0:
                     left += 1
                 else:
                     right -= 1
-        N = len(nums)
         for i in range(N - 2):
-            if i - 1 >= 0 and nums[i] == nums[i - 1]:
-                continue
             two_sum(i, i + 1, N - 1)
         return list(res)
 
 
 """
-for loop + 2 pointers
--1, -1, 2
--1 0 1
-[-4,-1,-1,0,1,2]
+1. for loop + hashmap + sort
+Input: nums = [-1,0,1,2,-1,-4]
+Output: [[-1,-1,2],[-1,0,1]]
+[-4,-1,-1,-1,0,1,2]
+     i     L     R  
+-4 + -1 + 2 = -3 < 0, so L++
+-4 +  0 + 2 = -2 < 0, so L++
+-4 +  1 + 2 = -1 < 0, so L++
 
-def two_sum(i, left, right):
-    while left < right:
-        total = i + left + right
-        if total == 0:
-            append [i, left, right]
-        if total <= 0:
-            left += 1
-        else:
-            right -= 1
-    
-for loop:
-    if i == prev_i:
-        skip
-    two_sum(i, i + 1, N - 1)
+-1 + -1 + 2 = 0 <= 0, so L++  solution [-1,-1,2]
+-1 +  0 + 2 = 1  > 0, so R--
+-1 +  0 + 1 = 0 <= 0, so L++  solution [-1,0,1]
+edge case:
+    handle nums[i - 1] == nums[i], skip to next
+    handle nums[L-1] == nums[L]
+    or 
+    make sure solutions are set
+Time: O(n^2)
+Space: O(n)
 
+2. brute force
+triple for loops
+Time: O(n^3)
+Space: O(n)
 """
