51274b79a177fd45e80b2602075cc19c8b29e3b9
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-09-08 19:20:13 -0700

    Time: 51 ms (92.39%) | Memory: 17.7 MB (57.70%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,23 +1,33 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        h = {} # {num: index}
+        hashmap = {}
         for i, n in enumerate(nums):
             delta = target - n
-            if delta in h:
-                return [h[delta], i]
-            h[n] = i
+            if delta in hashmap:
+                return [hashmap[delta], i]
+            hashmap[n] = i
+
 """
-1) brute force
+1. brute force
+[11,15,2,7]
+       i j     
+11 + 15 = 26
+11 + 2 = 13
+11 + 7 = 18
+2 + 7 = 9
 Time: O(n^2)
 Space: O(1)
 
-2) hashmap = {num: index}
-Time: O(n)
-Space: O(n)
+2. hashmap
+Input: nums = [11,15,2,7], target = 9
+Output: [2,3]
+hashmap = {  number: index
+    11: 0,
+    15: 1,
+    2: 2
 
-3) two pointers + sort
-(num, index)
-[(2,1), (3,0), (4, 2)]
-Time: O(n log n)
-Space: O(n)
+}
+return [2,3]
+time: O(n)
+space: O(n)
 """
