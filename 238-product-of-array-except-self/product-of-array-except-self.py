2f2c4b952b8d2bce7a5942d8795d95ea70725015
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-09-14 15:56:22 -0700

    Time: 288 ms (14.03%) | Memory: 26.2 MB (35.26%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,29 +1,38 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         N = len(nums)
-        prefix_sums = [1] * N
-        prefix = 1
-        for i in range(N):
-            prefix_sums[i] = prefix
-            prefix *= nums[i]
-        postfix = 1
+        prefix = []
+        curr_prefix = 1
+        for n in nums:
+            curr_prefix *= n
+            prefix.append(curr_prefix)
+        postfix = []
+        curr_postfix = 1
         for i in range(N - 1, -1, -1):
-            prefix_sums[i] *= postfix
-            postfix *= nums[i]
-        return prefix_sums
-        
+            curr_postfix *= nums[i]
+            postfix.append(curr_postfix)
+        postfix.reverse()
+        res = []
+        for i in range(N):
+            pre = prefix[i - 1] if i - 1 >= 0 else 1
+            post = postfix[i + 1] if i + 1 < N else 1
+            res.append(pre * post)
+        return res
 
 """
-1. brute force
-2 for loops
-Time: O(n^2)
-space: O(1)
+Input: nums = [1,2,3,4]
+Output: [24,12,8,6]
 
-2. prefix_sums
-nums = [ 1, 2, 3, 4]
-prefix [ 1, 1, 2, 6]
-postfix[24,12, 4, 1]
-ans    [24,12, 8, 6]
-time: O(n)
-space: O(n)
+prefix = [ 1, 2, 6,24]
+postfix= [24,24,12, 4]
+res    = [24,12, 8, 6] => prefix[i - 1] * postfix[i + 1]
+Time: O(n)
+Space: O(n)
+
+
+Optimized
+postfix= [24,12, 8, 4]
+prefix  =           6
+Time: O(n)
+Space: O(1)
 """
