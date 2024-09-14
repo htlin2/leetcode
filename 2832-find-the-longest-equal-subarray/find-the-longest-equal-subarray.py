b9fafa047ef732f47a1b5ae4c368e343ba4907cd
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-09-14 15:14:07 -0700

    Time: 1342 ms (45.61%) | Memory: 33.4 MB (72.28%) - LeetSync

--- 2832-find-the-longest-equal-subarray/find-the-longest-equal-subarray.py
+++ 2832-find-the-longest-equal-subarray/find-the-longest-equal-subarray.py
@@ -1,35 +1,23 @@
 class Solution:
     def longestEqualSubarray(self, nums: List[int], k: int) -> int:
+        N = len(nums)
         window = collections.defaultdict(int)
         left, max_f = 0, 0
-        for right in range(len(nums)):
+        for right in range(N):
             window[nums[right]] += 1
             max_f = max(max_f, window[nums[right]])
             while (right - left + 1) - max_f > k:
                 window[nums[left]] -= 1
+                max_f = max(max_f, window[nums[left]])
                 left += 1
         return max_f
 
-
 """
-Input: nums = 
-[1,3,2,3,1,3], k = 3
-[1,3,_,3,_,3], k = 3
-
-counter {
-    1: 1
-    3: 2
-    2: 1
-}
-
-1, 2, 3
-delete or skip
-delet k -= 1
-backtracking + memo
-dfs(i, k, prev_num):
-    prev_num
-
-
-[1,3,_,3,_,3], k = 3
-max_freq = 1
+Input: nums = [1,3,2,3,1,3], k = 3
+sliding window variable
+window= 1:1, 3:3, 2:1, 4: 1
+max_f = 3
+delta = (r - l + 1) - max_f = 4 is < k? no
+nums = [ 1, 3, 2, 3, 1, 3, 4]
+            l              r
 """
