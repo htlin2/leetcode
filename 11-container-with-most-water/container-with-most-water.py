306254b5c08ed001a52ac3def2a3a8cfd41e2b26
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-09-14 21:56:15 -0700

    Time: 518 ms (68.54%) | Memory: 30.1 MB (24.76%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -1,23 +1,24 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        res = 0
         N = len(height)
-        l, r = 0, N - 1
-        while l < r:
-            base = r - l
-            min_height = min(height[l], height[r])
-            res = max(res, min_height * base)
-            if height[l] < height[r]:
-                l += 1
+        left, right = 0, N - 1
+        res = 0
+        while left < right:
+            area = (right - left) * min(height[left], height[right])
+            res = max(area, res)
+            if height[left] < height[right]:
+                left += 1
             else:
-                r -= 1
+                right -= 1
         return res
-
 """
+Input: height = [1,8,6,2,5,4,8,3,7]
+Output: 49
  0,1,2,3,4,5,6,7,8
 [1,8,6,2,5,4,8,3,7]
- l               r
- base = r - l
- min_height = min(l, r)
- move lower r or l inside
+   l             r
+
+ max_area = 9
+ (8 - 1) = 7
+ 7 * min(8, 7) = 49
 """
