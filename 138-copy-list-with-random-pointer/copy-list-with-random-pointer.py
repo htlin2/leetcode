99fa3e548adc90257ca8633b598cc1f846e2de01
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-09-15 13:45:18 -0700

    Time: 42 ms (27.63%) | Memory: 17.4 MB (78.96%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.py
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.py
@@ -9,44 +9,17 @@ class Node:
 
 class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
-        copy_hashmap = {-1: Node(-1), None: None} # original_node: copy_node
-        dummy = copy_hashmap[-1]
-        curr = head
-        while curr:
-            # handle copy
-            if curr in copy_hashmap:
-                copy_node = copy_hashmap[curr]
-            else:
-                copy_node = Node(curr.val)
-                copy_hashmap[curr] = copy_node
-            dummy.next = copy_node
-            # handle random
-            if curr.random in copy_hashmap:
-                random_node = copy_hashmap[curr.random]
-            else:
-                random_node = Node(curr.random.val)
-                copy_hashmap[curr.random] = random_node
-            copy_node.random = random_node
-            dummy = dummy.next
-            curr = curr.next
-        return copy_hashmap[-1].next
-
-
-"""
-Input: head = [[7,null],[13,0],[11,4],[10,2],[1,0]] => [node.val, index]
-Output: [[7,null],[13,0],[11,4],[10,2],[1,0]]
-
-hashmap = {
-    index: node
-        0: node(7)
-        1: node(13)
-        2: node(11)
-        3: node(10)
-        4: node(1)
-} 
-dummy : 7    ->        13  ->  hashmap[2]
-random: null -> hashmap[0] ->  hashmap[4]
-
-Time: O(n)
-Space: O(n)
-"""
+        hashmap = {None: None} # original_node: copy_node
+        node = head
+        while node:
+            copy_node = Node(node.val)
+            hashmap[node] = copy_node
+            node = node.next
+        node = head
+        while node:
+            copy_node = hashmap[node]
+            next_node = hashmap[node.next]
+            random_node = hashmap[node.random]
+            copy_node.next, copy_node.random = next_node, random_node
+            node = node.next
+        return hashmap[head]
