0d5fd85ccd1cb6738c3eac09f464f1e6910305a9
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-09-15 16:04:20 -0700

    Time: 78 ms (51.11%) | Memory: 16.6 MB (91.82%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.py
+++ 572-subtree-of-another-tree/subtree-of-another-tree.py
@@ -5,28 +5,18 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def isSame(self, root, subRoot):
-        # post-order
-        if not root and not subRoot: return True
-        if root and not subRoot: return False
-        if not root and subRoot: return False
-        if root.val != subRoot.val: return False
-        left = self.isSame(root.left, subRoot.left)
-        right = self.isSame(root.right, subRoot.right)
-        return left and right
-
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        # pre-order dfs
-        # base case: 
-        if not root and not subRoot: return True
-        if root and not subRoot: return False
+        if root == subRoot: return True
         if not root and subRoot: return False
-        # when do i go to next level?
-        if root.val == subRoot.val and self.isSame(root, subRoot):
-            return True
-        else:
-            left = self.isSubtree(root.left, subRoot)
-            right = self.isSubtree(root.right, subRoot)
-        # if left and right are true: return True
-        # else return False
-        return left or right
+        if root and not subRoot: return False
+        if root.val == subRoot.val:
+            if self.isSame(root, subRoot):
+                return True
+        return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
+
+    def isSame(self, root, subRoot):
+        if root == subRoot: return True
+        if not root and subRoot: return False
+        if root and not subRoot: return False
+        if root.val != subRoot.val: return False
+        return self.isSame(root.left, subRoot.left) and self.isSame(root.right, subRoot.right)
