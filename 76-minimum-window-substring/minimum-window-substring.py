502cac1279c9123384bd950b5d1f9dec21e80c4c
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-09-15 10:27:03 -0700

    Time: 588 ms (10.36%) | Memory: 17.4 MB (21.34%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -1,22 +1,38 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        if len(s) < len(t): return ''
-        counter = collections.Counter(t)
-        need, have = len(counter), 0
-        window = collections.defaultdict(int)
+        def is_match(s_counter, t_counter):
+            if len(s_counter.keys()) != len(t_counter.keys()):
+                return False
+            for key in s_counter.keys():
+                if s_counter[key] < t_counter[key]:
+                    return False
+            return True
+        if len(s) < len(t):
+            return ''
         res = ''
-        l = 0
-        for r, char in enumerate(s):
-            if char not in counter: continue
-            window[char] += 1
-            if window[char] == counter[char]:
-                have += 1
-            while need == have:
-                if not res or len(res) > r - l + 1:
-                    res = s[l:r + 1]
-                if s[l] in counter:
-                    window[s[l]] -= 1
-                    if window[s[l]] < counter[s[l]]:
-                        have -= 1
-                l += 1
+        t_counter = collections.Counter(t)
+        s_counter = {}
+        left = 0
+        for right in range(len(s)):
+            char = s[right]
+            if char in t_counter:
+                s_counter[char] = s_counter.get(char, 0) + 1
+            while is_match(s_counter, t_counter):
+                if not res or right - left + 1 < len(res):
+                    res = s[left: right+1]
+                s_char = s[left]
+                if s_char in s_counter:
+                    s_counter[s_char] -= 1
+                    if s_counter[s_char] == 0:
+                        del s_counter[s_char]
+                left += 1
         return res
+"""
+Input: s = "ADOBECODEBANC", t = "ABC"
+Output: "BANC"
+t_counter = {A: 1, B: 1, C: 1}
+s_counter = {A: 1, B: 2, C: 1}
+s = "ADOBECODEBANC"
+      L       R
+window = ADOBEC
+"""
