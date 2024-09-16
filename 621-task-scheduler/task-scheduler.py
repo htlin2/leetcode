abad14084059aff3b3ac07ff240f341d412cfe8c
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-09-16 09:52:44 -0700

    Time: 511 ms (15.74%) | Memory: 17.2 MB (16.97%) - LeetSync

--- 621-task-scheduler/task-scheduler.py
+++ 621-task-scheduler/task-scheduler.py
@@ -3,22 +3,27 @@ import collections, heapq
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
         counter = collections.Counter(tasks)
-        max_heap = [-count for count in counter.values()]
+        max_heap = [(-count, task) for task, count in counter.items()]
         heapq.heapify(max_heap)
-        q = collections.deque() # (-count, cooling_time)
+        q = collections.deque([])
         time = 0
         while q or max_heap:
             time += 1
             if max_heap:
-                count = heapq.heappop(max_heap) + 1
+                count, task = heapq.heappop(max_heap)
+                count += 1
                 if count != 0:
-                    q.append((count, time + n))
-            if q and q[0][-1] == time:
-                count, _ = q.popleft()
-                heapq.heappush(max_heap, count)
+                    q.append((count, task, time + n))
+            while q and q[0][-1] == time:
+                count, task, _ = q.popleft()
+                heapq.heappush(max_heap, (count, task))
         return time
 
-
 """
-max heap + q
+Input: tasks = ["A","A","A","B","B"], n = 2
+max_heap = [(_, A), (_, B)] # count, task
+q = [(2, A, 4), (1, B, 5)] # count, task, time + n + 1
+time = 3
+A -> B -> idle ->
+1    2       3    
 """
