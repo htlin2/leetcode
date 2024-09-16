b2bafc63073db65b96968dd69caf009a4c13a640
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-09-16 16:13:02 -0700

    Time: 51 ms (67.43%) | Memory: 17.1 MB (15.75%) - LeetSync

--- 51-n-queens/n-queens.py
+++ 51-n-queens/n-queens.py
@@ -1,30 +1,61 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
-        col = set()
-        pos_diag = set() # r + c
-        neg_diag = set() # r - c
         grid = []
-        for r in range(n):
+        for i in range(n):
             row = ['.'] * n
             grid.append(row)
-        ans = []
-        def backtrack(r):
-            if r == n:
-                copy = [''.join(row) for row in grid]
-                ans.append(copy)
+
+        row = set()
+        col = set()
+        positive_diag = set() # r + c
+        negative_diag = set() # r - c
+
+        def is_valid(r, c):
+            # check row
+            if r in row: return False
+            # check col
+            if c in col: return False
+            # check diag
+            if (r + c) in positive_diag: return False
+            if (r - c) in negative_diag: return False
+            return True
+
+        res = []
+        def backtrack(c, grid):
+            if c >= n:
+                joined_grid = []
+                for grid_row in grid:
+                    joined_grid.append(''.join(grid_row))
+                res.append(joined_grid)
                 return
-            for c in range(n):
-                if c in col or (r + c) in pos_diag or (r - c) in neg_diag: continue
-                col.add(c)
-                pos_diag.add(r + c)
-                neg_diag.add(r - c)
-                grid[r][c] = 'Q'
+            for r in range(n):
+                if is_valid(r, c):
+                    grid[r][c] = 'Q'
+                    row.add(r)
+                    col.add(c)
+                    positive_diag.add(r + c)
+                    negative_diag.add(r - c)
 
-                backtrack(r + 1)
+                    backtrack(c + 1, grid)
 
-                col.remove(c)
-                pos_diag.remove(r + c)
-                neg_diag.remove(r - c)
-                grid[r][c] = '.'
-        backtrack(0)
-        return ans
+                    grid[r][c] = '.'
+                    row.remove(r)
+                    col.remove(c)
+                    positive_diag.remove(r + c)
+                    negative_diag.remove(r - c)
+        backtrack(0, grid)
+        return res
+
+"""
+
+[    0123
+   0".Q..",
+   1"...Q",
+   2"Q...",
+   3"..Q."
+]
+row:2, col: 0
+invalid (1,1) (0,2) positive_diag => (2),(2) =>  row + col
+invalid (3,1) (2,0) negative_diag => (2),(2) =>  row - col
+
+"""
