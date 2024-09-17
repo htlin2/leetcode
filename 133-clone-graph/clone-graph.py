7d6584c0675f3574e4450f98fe50b4545b0f3528
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-09-17 14:54:08 -0700

    Time: 35 ms (86.84%) | Memory: 17.1 MB (5.53%) - LeetSync

--- 133-clone-graph/clone-graph.py
+++ 133-clone-graph/clone-graph.py
@@ -9,22 +9,29 @@ class Node:
 from typing import Optional
 class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        if not node: return node
         hashmap = {None: None} # original_node: copy_node
-        def dfs(node):
-            if node in hashmap:
-                return hashmap[node]
-            copy_node = Node(node.val)
-            hashmap[node] = copy_node
-            for nei in node.neighbors:
-                nei_copy_node = dfs(nei)
-                copy_node.neighbors.append(nei_copy_node)
-            return copy_node
-        dfs(node)
+        q = collections.deque([node])
+        hashmap[node] = Node(node.val)
+        while q:
+            n = q.popleft()
+            copy_node = hashmap[n]
+            for nei in n.neighbors:
+                if not nei in hashmap:
+                    hashmap[nei] = Node(nei.val)
+                    q.append(nei)
+                copy_node.neighbors.append(hashmap[nei])
         return hashmap[node]
 
 """
 Input: adjList = [[2,4],[1,3],[2,4],[1,3]]
 Output: [[2,4],[1,3],[2,4],[1,3]]
+node: neighbors
+   1: [2,4]
+   2: [3,1]
+   3: [4,2]
+   4: [1, 3]
+
 hashmap = {
     original_node: copy_node
 }
@@ -41,10 +48,15 @@ Time: O(n)
 Space: O(n)
 
 2. bfs
-add adj nodes to q
+hashmap = {original_node: copy_node}
+q = [2, node(1), 2, node(3)]
+1: [2, 4]
 loop through q
     get or create copy_node
-    add copy_node to nei
+        add copy_node to hashmap
+    if nei:
+        make sure nei havent been visited
+        add nei to q
 Time: O(n)
 Space: O(n)
 """
