fa7b5dbfc28593a91433fb02c2b20a9182ed9809
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-09-20 22:03:57 -0700

    Time: 35 ms (61.88%) | Memory: 16.7 MB (5.83%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,29 +1,19 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         N = len(nums)
-        dp = collections.defaultdict(int)        
-        dp[N - 1] = nums[N - 1]
-        for i in range(N - 1, -1, -1):
-            skip = dp[i + 1]
-            rob = dp[i + 2] + nums[i]
-            dp[i] = max(skip, rob)
-        return dp[0]
+        memo = {} # i: total
+        def dfs(i):
+            if i >= N: return 0
+            if i in memo: return memo[i]
+            # rob
+            res = dfs(i + 2) + nums[i]
+            # skip
+            res = max(res, dfs(i + 1))
+            memo[i] = res
+            return res
+        return dfs(0)
+
 """
-input = [2,6,2,1,0], output = 7
-         - r - r
-2 choices > rob or skip
-if rob, we have to skip
+dp
 
-1: dfs + memo
-memo = {} # idx: max_money
-dfs(curr_index):
-    # skip
-    dfs(curr_index + 1)
-    # rob
-    dfs(curr_index + 2) + money
-    max (skip, rob)
-
-2: tabulation
-iterate thought nums from back to front
-    check dp[curr_index] = max(dp[curr_index - 1], dp[curr_index - 2])
 """
