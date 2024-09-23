008f743a4be49298ea975586c673f7075aa8ceb0
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-09-23 10:46:50 -0700

    Time: 1259 ms (68.64%) | Memory: 33.4 MB (73.52%) - LeetSync

--- 2832-find-the-longest-equal-subarray/find-the-longest-equal-subarray.py
+++ 2832-find-the-longest-equal-subarray/find-the-longest-equal-subarray.py
@@ -1,23 +1,24 @@
+import collections
 class Solution:
     def longestEqualSubarray(self, nums: List[int], k: int) -> int:
-        N = len(nums)
+        if not nums: return 0
         window = collections.defaultdict(int)
-        left, max_f = 0, 0
-        for right in range(N):
-            window[nums[right]] += 1
-            max_f = max(max_f, window[nums[right]])
-            while (right - left + 1) - max_f > k:
+        max_f = 0
+        left = 0
+        for right, num in enumerate(nums):
+            window[num] += 1
+            max_f = max(max_f, window[num])
+            while (right - left + 1 - max_f) > k:
                 window[nums[left]] -= 1
-                max_f = max(max_f, window[nums[left]])
                 left += 1
         return max_f
+            
 
 """
-Input: nums = [1,3,2,3,1,3], k = 3
 sliding window variable
-window= 1:1, 3:3, 2:1, 4: 1
+Input: nums = [1,3,2,3,1,3], k = 3
+counter = {1: 1`, 3: 3, 2: 1}
 max_f = 3
-delta = (r - l + 1) - max_f = 4 is < k? no
-nums = [ 1, 3, 2, 3, 1, 3, 4]
-            l              r
+[1,3,2,3,1,3,5,6]
+   l         i
 """
