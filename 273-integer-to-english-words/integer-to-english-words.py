d1c3796ce55f32d0b813362edfa41eaea6ffdd0a
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-09-24 20:20:29 -0700

    Time: 36 ms (61.61%) | Memory: 17.1 MB (5.01%) - LeetSync

--- 273-integer-to-english-words/integer-to-english-words.py
+++ 273-integer-to-english-words/integer-to-english-words.py
@@ -1,6 +1,7 @@
 class Solution:
     def numberToWords(self, num: int) -> str:
-        NUM_MAP = {
+        if not num: return 'Zero'
+        hashmap = {
             0: "Zero",
             1: "One",
             2: "Two",
@@ -32,48 +33,39 @@ class Solution:
             100: "Hundred",
             1000: "Thousand",
             1000000: "Million",
-            1000000000: "Billion"
+            1000000000: "Billion",
         }
-        if num <= 20:
-            return NUM_MAP[num]
-        if num < 100:
-            res = []
-            tenth_str = NUM_MAP[num // 10 * 10]
-            res.append(tenth_str)
-            if num % 10:
-                digit_str = NUM_MAP[num % 10]
-                res.append(digit_str)
-            return ' '.join(res)
-        if num < 1000:
-            res = []
-            before = self.numberToWords(num // 100)
-            res.extend([before, NUM_MAP[100]])
-            if num % 100:
-                after = self.numberToWords(num % 100)
-                res.append(after)
-            return ' '.join(res)
-        if num < 1000000:
-            res = []
-            before = self.numberToWords(num // 1000)
-            res.extend([before, NUM_MAP[1000]])
-            if num % 1000:
-                after = self.numberToWords(num % 1000)
-                res.append(after)
-            return ' '.join(res)
-        if num < 1000000000:
-            res = []
-            before = self.numberToWords(num // 1000000)
-            res.extend([before, NUM_MAP[1000000]])
-            if num % 1000000:
-                after = self.numberToWords(num % 1000000)
-                res.append(after)
-            return ' '.join(res)
-        else:
-            res = []
-            before = self.numberToWords(num // 1000000000)
-            res.extend([before, NUM_MAP[1000000000]])
-            if num % 1000000000:
-                after = self.numberToWords(num % 1000000000)
-                res.append(after)
-            return ' '.join(res)
-        
+        res = []
+        def dfs(num):
+            if num == 0: return ''
+            elif num <= 20:
+                res.append(hashmap[num])
+            elif num < 100:
+                tenth = hashmap[num // 10 * 10]
+                res.append(tenth)
+                dfs(num % 10)
+            elif num < 1000:
+                hundred = dfs(num // 100)
+                print(hundred, num // 100)
+                res.append(hundred)
+                res.append('Hundred')
+                dfs(num % 100)
+            elif num < 1000000:
+                thousand = dfs(num // 1000)
+                res.append(thousand)
+                res.append('Thousand')
+                dfs(num % 1000) 
+            elif num < 1000000000:
+                million = dfs(num // 1000000)
+                res.append(million)
+                res.append('Million')
+                dfs(num % 1000000)
+            else:
+                billion = dfs(num // 1000000000)
+                res.append(billion)
+                res.append('Billion')
+                dfs(num % 1000000000)
+        dfs(num)
+        res = [r for r in res if r]
+        return ' '.join(res)
+                
