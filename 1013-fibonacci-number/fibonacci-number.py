28d3acc6c6189dc7f8d36dce78e4b5debe36e6f4
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-09-26 16:16:21 -0700

    Time: 348 ms (26.05%) | Memory: 16.3 MB (89.37%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -1,28 +1,6 @@
 class Solution:
     def fib(self, n: int) -> int:
-        dp = collections.defaultdict(int)
-        dp[1] = 1
-        for i in range(2, n + 1):
-            dp[i] = dp[i - 1] + dp[i - 2]
-        return dp[n]
-"""
-1. recurrsion
-    dfs
-    base case: if n < 2: return n
-    return n - 1 + n -2
-    time: O(2 ^ n)
-    space: O(n) 
-2. recurrsion + memo
-    save {n: result}
-    add memo after base
-    time: O(n)
-    space: O(n)
-3. dp - tabulation
-    dp = []
-    time: O(n)
-    space: O(n)
-4. while loop
-    track prev, curr values
-    time:O(n)
-    space:O(1)
-"""
+        def dfs(n):
+            if n <= 1: return n
+            return dfs(n - 1) + dfs(n - 2)
+        return dfs(n)
