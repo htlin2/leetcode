0ddd2319ed965385bf9049c437d1802d563fb3e0
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-09-26 16:42:32 -0700

    Time: 2034 ms (6.01%) | Memory: 19.9 MB (95.52%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -2,49 +2,19 @@ class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         N = len(nums)
         nums.sort()
-        res = []
-        def two_sum(i, left, right):
+        res = set() # (n_i, n_j, n_k)
+        def two_sum(n_i, left, right):
             while left < right:
-                if left < right and left - 1 != i and nums[left - 1] == nums[left]:
-                    left += 1
-                    continue
-                three_sum = nums[i] + nums[left] + nums[right]
+                three_sum = n_i + nums[left] + nums[right]
                 if three_sum == 0:
-                    res.append([nums[i], nums[left], nums[right]])
+                    res.add((n_i, nums[left], nums[right]))
                 if three_sum <= 0:
                     left += 1
                 else:
                     right -= 1
-        for i in range(N - 2):
-            if i - 1 >= 0 and nums[i - 1] == nums[i]:
-                continue
-            two_sum(i, i + 1, N - 1)
-        return res
+            return 
 
-
-"""
-1. for loop + hashmap + sort
-Input: nums = [-1,0,1,2,-1,-4]
-Output: [[-1,-1,2],[-1,0,1]]
-[-4,-1,-1,-1,0,1,2]
-     i     L     R  
--4 + -1 + 2 = -3 < 0, so L++
--4 +  0 + 2 = -2 < 0, so L++
--4 +  1 + 2 = -1 < 0, so L++
-
--1 + -1 + 2 = 0 <= 0, so L++  solution [-1,-1,2]
--1 +  0 + 2 = 1  > 0, so R--
--1 +  0 + 1 = 0 <= 0, so L++  solution [-1,0,1]
-edge case:
-    handle nums[i - 1] == nums[i], skip to next
-    handle nums[L-1] == nums[L]
-    or 
-    make sure solutions are set
-Time: O(n^2)
-Space: O(n)
-
-2. brute force
-triple for loops
-Time: O(n^3)
-Space: O(n)
-"""
+        for i in range(0, N - 2):
+            n_i = nums[i]
+            two_sum(n_i, i + 1, N - 1)
+        return list(res)
