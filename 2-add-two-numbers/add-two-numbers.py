f4ebae13df0d5a690eb6954b5bcb29b8ed103105
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-09-25 22:19:22 -0700

    Time: 59 ms (29.83%) | Memory: 16.7 MB (29.98%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -4,46 +4,21 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        dummy = curr = ListNode(0)
-        add_one = 0
-        while l1 or l2:
-            total = add_one
-            if l1:
-                total += l1.val
-                l1 = l1.next
-            if l2:
-                total += l2.val
-                l2 = l2.next
-            add_one = total // 10
-            total = total % 10
-            curr.next = ListNode(total)
+    def addTwoNumbers(self, A: Optional[ListNode], B: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = curr = ListNode(-1)
+        carry = 0
+        while A or B:
+            A_val, B_val = 0, 0
+            if A:
+                A_val = A.val
+                A = A.next
+            if B:
+                B_val = B.val
+                B = B.next
+            combined = A_val + B_val + carry # 14
+            carry = combined // 10 # 14 // 10 => 1
+            curr.next = ListNode(combined % 10) # 14 % 10 => 4
             curr = curr.next
-        if add_one:
-            curr.next = ListNode(add_one)
+        if carry:
+            curr.next = ListNode(carry)
         return dummy.next
-
-"""
-l1: 1 -> 4 -> 3
-l2: 9 -> 5 -> 6
-    0    0    0   1
-
- 341
- 957
-1000
-add_one = 1
-l1: 1 -> 4 -> 3
-l2: 9 -> 5 -> 6
-    0    0    0 
-head c   c    c
-if add_one:
-    c.next = node(add_one)
-return head
-
-add_one = 1
-l1: 1 -> 4
-l2: 9 -> 5 -> 6
-    0    0
-headc    c
-c.next = l2.next
-"""
