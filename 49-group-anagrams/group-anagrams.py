7d403bac3500c28e0d66f86e4ce32c86b0d0efcb
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-09-25 21:55:57 -0700

    Time: 90 ms (56.18%) | Memory: 20.4 MB (47.03%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,25 +1,14 @@
+import collections
 class Solution:
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        hashmap = collections.defaultdict(list)
-        for word in strs:
-            sorted_word = sorted(list(word))
-            hashmap[''.join(sorted_word)].append(word)
+    def groupAnagrams(self, words: List[str]) -> List[List[str]]:
+        hashmap = collections.defaultdict(list) # sorted_key: list of word
+        for w in words:
+            key = ''.join(sorted(w))
+            hashmap[key].append(w)
         return hashmap.values()
 
 """
-1) sort + hashmap
-key: list()
-abt: [bat]
-ant: [nat, tan]
-time: n * w log w
-space: n
-
-2) predefined index + hashmap
-a: 1
-b: 2
-c: 3
-[1, 0, 0, 1]
-key = 1001
-time: n
-space: n
+hashmap + sort
+Time: O(n * w log w)
+Space: O(n)
 """
