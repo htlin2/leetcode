95b5be2cacbd969161d65c4243d78a616df9abe3
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-09-26 17:09:07 -0700

    Time: 26 ms (96.20%) | Memory: 16.5 MB (84.95%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -1,31 +1,26 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        digit_map = {
-            '1': '',
-            '2': 'abc',
-            '3': 'def',
-            '4': 'ghi',
-            '5': 'jkl',
-            '6': 'mno',
-            '7': 'pqrs',
-            '8': 'tuv',
-            '9': 'wxyz',
-            '0': '',
+        if not digits: return []
+        hashmap = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz"
         }
-        if not digits: return ''
-        res = []
         N = len(digits)
-        def backtrack(i, combos):
+        res = []
+        def dfs(i, temp):
             if i == N:
-                res.append(''.join(combos))
+                substr = ''.join(temp)
+                res.append(substr)
                 return
-            for char in digit_map[digits[i]]:
-                combos.append(char)
-                backtrack(i + 1, combos)
-                combos.pop()
-        backtrack(0, [])
+            for char in hashmap[digits[i]]:
+                temp.append(char)
+                dfs(i + 1, temp)
+                temp.pop()
+        dfs(0, [])
         return res
-
-"""
-
-"""
