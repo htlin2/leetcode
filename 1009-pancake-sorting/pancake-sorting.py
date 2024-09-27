37413560f361cef57e2a79b24c29e2f43df7039d
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-09-26 17:04:29 -0700

    Time: 42 ms (61.71%) | Memory: 16.6 MB (9.16%) - LeetSync

--- 1009-pancake-sorting/pancake-sorting.py
+++ 1009-pancake-sorting/pancake-sorting.py
@@ -1,18 +1,34 @@
 class Solution:
     def pancakeSort(self, arr: List[int]) -> List[int]:
-        def flip(end):
-            arr[:end + 1] = arr[:end + 1][::-1]
-
+        def flip(n):
+            reverse_arr = arr[:n + 1][::-1]
+            arr[:n + 1] = reverse_arr
         res = []
-        N = len(arr)
-        for i in range(N - 1, 0, -1):
-            copy_arr = arr[:i+1]
-            max_n = max(copy_arr)
-            max_i = copy_arr.index(max_n)
-            if max_i == i: continue
-            if max_i != 0:
-                flip(max_i)
-                res.append(max_i + 1)
-            flip(i)
-            res.append(i + 1)
-        return res
+        end_idx = len(arr) - 1
+        while True:
+            if end_idx == 0:
+                return res
+            max_num = max(arr[:end_idx + 1])
+            max_idx = arr.index(max_num)
+            if max_idx == end_idx:
+                end_idx -= 1
+                continue
+            flip(max_idx)
+            res.append(max_idx + 1)
+            flip(end_idx)
+            res.append(end_idx + 1)
+            end_idx -= 1
+
+"""
+Input: arr = [3,2,4,1]
+Output: [4,2,4,3]
+[3,2,4,1] k = 3
+     i
+[4,2,3,1]
+[1,3,2,4] k = 1
+   i
+[3,1,2,4]
+[2,1,3,4] k =1
+[1,2,3,4]
+
+"""
