6a21a5e02f568213d80b6a64654c301c4bd7c477
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-09-28 15:10:04 -0700

    Time: 76 ms (52.53%) | Memory: 16.9 MB (38.14%) - LeetSync

--- 332-reconstruct-itinerary/reconstruct-itinerary.py
+++ 332-reconstruct-itinerary/reconstruct-itinerary.py
@@ -1,14 +1,13 @@
-import heapq, collections
 class Solution:
     def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        tickets.sort(reverse=True)
         adj = collections.defaultdict(list)
         for src, dst in tickets:
-            heapq.heappush(adj[src], dst)
+            adj[src].append(dst)
         res = []
         def dfs(src):
             while adj[src]:
-                nei = heapq.heappop(adj[src])
-                dfs(nei)
+                dfs(adj[src].pop())
             res.append(src)
         dfs('JFK')
         return res[::-1]
