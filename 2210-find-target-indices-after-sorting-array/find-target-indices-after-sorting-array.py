e03c548d84b30aa36c00892e89dd9ad37396ff81
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-09-30 22:05:01 -0700

    Time: 46 ms (68.34%) | Memory: 16.4 MB (75.13%) - LeetSync

--- 2210-find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.py
+++ 2210-find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.py
@@ -2,22 +2,14 @@ import bisect
 
 class Solution:
     def targetIndices(self, nums: List[int], target: int) -> List[int]:
-        N = len(nums)
         nums.sort()
-        left_idx = bisect.bisect_left(nums, target)
-        if not (0 <= left_idx < N):
-            return []
-        right_idx = bisect.bisect_right(nums, target)
-        return [i for i in range(left_idx, right_idx)]
+        res = []
+        for i, n in enumerate(nums):
+            if n == target:
+                res.append(i)
+        return res
 """
-Binary Search
-weight left to find left idx
-weight right to find right idx
-slice to create the result list
-Time: O(n log n)
-Space: O(n)
-
-Brute Force
+Brute Force + sort
 loop from beginning to find left idx
 loop from end to find right idx
 Time: O(n log n)
