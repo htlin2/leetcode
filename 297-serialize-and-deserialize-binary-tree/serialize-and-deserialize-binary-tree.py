acafa1dde9f8b264c271914e0284eec1691abaf0
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-10-05 15:47:48 -0700

    Time: 78 ms (78.21%) | Memory: 20.9 MB (18.36%) - LeetSync

--- 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.py
+++ 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.py
@@ -13,19 +13,17 @@ class Codec:
         :type root: TreeNode
         :rtype: str
         """
-        if not root: return ''
         res = []
-        q = collections.deque([root])
-        while q:
-            node = q.popleft()
-            if not node:
+        def dfs(node):
+            if node:
+                res.append(str(node.val))
+            else:
                 res.append('N')
-                continue
-            res.append(str(node.val))
-            q.append(node.left)
-            q.append(node.right)
+                return
+            dfs(node.left)
+            dfs(node.right)
+        dfs(root)
         return ','.join(res)
-        
 
     def deserialize(self, data):
         """Decodes your encoded data to tree.
@@ -33,25 +31,33 @@ class Codec:
         :type data: str
         :rtype: TreeNode
         """
-        if not data: return None
-        arr = data.split(',')
-        i = 0
-        root = TreeNode(int(arr[i]))
-        q = collections.deque([root])
-        while q:
-            node = q.popleft()
-            if not node: continue
+        data_arr = data.split(',')
+        i = -1
+        def dfs():
+            nonlocal i
             i += 1
-            node.left = TreeNode(int(arr[i])) if arr[i] != 'N' else None
-            q.append(node.left)
-
-            i += 1
-            node.right = TreeNode(int(arr[i])) if arr[i] != 'N' else None
-            q.append(node.right)
-        return root
-        
+            if i >= len(data_arr): return None
+            val = data_arr[i]
+            if val == 'N': return None
+            node = TreeNode(int(val))
+            node.left = dfs()
+            node.right = dfs()
+            return node
+        return dfs()
 
 # Your Codec object will be instantiated and called as such:
 # ser = Codec()
 # deser = Codec()
 # ans = deser.deserialize(ser.serialize(root))
+
+"""
+post order dfs
+1,2,N,N,3,4,N,N,5,N,N
+         1
+       2
+    N
+      0
+    1 0*2+1    2 0*2+2
+  3 1*2+1   4 1*2+2  5 2*2+1 6 2*2+2
+7 3*2+1 8 3*2+2
+""" 
