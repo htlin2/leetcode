51d81b8a5b7c904a3b0b165483c1f8bc89b47d28
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-10-05 14:58:42 -0700

    Time: 157 ms (75.11%) | Memory: 17.8 MB (89.41%) - LeetSync

--- 581-shortest-unsorted-continuous-subarray/shortest-unsorted-continuous-subarray.py
+++ 581-shortest-unsorted-continuous-subarray/shortest-unsorted-continuous-subarray.py
@@ -1,27 +1,15 @@
 class Solution:
     def findUnsortedSubarray(self, nums: List[int]) -> int:
+        sorted_nums = sorted(nums)
         N = len(nums)
-        left, right = N - 1, 0
-        stack = [] # monotonic increasing
-        for i in range(N):
-            while stack and nums[stack[-1]] > nums[i]:
-                left = min(left, stack.pop())
-            stack.append(i)
-        stack = [] # monotonic decreasing
-        for i in range(N - 1, -1, -1):
-            while stack and nums[stack[-1]] < nums[i]:
-                right = max(right, stack.pop())
-            stack.append(i)
-        if right > left:
-            return right - left + 1
-        return 0
-"""
-Input: nums = [2,6,4,8,10,9,15]
-Output: 5
-Explanation: You need to sort [6, 4, 8, 10, 9] in ascending order to make the whole array sorted in ascending order.
+        left, right = 0, N - 1
+        while left <= right and left < N and nums[left] == sorted_nums[left]:
+            left += 1
+        while left <= right and right >= 0 and nums[right] == sorted_nums[right]:
+            right -= 1
+        if left >= right: return 0
+        return right - left + 1
 
-count + 2
-stack = [2,4,8,9] monotonic increasing
-[2,6,4,8,10,9,15]
-            l  l         
+"""
+
 """
