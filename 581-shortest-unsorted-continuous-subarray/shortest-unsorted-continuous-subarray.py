9d0670b8be6240bdef9fbb17208fc35a4cf8b8fb
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-10-05 15:14:58 -0700

    Time: 189 ms (13.09%) | Memory: 17.8 MB (56.01%) - LeetSync

--- 581-shortest-unsorted-continuous-subarray/shortest-unsorted-continuous-subarray.py
+++ 581-shortest-unsorted-continuous-subarray/shortest-unsorted-continuous-subarray.py
@@ -1,15 +1,28 @@
 class Solution:
     def findUnsortedSubarray(self, nums: List[int]) -> int:
-        sorted_nums = sorted(nums)
         N = len(nums)
-        left, right = 0, N - 1
-        while left <= right and left < N and nums[left] == sorted_nums[left]:
-            left += 1
-        while left <= right and right >= 0 and nums[right] == sorted_nums[right]:
-            right -= 1
-        if left >= right: return 0
-        return right - left + 1
-
+        left, right = N - 1, 0
+        stack = [] # mono stack increasing
+        for i in range(N):
+            while stack and nums[stack[-1]] > nums[i]:
+                left = min(left, stack.pop())
+            stack.append(i)
+        stack = [] # mono stack decreasing
+        for i in range(N - 1, -1, -1):
+            while stack and nums[stack[-1]] < nums[i]:
+                right = max(right, stack.pop())
+            stack.append(i)
+        if left < right:
+            return right - left + 1
+        return 0
 """
-
+Input: nums = [2,6,4,8,10,9,15]
+Output: 5
+monotonic stack increasing
+[2,6,4,8,10,9,15]
+ 2,6,
+left = 1
+monotonic stack decreasing
+15,9,10
+right = 9
 """
