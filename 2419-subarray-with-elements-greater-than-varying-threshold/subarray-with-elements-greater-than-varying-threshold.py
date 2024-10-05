1154c0eb078a317357a9c7faf9ef8afa9b3d0fa9
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-10-05 14:49:14 -0700

    Time: 795 ms (62.90%) | Memory: 31 MB (75.25%) - LeetSync

--- 2419-subarray-with-elements-greater-than-varying-threshold/subarray-with-elements-greater-than-varying-threshold.py
+++ 2419-subarray-with-elements-greater-than-varying-threshold/subarray-with-elements-greater-than-varying-threshold.py
@@ -4,13 +4,27 @@ class Solution:
         N = len(nums)
         stack = [] # monotonic stack increasing
         for i in range(N):
-            while stack and nums[stack[-1]] >= nums[i]:
-                left = stack.pop()
+            next_smaller = nums[i]
+            while stack and nums[stack[-1]] > next_smaller:
+                highest_idx = stack.pop()
                 k = i - stack[-1] - 1 if stack else i
-                if nums[left] > threshold / k:
+                if nums[highest_idx] > threshold / k:
                     return k
             stack.append(i)
         return -1
 """
+find the width between next_smaller and prev_smaller
+b/c the mid in this range is the highest rectangle
 
+Input: nums = [1,3,4,3,1], threshold = 6
+[1,3,4,3,1]
+ 0,1,2,3,4
+stack = 1,3
+next_smaller = nums[i] = 3
+mid = 4
+prev_smaller = nums[stack[-1]] = 3
+k = 3 - 1 + 1
+min = min(next_smaller, prev_smaller)
+if min > threshold / k:
+    return k
 """
