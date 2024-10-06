8b6ffe1b3173dc94cd40e6f452f39b82496c3d99
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-10-06 10:17:37 -0700

    Time: 60 ms (71.46%) | Memory: 17.9 MB (60.46%) - LeetSync

--- 224-basic-calculator/basic-calculator.py
+++ 224-basic-calculator/basic-calculator.py
@@ -1,9 +1,7 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        stack = []
-        digits = 0
-        curr_sum = 0
-        sign = 1
+        stack = [] # (curr_sum, sign)
+        sign, digits, curr_sum = 1, 0, 0
         for char in s:
             if char.isdigit():
                 digits = digits * 10 + int(char)
@@ -15,36 +13,31 @@ class Solution:
                 stack.append((curr_sum, sign))
                 curr_sum, sign = 0, 1
             elif char == ')':
-                curr_sum += (sign * digits)
+                curr_sum += sign * digits
                 sign, digits = 1, 0
                 prev_sum, prev_sign = stack.pop()
-                curr_sum = prev_sum + (prev_sign * curr_sum)
+                curr_sum = prev_sum + prev_sign * curr_sum
         return curr_sum + sign * digits
-
 """
-stack = [9]
-time: O(n)
-space: O(n)
+stack
+Example 2:
+Input: s = "2-1+2"
+Output: 3
+stack = [3]
+sign   = + 
+digits = 2
+starts with sign = +, stack = [0], digits = 0
+if num, add to digits
+if sign, add stack[-1] + digits, reset digits to 0, assign sign with sign
+add remaining digits stack
+sum up all stack as result
 
--(3+(4+5))
-stack = [(0, -), (3, +)]
-res = 9
-sign = 1
-digits = 0
-iterate through s:
-    if char is digit:
-        digits = digits * 10 + Number(char)
-    if char is [+, -]:
-        res += digits
-        reset digits
-        sign = -1
-    if char == (:
-        stack.push([res, sign])
-        reset sign, res
-    if char == ):
-        res += sign * digits
-        reset sign, digits
-        const [prevRes, prevSign] = stack.pop()
-        res = prevres + (prevSign * res)
-return res + curr * sign
+
+Example 3:
+Input: s = "(1+(4+5+2)-3)+(6+8)"
+Output: 23
+stack  = [0, 12] sign, num
+sign   = -
+digits = 3
+what to do with parentheses?
 """
