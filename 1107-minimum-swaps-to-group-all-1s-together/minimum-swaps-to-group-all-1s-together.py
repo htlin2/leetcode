b6458e560f1757d1a8968570a3c361820c583dea
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-10-06 15:38:43 -0700

    Time: 587 ms (32.85%) | Memory: 19.7 MB (91.88%) - LeetSync

--- 1107-minimum-swaps-to-group-all-1s-together/minimum-swaps-to-group-all-1s-together.py
+++ 1107-minimum-swaps-to-group-all-1s-together/minimum-swaps-to-group-all-1s-together.py
@@ -1,36 +1,14 @@
-import collections
 class Solution:
     def minSwaps(self, nums: List[int]) -> int:
         N = len(nums)
-        min_swap = N
-        counter = collections.Counter(nums)
-        if not 1 in counter:
-            return 0
-        window = collections.defaultdict(int)
+        window_size = sum(nums)
+        window = 0
+        max_ones = 0
         left = 0
-        for right, num in enumerate(nums):
-            window[num] += 1
-            if right - left + 1 >= counter[1]:
-                min_swap = min(min_swap, window[0])
-                window[nums[left]] -= 1
+        for right in range(N):
+            window += nums[right]
+            if right - left + 1 > window_size:
+                window -= nums[left]
                 left += 1
-        return min_swap
-"""
-quick sort / bubble sort?
-
-sliding window fixed
-Input: nums = [1,0,1,0,1]
-counter = {1:3, 0: 2}
-
-
-Input: nums = [1,0,1,0,1,0,0,1,1,0,1]
-Output: 3
-{1:6, 0:5 }
-min_swap = 3
-window = {
-    1:3
-    0:3
-}
-[1,0,1,0,1,0,0,1,1,0,1]
-     l         r
-"""
+            max_ones = max(max_ones, window)
+        return window_size - max_ones
