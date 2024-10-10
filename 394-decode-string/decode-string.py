c420f8338756e0ce73f077f7cd329e3082e67981
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-10-10 06:42:49 -0700

    Time: 37 ms (38.01%) | Memory: 16.4 MB (96.13%) - LeetSync

--- 394-decode-string/decode-string.py
+++ 394-decode-string/decode-string.py
@@ -1,45 +1,15 @@
 class Solution:
     def decodeString(self, s: str) -> str:
         stack = []
-        substr, digits = '', ''
         for char in s:
             if char == ']':
+                substr, digits = '', ''
                 while stack and stack[-1] != '[':
                     substr = stack.pop() + substr
                 stack.pop() # pop [
                 while stack and stack[-1].isdigit():
                     digits = stack.pop() + digits
                 stack.append(int(digits) * substr)
-                substr, digits = '', ''
             else:
                 stack.append(char)
-        stack.append(substr)
         return ''.join(stack)
-
-"""
-Input: s = "3[a]2[bc]"
-Output: "aaabcbc"
-stack = [(aaa, 2)]
-digits = 0
-substr = bc
-
-
-Example 2:
-Input: s = "3[a2[c]]"
-Output: "accaccacc"
-stack = [('', 3)]
-digits = 0
-substr = ''  + accaccacc
-loop through s:
-    if char is digit:
-        add to digits
-    elif char is alphbets:
-        add to substr
-    elif char == [:
-        append (substr, digits) to stack
-        reset substr, digits
-    elif char == ]:
-        stack_char, stack_digits = stack.pop()
-        substr = stack_char + substr * stack_digits
-        add substr to res
-"""
