f0ab3bcf35ac0dbf5379a19abb42680873fbd05d
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-10-10 06:52:09 -0700

    Time: 86 ms (37.06%) | Memory: 21.3 MB (25.71%) - LeetSync

--- 1320-remove-all-adjacent-duplicates-in-string-ii/remove-all-adjacent-duplicates-in-string-ii.py
+++ 1320-remove-all-adjacent-duplicates-in-string-ii/remove-all-adjacent-duplicates-in-string-ii.py
@@ -1,26 +1,30 @@
-import collections
 class Solution:
     def removeDuplicates(self, s: str, k: int) -> str:
         stack = [] # (char, count)
         for char in s:
             if stack and stack[-1][0] == char:
                 stack[-1][-1] += 1
+                if stack[-1][-1] == k:
+                    stack.pop()
             else:
                 stack.append([char, 1])
-            while stack and stack[-1][-1] == k:
-                stack.pop()
         res = ''
         for char, count in stack:
             res += char * count
         return res
+
 """
+stack
+Example 2:
+
 Input: s = "deeedbbcccbdaa", k = 3
 Output: "aa"
-Explanation: 
-First delete "eee" and "ccc", get "ddbbbdaa"
-Then delete "bbb", get "dddaa"
-Finally delete "ddd", get "aa"
+deeedbbcccbdaa
+a: 2
 
-stack + sliding window fixed
 
+Input: s = "pbbcggttciiippooaais", k = 2
+Output: "ps"
+stack
+p:1 , s
 """
