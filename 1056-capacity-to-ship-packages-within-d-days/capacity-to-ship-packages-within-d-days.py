57ff0123bcad5c08fba8d451463a16ad7d8c16ae
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-10-13 16:10:55 -0700

    Time: 394 ms (68.88%) | Memory: 22.3 MB (86.78%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.py
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.py
@@ -1,28 +1,30 @@
 class Solution:
-    def shipWithinDays(self, weights: List[int], days: int) -> int:
-        left, right = max(weights), sum(weights)
+    def shipWithinDays(self, nums: List[int], days: int) -> int:
+        left, right = max(nums), sum(nums)
         while left <= right:
             mid = (left + right) // 2
-            total_days, curr_weight = 1, 0
-            for w in weights:
-                if curr_weight + w <= mid:
-                    curr_weight += w
-                else:
+            total_days, curr_weight = 0, 0
+            for n in nums:
+                if curr_weight + n > mid:
                     total_days += 1
-                    curr_weight = w
-            if total_days <= days:
+                    curr_weight = n
+                else:
+                    curr_weight += n
+            if curr_weight:
+                total_days += 1
+            if total_days == days:
+                right = mid - 1
+            elif total_days < days:
+                # too much weight per day, decrease weight
                 right = mid - 1
             else:
+                # increase package
                 left = mid + 1
         return left
 """
-Input: weights = [1,2,3,4,5,6,7,8,9,10], days = 5
+Binary Search weight left
+Input: nums = [1,2,3,4,5,6,7,8,9,10], days = 5
 Output: 15
-binary search
-low: 1, high: 55
-mid = 10
-total_days = 3
-cal_days: 
-    1,2,3,4,5,6,7,8,9,10
-    1       2 3 4 5 6 7
+left, right = 10, sum(nums)
+
 """
