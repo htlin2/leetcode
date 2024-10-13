1151b19353356e6ec8732f8048f3e0a6814f0d5d
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-10-13 15:45:20 -0700

    Time: 597 ms (21.63%) | Memory: 19.7 MB (63.09%) - LeetSync

--- 1107-minimum-swaps-to-group-all-1s-together/minimum-swaps-to-group-all-1s-together.py
+++ 1107-minimum-swaps-to-group-all-1s-together/minimum-swaps-to-group-all-1s-together.py
@@ -1,14 +1,23 @@
 class Solution:
     def minSwaps(self, nums: List[int]) -> int:
-        N = len(nums)
-        window_size = sum(nums)
-        window = 0
-        max_ones = 0
-        left = 0
-        for right in range(N):
-            window += nums[right]
-            if right - left + 1 > window_size:
-                window -= nums[left]
+        total_ones = sum(nums)
+        curr_ones = 0
+        res, left = total_ones, 0
+        for right, num in enumerate(nums):
+            curr_ones += num
+            if right - left + 1 > total_ones:
+                curr_ones -= nums[left]
                 left += 1
-            max_ones = max(max_ones, window)
-        return window_size - max_ones
+            res = min(res, total_ones - curr_ones)
+        return res
+"""
+sliding window
+Input: data = [1,0,1,0,1]
+output: 1
+res = 2
+total_ones = 3
+curr_ones = 1
+ 1, 0, 1, 0, 1]
+l      r      
+
+"""
