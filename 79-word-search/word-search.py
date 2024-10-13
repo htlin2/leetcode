7898908ac875c2bbc68fc60be38b483a7571c5df
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-10-13 16:22:29 -0700

    Time: 4421 ms (42.27%) | Memory: 16.7 MB (38.87%) - LeetSync

--- 79-word-search/word-search.py
+++ 79-word-search/word-search.py
@@ -1,33 +1,33 @@
 class Solution:
     def exist(self, grid: List[List[str]], word: str) -> bool:
-        if not word: return True
-        ROWS, COLS = len(grid), len(grid[0])
+        ROW, COL = len(grid), len(grid[0])
+        N = len(word)
         directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
         visited = set() # (r, c)
-        def dfs(r, c, idx):
-            if idx >= len(word):
-                return True
-            if r < 0 or c < 0 or r >= ROWS or c >= COLS: 
-                return False
+        def dfs(r, c, i):
+            if i == N: return True
+            if r < 0 or c < 0 or r >= ROW or c >= COL: return False
             if (r, c) in visited: return False
-            if grid[r][c] != word[idx]: return False
+            if grid[r][c] != word[i]: return False
             visited.add((r, c))
             for dr, dc in directions:
-                if dfs(r + dr, c + dc, idx + 1):
+                if dfs(r + dr, c + dc, i + 1):
                     return True
             visited.remove((r, c))
             return False
-        
-        for r in range(ROWS):
-            for c in range(COLS):
-                if dfs(r, c, 0):
-                    return True
+        for r in range(ROW):
+            for c in range(COL):
+                if dfs(r, c, 0): return True
         return False
 """
-backtracking
-ABCCED
-[A,B,C,E],
-[S,F,C,S],
-[A,D,E,E]
+Backtracking
+Input: grid = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCCED"
+Output: true
+
+[
+    ["A","B","C","E"],
+    ["S","F","C","S"],
+    ["A","D","E","E"]
+]
 
 """
