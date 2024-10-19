efdf38fa162767d0ea813174936abed7b67bf442
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-10-19 12:14:30 -0700

    Time: 0 ms (100.00%) | Memory: 16.6 MB (43.23%) - LeetSync

--- 394-decode-string/decode-string.py
+++ 394-decode-string/decode-string.py
@@ -1,15 +1,45 @@
 class Solution:
     def decodeString(self, s: str) -> str:
-        stack = []
+        stack = [] # (chars, count)
+        digits, chars = 0, ''
         for char in s:
-            if char == ']':
-                substr, digits = '', ''
-                while stack and stack[-1] != '[':
-                    substr = stack.pop() + substr
-                stack.pop() # pop [
-                while stack and stack[-1].isdigit():
-                    digits = stack.pop() + digits
-                stack.append(int(digits) * substr)
-            else:
-                stack.append(char)
-        return ''.join(stack)
+            if char.isdigit():
+                digits = digits * 10 + int(char)
+            elif char.isalpha():
+                chars += char
+            elif char == '[':
+                stack.append((chars, digits))
+                chars, digits = '', 0
+            elif char == ']':
+                stack_chars, stack_count = stack.pop()
+                chars = stack_chars + stack_count * chars
+        return chars
+"""
+stack 
+Input: s = "3[a]2[bc]"
+Output: "aaabcbc"
+stack = [(aaa, 2)] # (characters, count)
+digits = 0
+characters = aaabcbc
+3[a]2[bc]
+        i
+
+
+Input: s = "3[a2[c]]"
+Output: "accaccacc"
+stack = [('', 3) ] # (characters, count)
+digits = 0
+chars = '' + accaccacc
+3[a2[c]]
+       i 
+if digit:
+    add to digits
+elif char:
+    add to characters
+elif [:
+    stack.append((characters, digits))
+    reset characters, digits
+elif ]:
+    stack_char, stack_digits = pop stack
+    chars = stack_char + stack_digits * chars
+"""
