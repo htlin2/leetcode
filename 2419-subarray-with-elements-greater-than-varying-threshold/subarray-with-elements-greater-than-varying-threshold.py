bd42f65161862c4ae12137276096bbab9d90d50d
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-10-19 13:57:40 -0700

    Time: 119 ms (99.76%) | Memory: 31.2 MB (49.70%) - LeetSync

--- 2419-subarray-with-elements-greater-than-varying-threshold/subarray-with-elements-greater-than-varying-threshold.py
+++ 2419-subarray-with-elements-greater-than-varying-threshold/subarray-with-elements-greater-than-varying-threshold.py
@@ -1,30 +1,23 @@
 class Solution:
     def validSubarraySize(self, nums: List[int], threshold: int) -> int:
-        nums.append(0)
-        N = len(nums)
         stack = [] # monotonic stack increasing
-        for i in range(N):
-            next_smaller = nums[i]
-            while stack and nums[stack[-1]] > next_smaller:
-                highest_idx = stack.pop()
+        nums.append(0)
+        for i in range(len(nums)):
+            while stack and nums[stack[-1]] > nums[i]:
+                left = stack.pop()
                 k = i - stack[-1] - 1 if stack else i
-                if nums[highest_idx] > threshold / k:
+                if nums[left] > threshold / k:
                     return k
             stack.append(i)
         return -1
 """
-find the width between next_smaller and prev_smaller
-b/c the mid in this range is the highest rectangle
-
+monotonic stack increasing
 Input: nums = [1,3,4,3,1], threshold = 6
-[1,3,4,3,1]
- 0,1,2,3,4
-stack = 1,3
-next_smaller = nums[i] = 3
-mid = 4
-prev_smaller = nums[stack[-1]] = 3
-k = 3 - 1 + 1
-min = min(next_smaller, prev_smaller)
-if min > threshold / k:
-    return k
+output: 3 -> 3,4,3
+stack = [1,3,3,1]
+
+
+num 1, 3, 4, 3, 1, 0
+idx 0, 1, 2, 3, 4, 5
+
 """
