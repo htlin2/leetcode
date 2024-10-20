2b6254fd62da9f4191abb5454434c44fc85fe3c8
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-10-20 11:30:07 -0700

    Time: 186 ms (99.50%) | Memory: 21.2 MB (99.72%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.py
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.py
@@ -3,14 +3,14 @@ class Solution:
         left, right = max(nums), sum(nums)
         while left <= right:
             mid = (left + right) // 2
-            total_days, curr_num = 0, 0
+            total_days, curr_weight = 0, 0
             for n in nums:
-                if curr_num + n > mid:
-                    curr_num = n
+                if curr_weight + n > mid:
+                    curr_weight = n
                     total_days += 1
                 else:
-                    curr_num += n
-            if curr_num:
+                    curr_weight += n
+            if curr_weight:
                 total_days += 1
             if total_days == days:
                 right = mid - 1
@@ -18,5 +18,6 @@ class Solution:
                 # too fast, decrease load
                 right = mid - 1
             else:
+                # too slow, increase load
                 left = mid + 1
         return left
