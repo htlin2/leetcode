5aebdec57c0002efd70638279a499b31a06a8152
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-10-20 11:28:33 -0700

    Time: 187 ms (99.50%) | Memory: 21.2 MB (99.16%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.py
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.py
@@ -3,26 +3,20 @@ class Solution:
         left, right = max(nums), sum(nums)
         while left <= right:
             mid = (left + right) // 2
-            total_days, curr_weight = 0, 0
+            total_days, curr_num = 0, 0
             for n in nums:
-                if curr_weight + n > mid:
+                if curr_num + n > mid:
+                    curr_num = n
                     total_days += 1
-                    curr_weight = 0
-                curr_weight += n
-            total_days += 1 if curr_weight else 0
+                else:
+                    curr_num += n
+            if curr_num:
+                total_days += 1
             if total_days == days:
                 right = mid - 1
             elif total_days < days:
-                # too much weight per day, decrease weight
+                # too fast, decrease load
                 right = mid - 1
             else:
-                # increase package
                 left = mid + 1
         return left
-"""
-Binary Search weight left
-Input: nums = [1,2,3,4,5,6,7,8,9,10], days = 5
-Output: 15
-left, right = 10, sum(nums)
-
-"""
