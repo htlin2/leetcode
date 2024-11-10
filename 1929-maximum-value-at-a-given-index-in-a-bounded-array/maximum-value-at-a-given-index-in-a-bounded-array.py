a9498f69bd7d041413cd0f6d2b49972fedef5fff
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-11-09 17:33:59 -0800

    Time: 0 ms (100.00%) | Memory: 16.7 MB (37.87%) - LeetSync

--- 1929-maximum-value-at-a-given-index-in-a-bounded-array/maximum-value-at-a-given-index-in-a-bounded-array.py
+++ 1929-maximum-value-at-a-given-index-in-a-bounded-array/maximum-value-at-a-given-index-in-a-bounded-array.py
@@ -1,43 +1,43 @@
 class Solution:
     def maxValue(self, n: int, index: int, maxSum: int) -> int:
-        res = 1
-        left, right = 0, maxSum
-        
-        def sequence(mid):
+        def is_valid(mid):
+            # sum left side
             total = 0
-            
-            # Can fit sequence on left side
             if mid <= index:
-                total += mid * (mid + 1) // 2
+                total += (mid + 1) * mid // 2
                 left_of_sequence = index - mid
                 total += left_of_sequence + 1
-            # Can't fit sequence on left side
             else:
-                total += mid * (mid + 1) // 2
+                total += (mid + 1) * mid // 2
                 left_of_sequence = mid - index - 1
                 total -= left_of_sequence * (left_of_sequence + 1) // 2
-            
-            end = n - 1
-            # Can fit sequence on right side
-            if (end - index + 1) >= mid:
-                total += (mid) * (mid + 1) // 2
+            # sum right side
+            if (n - index) >= mid:
+                total += (mid + 1) * mid // 2
                 right_of_sequence = index + mid
-                total += end - right_of_sequence + 1
-            # Can't fit sequence on right side
+                total += n - right_of_sequence
             else:
-                total += (mid) * (mid + 1) // 2
-                right_of_sequence = mid - (end + 1 - index)
-                total -= (right_of_sequence) * (right_of_sequence + 1) // 2
-            
-            total -= mid
-            return total
-        
+                total += (mid + 1) * mid // 2
+                right_of_sequence = mid - (n - index)
+                total -= right_of_sequence * (right_of_sequence + 1) // 2
+            return (total - mid) <= maxSum
+        left, right = 0, maxSum
+        res = 1
         while left <= right:
-            mid = (right + left) // 2
-            if sequence(mid) <= maxSum:
+            mid = (left + right) // 2
+            if is_valid(mid):
                 res = max(res, mid)
                 left = mid + 1
             else:
                 right = mid - 1
-        
         return res
+"""
+index = 3
+1 1 2 3 2 1 1
+0 1 2 3 4 5 6
+
+2 3 4 5
+0 1 2 3 4 5
+
+
+"""
