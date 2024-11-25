58065f7847c78093e1e71a5d31eeeac2475ae6ff
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-11-24 16:52:07 -0800

    Time: 1303 ms (13.47%) | Memory: 201.9 MB (16.51%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.py
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -1,33 +1,19 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        memo = {} # (i, j): lcs
-        def dfs(i, j):
-            if i >= len(text1) or j >= len(text2):
-                return 0
-            if (i, j) in memo: return memo[i, j]
-            res = 0
-            if text1[i] == text2[j]:
-                res = dfs(i + 1, j + 1) + 1
-            else:
-                res = max(dfs(i + 1, j), dfs(i, j + 1))
-            memo[i, j] = res
-            return res
-        return dfs(0, 0)
-
-"""
-Input: text1 = "abcde", text2 = "ace" 
-
-output: ace
-mono stack? 
-stack = ace
-max_sub = 1
-
-Input: text1 = "xabcde", text2 = "acfe" 
-                012345
-output: ace
-stack = 1
-
-dp
-skip text1 or skip text2
-find longest
-"""
+        dp = collections.defaultdict(int)
+        for i in range(len(text1) - 1, -1, -1):
+            for j in range(len(text2) - 1, -1, -1):
+                if text1[i] == text2[j]:
+                    dp[i, j] = dp[i + 1, j + 1] + 1
+                else:
+                    dp[i, j] = max(dp[i + 1, j], dp[i, j + 1])
+        return dp[0, 0]
+        # def dfs(i, j):
+        #     if (i, j) in memo: return memo[(i, j)]
+        #     if i == len(text1) or j == len(text2): return 0
+        #     if text1[i] == text2[j]:
+        #         memo[(i, j)] = dfs(i + 1, j + 1) + 1
+        #     else:
+        #         memo[(i, j)] = max(dfs(i + 1, j), dfs(i, j + 1))
+        #     return memo[(i, j)]
+        # return dfs(0, 0)
