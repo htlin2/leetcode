c6f93475275cdd651f0f37f3aba9d393bd799ed0
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-11-24 16:52:52 -0800

    Time: 1279 ms (13.81%) | Memory: 202 MB (16.51%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.py
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -8,12 +8,3 @@ class Solution:
                 else:
                     dp[i, j] = max(dp[i + 1, j], dp[i, j + 1])
         return dp[0, 0]
-        # def dfs(i, j):
-        #     if (i, j) in memo: return memo[(i, j)]
-        #     if i == len(text1) or j == len(text2): return 0
-        #     if text1[i] == text2[j]:
-        #         memo[(i, j)] = dfs(i + 1, j + 1) + 1
-        #     else:
-        #         memo[(i, j)] = max(dfs(i + 1, j), dfs(i, j + 1))
-        #     return memo[(i, j)]
-        # return dfs(0, 0)
