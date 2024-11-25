66bbe0d89a01cf49c11130639168c0476fd42a68
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-11-24 16:35:11 -0800

    Time: 1405 ms (11.35%) | Memory: 309.3 MB (8.68%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.py
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -1,18 +1,33 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        # case 2 tabulation
-        prev_dp = collections.defaultdict(int)
-        # iterate r through text2
-        for r in range(len(text2)):
-            # iterate c through text1
-            dp = collections.defaultdict(int)
-            for c in range(len(text1)):
-                if text1[c] == text2[r]:
-                    dp[c] = prev_dp[c - 1] + 1
-                else:
-                    dp[c] = max(prev_dp[c], dp[c - 1])
-            prev_dp = dp
-        return dp[len(text1) - 1]
-        # time: O(n + m)
-        # space:O(n)
-        
+        memo = {} # (i, j): lcs
+        def dfs(i, j):
+            if i >= len(text1) or j >= len(text2):
+                return 0
+            if (i, j) in memo: return memo[i, j]
+            res = 0
+            if text1[i] == text2[j]:
+                res = dfs(i + 1, j + 1) + 1
+            else:
+                res = max(dfs(i + 1, j), dfs(i, j + 1))
+            memo[i, j] = res
+            return res
+        return dfs(0, 0)
+
+"""
+Input: text1 = "abcde", text2 = "ace" 
+
+output: ace
+mono stack? 
+stack = ace
+max_sub = 1
+
+Input: text1 = "xabcde", text2 = "acfe" 
+                012345
+output: ace
+stack = 1
+
+dp
+skip text1 or skip text2
+find longest
+"""
