49861532a2dcd7f03c4209c9e619b5f6af6ebbb9
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-11-29 13:03:09 -0800

    Time: 88 ms (61.53%) | Memory: 22.7 MB (15.98%) - LeetSync

--- 127-word-ladder/word-ladder.py
+++ 127-word-ladder/word-ladder.py
@@ -1,57 +1,26 @@
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        if endWord not in wordList:
-            return 0
+        if endWord not in wordList: return 0
         adj = collections.defaultdict(set)
-        for word in wordList:
-            for i in range(len(word)):
-                star_word = word[:i] + '*' + word[i+1:]
-                adj[star_word].add(word)
+        for w in wordList:
+            for i in range(len(w)):
+                star_word = list(w)
+                star_word[i] = '*'
+                adj[''.join(star_word)].add(w)
+
+        min_heap = [] # (count, word)
+        heapq.heappush(min_heap, (0, beginWord))
         visited = set()
-        res = 0
-        q = collections.deque([beginWord])
-        while q:
-            res += 1
-            for _ in range(len(q)):
-                first = q.popleft()
-                if first in visited: continue
-                visited.add(first)
-                if first == endWord:
-                    return res
-                for i in range(len(first)):
-                    star_word = first[:i] + '*' + first[i+1:]
-                    for nei in adj[star_word]:
-                        q.append(nei)
+        while min_heap:
+            count, w = heapq.heappop(min_heap)
+            if w in visited: continue
+            visited.add(w)
+            count += 1
+            if w == endWord: return count
+            for j in range(len(w)):
+                key = list(w)
+                key[j] = '*'
+                for nei in adj[''.join(key)]:
+                    if nei in visited: continue
+                    heapq.heappush(min_heap, (count, nei))
         return 0
-"""
-BFS + hashmap
-Input: beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log","cog"]
-Output: 5 
-"hit" -> "hot" -> "dot" -> "dog" -> cog"
-ho*: hot
-h*t: hot
-*ot: hot, dot, lot
-do*: dot, dog
-d*t: dot
-d*g: dog
-*og: dog, log, cog
-
-visited = hot
-count = 3
-q = [dot, lot]
-
-{
-    '*ot': {'dot', 'hot', 'lot'}, 
-    'h*t': {'hot'}, 
-    'ho*': {'hot'}, 
-    'd*t': {'dot'}, 
-    'do*': {'dot', 'dog'}, 
-    '*og': {'log', 'cog', 'dog'}, 
-    'd*g': {'dog'}, 
-    'l*t': {'lot'}, 
-    'lo*': {'log', 'lot'}, 
-    'l*g': {'log'}, 
-    'c*g': {'cog'}, 
-    'co*': {'cog'}
-}
-"""
