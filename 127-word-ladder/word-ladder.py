bbfca120ffd9fb9ccb87d1ec7eda9fd0714aa6b8
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-11-29 12:51:26 -0800

    Time: 67 ms (71.02%) | Memory: 22.5 MB (16.51%) - LeetSync

--- 127-word-ladder/word-ladder.py
+++ 127-word-ladder/word-ladder.py
@@ -1,48 +1,57 @@
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        if endWord not in wordList: return 0
-        adj = collections.defaultdict(set) # word: [word1, word2]
-        for w in wordList:
-            for i in range(len(w)):
-                key = list(w)
-                key[i] = '*'
-                adj[''.join(key)].add(w)
-
+        if endWord not in wordList:
+            return 0
+        adj = collections.defaultdict(set)
+        for word in wordList:
+            for i in range(len(word)):
+                star_word = word[:i] + '*' + word[i+1:]
+                adj[star_word].add(word)
         visited = set()
-        min_heap = [[0, beginWord]]
-        while min_heap:
-            count, w = heapq.heappop(min_heap)
-            visited.add(w)
-            count += 1
-            if w == endWord: return count
-            for i in range(len(w)):
-                key = list(w)
-                key[i] = '*'
-                for nei in adj[''.join(key)]:
-                    if nei in visited: continue
-                    heapq.heappush(min_heap, [count, nei])
+        res = 0
+        q = collections.deque([beginWord])
+        while q:
+            res += 1
+            for _ in range(len(q)):
+                first = q.popleft()
+                if first in visited: continue
+                visited.add(first)
+                if first == endWord:
+                    return res
+                for i in range(len(first)):
+                    star_word = first[:i] + '*' + first[i+1:]
+                    for nei in adj[star_word]:
+                        q.append(nei)
         return 0
-
-
 """
-hashmap + graph (bfs)
-Input: beginWord = "hit", endWord = "cog"
-["hot","dot","dog","lot","log","cog"]
-hit > hot > dot > dog > cog
+BFS + hashmap
+Input: beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log","cog"]
+Output: 5 
+"hit" -> "hot" -> "dot" -> "dog" -> cog"
+ho*: hot
+h*t: hot
+*ot: hot, dot, lot
+do*: dot, dog
+d*t: dot
+d*g: dog
+*og: dog, log, cog
 
-build adj
-*it = []
-h*t = [hot]
-hi* = []
+visited = hot
+count = 3
+q = [dot, lot]
 
-*ot = [dot, lot]
-h*t = []
-ho* = []
-
-*ot = []
-d*t
-do* = [dog]
-
-*og = [cog]
-
+{
+    '*ot': {'dot', 'hot', 'lot'}, 
+    'h*t': {'hot'}, 
+    'ho*': {'hot'}, 
+    'd*t': {'dot'}, 
+    'do*': {'dot', 'dog'}, 
+    '*og': {'log', 'cog', 'dog'}, 
+    'd*g': {'dog'}, 
+    'l*t': {'lot'}, 
+    'lo*': {'log', 'lot'}, 
+    'l*g': {'log'}, 
+    'c*g': {'cog'}, 
+    'co*': {'cog'}
+}
 """
