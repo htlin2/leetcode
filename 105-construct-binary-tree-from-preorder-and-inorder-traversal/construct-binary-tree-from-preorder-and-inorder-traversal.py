39dd7beccbb75975d5d7b5c9484473ff9459be29
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-12-07 09:11:29 -0800

    Time: 103 ms (28.71%) | Memory: 88.9 MB (7.38%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -6,23 +6,37 @@
 #         self.right = right
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        if not preorder or not inorder: return None
-        root = TreeNode(preorder[0])
-        mid = inorder.index(preorder[0])
-        root.left = self.buildTree(preorder[1:mid+1], inorder[:mid])
-        root.right = self.buildTree(preorder[mid+1:], inorder[mid+1:])
-        return root
-
+        if not preorder: return None
+        node = TreeNode(preorder[0])
+        idx = inorder.index(node.val)
+        node.left = self.buildTree(preorder[1:idx + 1], inorder[:idx])
+        node.right = self.buildTree(preorder[idx + 1:], inorder[idx + 1:])
+        return node
 
 """
+Input: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+Output: [3,9,20,null,null,15,7]
 preorder = [3,9,20,15,7]
-inorder =  [9,3,15,20,7]
-   3
- 9   20
-15 7
-root = 3
-root.left = [9,] -> from inorder
-root.right = [20, 15, 7] -> from preorder
+            n l  n
+inorder = [9,3,15,20,7]
+           l n r
+
+         3
+        / \
+       9   20
+      / \  / \ 
+     1  2 15 7
+out: [3,9,20,1,2,15,7]
+preorder = [3,9,1,2,20,15,7]
+            n n 
+inorder =  [1,9,2,3,15,20,7]
+              n   n
+def dfs(preorder, inorder):
+    node = preorder[0]
+    idx = find node.val in inorder
+    node.left = dfs([1:4 idx + 1](9-2), [0:3 idx](1-2))
+    node.right = dfs([], [idx+1:])
+    return node
 
 
 """
