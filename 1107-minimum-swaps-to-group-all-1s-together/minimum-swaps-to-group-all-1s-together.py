2770f8469ad237e711b0c5b3a932140c59e16f7b
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-12-08 19:39:11 -0800

    Time: 88 ms (33.37%) | Memory: 21.2 MB (11.93%) - LeetSync

--- 1107-minimum-swaps-to-group-all-1s-together/minimum-swaps-to-group-all-1s-together.py
+++ 1107-minimum-swaps-to-group-all-1s-together/minimum-swaps-to-group-all-1s-together.py
@@ -1,23 +1,38 @@
 class Solution:
     def minSwaps(self, nums: List[int]) -> int:
-        total_ones = sum(nums)
-        curr_ones = 0
-        res, left = total_ones, 0
-        for right, num in enumerate(nums):
-            curr_ones += num
-            if right - left + 1 > total_ones:
-                curr_ones -= nums[left]
+        N = len(nums)
+        window_size = sum(nums)
+        window = 0
+        left = 0
+        max_ones = 0
+        for right in range(N):
+            window += nums[right]
+            if right - left + 1 > window_size:
+                window -= nums[left]
                 left += 1
-            res = min(res, total_ones - curr_ones)
-        return res
+            max_ones = max(max_ones, window)
+        return window_size - max_ones
+
 """
-sliding window
 Input: data = [1,0,1,0,1]
-output: 1
+Output: 1
+
+
+Example 2:
+Input: data = [0,0,0,1,0]
+Output: 0
+
+
+Input: data = [1,0,1,0,1,0,0,1,1,0,1]
+               5   4   3     2 1
+Output: 3
+ones = 6
+ans = 4
+
+Input: data = [1,0,1,1,0,0,1,0,1,0,1]
+               5   4 3     2   1
+Output: 3
+ones = 6
 res = 2
-total_ones = 3
-curr_ones = 1
- 1, 0, 1, 0, 1]
-l      r      
 
 """
