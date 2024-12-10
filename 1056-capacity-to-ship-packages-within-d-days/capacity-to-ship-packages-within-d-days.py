e6258bec6a0ca229bcb8a724a0b8d31f9134877f
Tim Lin <28332496+htlin2@users.noreply.github.com>
2024-12-09 17:42:35 -0800

    Time: 185 ms (60.42%) | Memory: 21.7 MB (20.20%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.py
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.py
@@ -1,23 +1,38 @@
 class Solution:
     def shipWithinDays(self, nums: List[int], days: int) -> int:
+        def get_count(mid):
+            count = 0
+            curr_total = 0
+            for num in nums:
+                if curr_total + num > mid:
+                    count += 1
+                    curr_total = num
+                    continue
+                curr_total += num
+            if curr_total:
+                count += 1
+            return count
         left, right = max(nums), sum(nums)
         while left <= right:
             mid = (left + right) // 2
-            total_days, curr_weight = 0, 0
-            for n in nums:
-                if curr_weight + n > mid:
-                    curr_weight = n
-                    total_days += 1
-                else:
-                    curr_weight += n
-            if curr_weight:
-                total_days += 1
-            if total_days == days:
+            count = get_count(mid)
+            if count == days:
                 right = mid - 1
-            elif total_days < days:
-                # too fast, decrease load
+            elif count < days:
+                # too fast, decrease mid
                 right = mid - 1
             else:
-                # too slow, increase load
+                # too slow, increase mid
                 left = mid + 1
         return left
+"""
+binary search weight left
+Input: weights = [1,2,3,4,5,6,7,8,9,10], days = 5
+Output: 15
+
+
+
+Input: weights = [3,2,2,4,1,4], days = 3
+Output: 6
+
+"""
