0c58ea34eaf0df2f641bd27c29372ec34d72cf78
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-01-01 15:39:25 -0800

    Time: 4 ms (77.55%) | Memory: 18.7 MB (16.72%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.py
+++ 735-asteroid-collision/asteroid-collision.py
@@ -1,30 +1,15 @@
 class Solution:
     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
         stack = []
-        is_same = False
         for a in asteroids:
-            while stack and stack[-1] >= 0 and a <= 0:
-                last = stack.pop()
-                if last == abs(a):
-                    is_same = True
+            while stack and stack[-1] > 0 and a < 0:
+                if stack[-1] == abs(a):
+                    stack.pop()
                     break
-                if last > abs(a):
-                    a = last
+                elif stack[-1] > abs(a):
+                    break
                 else:
-                    a = a
-            if is_same:
-                is_same = False
-                continue
-            stack.append(a)
+                    stack.pop()
+            else:
+                stack.append(a)
         return stack
-
-"""
-stack
-stack = [10,3]
-Input: asteroids = [10,2,-5,3,-2]
-Output: [10,3]
-
-Time: O(n)
-Space: O(n)
-
-"""
