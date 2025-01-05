701e770b82d757b00a05faaa65d5061188fbc1a8
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-01-05 14:48:21 -0800

    Time: 0 ms (100.00%) | Memory: 18 MB (10.84%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -3,31 +3,30 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # quick sort
-        # merge sort
-        def merge(left, right):
-            res = []
-            i, j = 0, 0
-            while i < len(left) and j < len(right):
-                if left[i] <= right[j]:
-                    res.append(left[i])
-                    i += 1
-                else:
-                    res.append(right[j])
-                    j += 1
-            while i < len(left):
-                res.append(left[i])
-                i += 1
-            while j < len(right):
-                res.append(right[j])
-                j += 1
-            return res
-        
         def split(nums):
             if len(nums) <= 1: return nums
-            m = (len(nums) - 1) // 2
-            left = split(nums[:m+1])
-            right = split(nums[m+1:])
-            return merge(left, right)
-        sorted_nums = split(nums)
-        nums[:] = sorted_nums
+            mid = (len(nums) - 1) // 2
+            A = split(nums[0:mid + 1])
+            B = split(nums[mid + 1:])
+            return merge(A, B)
+        def merge(A, B):
+            i, j = 0, 0
+            res = []
+            while i < len(A) and j < len(B):
+                if A[i] <= B[j]:
+                    res.append(A[i])
+                    i += 1
+                else:
+                    res.append(B[j])
+                    j += 1
+            if i < len(A):
+                res += A[i:]
+            if j < len(B):
+                res += B[j:]
+            return res
+        merge_sort = split(nums)
+        nums[:] = merge_sort
+"""
+merge sort
+quick sort
+"""
