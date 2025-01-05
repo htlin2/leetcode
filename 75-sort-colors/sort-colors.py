fa9ff96c9a95e3129e1559962f8c34df1626f4d9
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-01-05 15:12:43 -0800

    Time: 3 ms (14.68%) | Memory: 17.8 MB (10.84%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -3,30 +3,25 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        def split(nums):
-            if len(nums) <= 1: return nums
-            mid = (len(nums) - 1) // 2
-            A = split(nums[0:mid + 1])
-            B = split(nums[mid + 1:])
-            return merge(A, B)
-        def merge(A, B):
-            i, j = 0, 0
-            res = []
-            while i < len(A) and j < len(B):
-                if A[i] <= B[j]:
-                    res.append(A[i])
-                    i += 1
-                else:
-                    res.append(B[j])
-                    j += 1
-            if i < len(A):
-                res += A[i:]
-            if j < len(B):
-                res += B[j:]
-            return res
-        merge_sort = split(nums)
-        nums[:] = merge_sort
+        def partition(start, end):
+            pi = nums[end]
+            left = start
+            for right in range(start, end):
+                if nums[right] > pi:
+                    continue
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1
+            nums[left], nums[end] = nums[end], nums[left]
+            return left
+        def qs(start, end):
+            if start <= end:
+                pi = partition(start, end)
+                qs(start, pi - 1)
+                qs(pi + 1, end)
+        qs(0, len(nums) - 1)
+ 
 """
 merge sort
 quick sort
+        
 """
