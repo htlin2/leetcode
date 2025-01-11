98450211b488f2cee1323ae4a947db0186141ce4
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-01-11 13:58:43 -0800

    Time: 0 ms (100.00%) | Memory: 17.9 MB (15.10%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -4,24 +4,23 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         def partition(start, end):
-            pi = nums[end]
+            pivot = nums[end]
             left = start
             for right in range(start, end):
-                if nums[right] > pi:
+                if nums[right] >= pivot:
                     continue
                 nums[left], nums[right] = nums[right], nums[left]
                 left += 1
             nums[left], nums[end] = nums[end], nums[left]
             return left
+
         def qs(start, end):
-            if start <= end:
+            if start < end:
                 pi = partition(start, end)
                 qs(start, pi - 1)
                 qs(pi + 1, end)
         qs(0, len(nums) - 1)
- 
+
 """
-merge sort
-quick sort
-        
+quick sort find pivot
 """
