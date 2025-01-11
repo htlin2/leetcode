5b19c1faeca7e9ac6969e489d80724a1ec2294be
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-01-11 14:31:08 -0800

    Time: 116 ms (32.03%) | Memory: 18 MB (37.57%) - LeetSync

--- 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.py
+++ 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.py
@@ -1,29 +1,18 @@
 class Solution:
     def longestSubstring(self, s: str, k: int) -> int:
+        max_uniq_chars = len(set(s))
         res = 0
-        uniq_count = len(set(s))
-        N = len(s)
-        for target_uniq in range(1, uniq_count + 1):
+        for uniq_chars in range(1, max_uniq_chars + 1):
             left = 0
-            freq = collections.defaultdict(int)
-            curr_uniq = 0
-            count_at_least_k = 0
-
-            for right in range(N):
-                char = s[right]
-                if freq[char] == 0:
-                    curr_uniq += 1
-                freq[char] += 1
-                if freq[char] == k:
-                    count_at_least_k += 1
-                
-                while curr_uniq > target_uniq:
-                    if freq[s[left]] == k:
-                        count_at_least_k -= 1
-                    freq[s[left]] -= 1
-                    if freq[s[left]] == 0:
-                        curr_uniq -= 1
+            window = collections.defaultdict(int)
+            for right in range(len(s)):
+                window[s[right]] += 1
+                while len(window) > uniq_chars:
+                    window[s[left]] -= 1
+                    if window[s[left]] == 0:
+                        del window[s[left]]
                     left += 1
-                if curr_uniq == count_at_least_k:
-                    res = max(res, right - left + 1)
+                is_valid = all([True if v >= k else False for v in window.values()])
+                if is_valid:
+                    res = max(res, sum(window.values()))
         return res
