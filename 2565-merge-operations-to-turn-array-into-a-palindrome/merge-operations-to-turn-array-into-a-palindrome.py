e4bec8ca309421502c6cd421d2ffdbfe0afc6171
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-01-12 17:49:13 -0800

    Time: 72 ms (68.17%) | Memory: 31.3 MB (14.75%) - LeetSync

--- 2565-merge-operations-to-turn-array-into-a-palindrome/merge-operations-to-turn-array-into-a-palindrome.py
+++ 2565-merge-operations-to-turn-array-into-a-palindrome/merge-operations-to-turn-array-into-a-palindrome.py
@@ -1,35 +1,17 @@
 class Solution:
     def minimumOperations(self, nums: List[int]) -> int:
         res = 0
-        left, right = -1, len(nums)
-        left_sum = right_sum = 0
+        left, right = 0, len(nums) - 1
         while left < right:
-            if left_sum == right_sum:
+            if nums[left] == nums[right]:
                 left += 1
                 right -= 1
-                left_sum += nums[left]
-                right_sum += nums[right]
-                continue
-            res += 1
-            if left_sum > right_sum:
+            elif nums[left] > nums[right]:
+                nums[right - 1] += nums[right]
                 right -= 1
-                right_sum += nums[right]
+                res += 1
             else:
+                nums[left + 1] += nums[left]
                 left += 1
-                left_sum += nums[left]
+                res += 1
         return res
-"""
-dp - dfs + memo
-two pointers
-Input: nums = [4,3,2,1,2,3,1]
-Output: 2
-1) [4,3,2,3,3,1]
-2) [4,3,2,3,4]
-
-Input: nums = [1,2,3,4]
-Output: 3
-1) [3,3,4]
-2) [6,4]
-3) [10]
-
-"""
