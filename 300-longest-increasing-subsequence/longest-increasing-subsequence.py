7337bebdddd0cb8283077d5f00b813ac9a5abe10
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-01-12 16:20:08 -0800

    Time: 1 ms (97.95%) | Memory: 18.1 MB (21.76%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.py
@@ -1,34 +1,10 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        if not nums: return 0
-        dp = collections.defaultdict(lambda: 1)
-        for i in range(len(nums) - 1, -1, -1):
-            for j in range(i + 1, len(nums)):
-                if nums[i] < nums[j]:
-                    dp[i] = max(dp[j] + 1, dp[i])
-        res = 1
-        for v in dp.values():
-            res = max(res, v)
-        return res
-"""
-monotonic stack increasing + sliding window condition
-dfs
-
-Input: nums = [10,9,2,5,3,7,101,18]
-Output: 4
-[2,3,7,101,18]
-
-Input: nums = [0,1,0,3,2,3]
-Output: 4
-[0,2,3]
-[0,1,2,3]
-[0,1,2,3]
-
-Input: nums = [7,7,7,7,7,7,7]
-Output: 1
-[7]
-
-nums = [1,3,6,7,9,4,10,5,6]
-output = 6
-[1,3,6,7,9,10]
-"""
+        res = []
+        for num in nums:
+            if not res or res[-1] < num:
+                res.append(num)
+                continue
+            idx = bisect.bisect_left(res, num)
+            res[idx] = num
+        return len(res)
