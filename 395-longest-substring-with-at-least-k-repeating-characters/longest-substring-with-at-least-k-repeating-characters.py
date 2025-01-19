b1830f8931188a7c1d1840853e507b16b55152ab
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-01-19 15:45:49 -0800

    Time: 115 ms (31.38%) | Memory: 17.9 MB (39.97%) - LeetSync

--- 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.py
+++ 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.py
@@ -1,18 +1,22 @@
 class Solution:
     def longestSubstring(self, s: str, k: int) -> int:
-        max_uniq_chars = len(set(s))
         res = 0
-        for uniq_chars in range(1, max_uniq_chars + 1):
-            left = 0
+        for i in range(1, len(set(s)) + 1):
             window = collections.defaultdict(int)
-            for right in range(len(s)):
-                window[s[right]] += 1
-                while len(window) > uniq_chars:
+            left = 0
+            for right, char in enumerate(s):
+                window[char] += 1
+                while len(window) > i:
                     window[s[left]] -= 1
                     if window[s[left]] == 0:
                         del window[s[left]]
                     left += 1
                 is_valid = all([True if v >= k else False for v in window.values()])
                 if is_valid:
-                    res = max(res, sum(window.values()))
+                    res = max(res, right - left + 1)
         return res
+
+"""
+brute force
+
+"""
