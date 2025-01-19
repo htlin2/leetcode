2c59f5ad5c9a65d21fe7135709cd763a857e5142
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-01-18 16:01:40 -0800

    Time: 1221 ms (53.90%) | Memory: 124.4 MB (37.20%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.py
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.py
@@ -1,22 +1,36 @@
-import collections, heapq
-
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
-        adj = collections.defaultdict(list) # src: [(weight, dst)]
-        N = len(points)
-        for i in range(N):
-            for j in range(i + 1, N):
-                weight = abs(points[i][0] - points[j][0]) + abs(points[i][-1] - points[j][-1])
-                adj[i].append((weight, j))
-                adj[j].append((weight, i))
-        shortest = collections.defaultdict(int) # src: total_weight
+        adj = collections.defaultdict(list) # idx1: [(distance, idx2)]
+        for i in range(len(points)):
+            xi, yi = points[i]
+            for j in range(i + 1, len(points)):
+                xj, yj = points[j]
+                distance = abs(xi - xj) + abs(yi - yj)
+                adj[i].append((distance, j))
+                adj[j].append((distance, i))
+        visited = set() # idx
+        res = 0
         min_heap = [(0, 0)]
-        while min_heap and len(shortest) < N:
-            weight, dst = heapq.heappop(min_heap)
-            if dst in shortest: continue
-            shortest[dst] = weight
-            for nei_weight, nei_dst in adj[dst]:
-                if nei_dst in shortest: continue
-                heapq.heappush(min_heap, (nei_weight, nei_dst))
-        return sum(shortest.values())
-        
+        while len(visited) < len(points):
+            distance, idx = heapq.heappop(min_heap)
+            if idx in visited:
+                continue
+            visited.add(idx)
+            res += distance
+            for nei in adj[idx]:
+                if nei[-1] in visited:
+                    continue
+                heapq.heappush(min_heap, nei)
+        return res
+"""
+graph + min_heap (BFS)
+{
+    0: [(4, 1), (13, 2), (7, 3), (7, 4)], 
+    1: [(9, 2), (3, 3), (7, 4)], 
+    2: [(10, 3), (14, 4)],
+    3: [(4, 4)]
+}
+visited: (0, 1, 2)
+heap: (10, 2), (11, 3), (9, 2)
+total = 13
+"""
