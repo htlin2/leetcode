7095173d550ab1a0cc4dcb6cad403b8e09dc19b5
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-01-20 17:02:32 -0800

    Time: 0 ms (100.00%) | Memory: 18.8 MB (66.68%) - LeetSync

--- 261-graph-valid-tree/graph-valid-tree.py
+++ 261-graph-valid-tree/graph-valid-tree.py
@@ -1,20 +1,30 @@
+class UnionFind:
+    def __init__(self, N):
+        self.parents = [i for i in range(N)]
+        self.ranks = [1] * N
+
+    def find(self, n):
+        if n == self.parents[n]: return n
+        self.parents[n] = self.find(self.parents[self.parents[n]])
+        return self.parents[n]
+
+    def union(self, n1, n2):
+        p1, p2 = self.find(n1), self.find(n2)
+        if p1 == p2: return False
+        if self.ranks[p1] >= self.ranks[p2]:
+            self.ranks[p1] += self.ranks[p2]
+            self.parents[p2] = p1
+        else:
+            self.ranks[p2] += self.ranks[p1]
+            self.parents[p1] = p2
+        return True
+
 class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
-        adj = collections.defaultdict(list) # src: [dst]
-        for src, dst in edges:
-            adj[src].append(dst)
-            adj[dst].append(src)
-        visited = set()
-        def dfs(node, prev):
-            if node in visited:
+        nodes = n
+        uf = UnionFind(n)
+        for a, b in edges:
+            if not uf.union(a, b):
                 return False
-            visited.add(node)
-            for nei in adj[node]:
-                if nei == prev:
-                    continue
-                if not dfs(nei, node):
-                    return False
-            return True
-        if not dfs(0, None):
-            return False
-        return True if len(visited) == n else False
+            nodes -= 1
+        return nodes == 1
