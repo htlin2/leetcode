bca4fc4b4850134f78ad72d5bd40a05e8d135084
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-01-25 15:41:03 -0800

    Time: 4 ms (16.98%) | Memory: 17.9 MB (32.28%) - LeetSync

--- 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
@@ -1,26 +1,22 @@
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
-        def helper(string):
-            ans, temp = set(), ''
-            for i, char in enumerate(string):
-                temp += char
-                if len(string) % (i + 1) != 0:
-                    continue
-                multiply = len(string) // len(temp)
-                if temp * multiply == string:
-                    ans.add(temp)
-            return ans
-        ans1 = helper(str1)
-        ans2 = helper(str2)
-        common = ans1.intersection(ans2)
-        if not common: return ''
-        res = ''
-        for substr in common:
-            if len(substr) > len(res):
-                res = substr
-        return res
+        def reducer(substr):
+            res = set()
+            window = ''
+            for right in range(len(substr)):
+                window += substr[right]
+                if len(substr) % len(window) == 0 and window * (len(substr) // len(window)) == substr:
+                    res.add(window)
+            return res
+        reduced_1, reduced_2 = reducer(str1), reducer(str2)
+        common_set = reduced_1.intersection(reduced_2)
+        longest = ''
+        for common in common_set:
+            if len(common) >= len(longest):
+                longest = common
+        return longest
+
 """
-Input: str1 = "ABABAB", str2 = "ABAB"
-Output: "AB"
+sliding window
 
 """
