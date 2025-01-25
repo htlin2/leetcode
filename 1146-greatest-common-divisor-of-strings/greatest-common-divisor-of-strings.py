2d85eb5599df374d51966321bec5ebb1d252bbce
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-01-25 15:45:34 -0800

    Time: 0 ms (100.00%) | Memory: 17.8 MB (32.28%) - LeetSync

--- 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
@@ -1,22 +1,13 @@
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
-        def reducer(substr):
-            res = set()
-            window = ''
-            for right in range(len(substr)):
-                window += substr[right]
-                if len(substr) % len(window) == 0 and window * (len(substr) // len(window)) == substr:
-                    res.add(window)
-            return res
-        reduced_1, reduced_2 = reducer(str1), reducer(str2)
-        common_set = reduced_1.intersection(reduced_2)
-        longest = ''
-        for common in common_set:
-            if len(common) >= len(longest):
-                longest = common
-        return longest
-
+        n1, n2 = len(str1), len(str2)
+        for i in range(min(n1, n2), 0, -1):
+            if n1 % i or n2 % i:
+                continue
+            m1, m2 = n1 // i, n2 // i
+            if m1 * str1[:i] == str1 and m2 * str1[:i] == str2:
+                return str1[:i]
+        return ''
 """
-sliding window
-
+greedy + math
 """
