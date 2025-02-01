61858bc44646f3b248f2d3ad781b9399dacd14aa
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-02-01 14:54:43 -0800

    Time: 231 ms (47.27%) | Memory: 34.1 MB (37.75%) - LeetSync

--- 892-shortest-subarray-with-sum-at-least-k/shortest-subarray-with-sum-at-least-k.py
+++ 892-shortest-subarray-with-sum-at-least-k/shortest-subarray-with-sum-at-least-k.py
@@ -1,36 +1,20 @@
 class Solution:
     def shortestSubarray(self, nums: List[int], k: int) -> int:
-        if not nums: return -1
+        res = float('inf')
         prefix_sums = []
         curr_sum = 0
-        res = float('inf')
-        # prefix sums
-        for i, n in enumerate(nums):
-            curr_sum += n
+        for i, num in enumerate(nums):
+            curr_sum += num
             if curr_sum >= k:
                 res = min(res, i + 1)
             prefix_sums.append(curr_sum)
-        # monotonic q
-        N = len(nums)
-        q = collections.deque()
-        for i in range(N):
+
+        q = collections.deque([])
+        for i in range(len(prefix_sums)):
             while q and prefix_sums[i] - prefix_sums[q[0]] >= k:
                 first = q.popleft()
                 res = min(res, i - first)
-            while q and prefix_sums[q[-1]] >= prefix_sums[i]:
+            while q and prefix_sums[q[-1]] > prefix_sums[i]:
                 q.pop()
             q.append(i)
         return -1 if res == float('inf') else res
-
-"""
-monotonic queue increasing + prefix sums
-k = 22
-queue= [2, 5, 11, 18]
-nums = [2, -1, -2, -3, 4, 5, 6, 7]
-prefx=  2,  1, -1, -4, 0, 5, 11,18
-         /
-        /
-\      /
- \    /
-  \__/
-"""
