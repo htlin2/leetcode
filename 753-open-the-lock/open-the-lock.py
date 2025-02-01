70fbf98113ab88da0eba9bbdcada0f05dd90a81a
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-02-01 15:31:17 -0800

    Time: 420 ms (42.17%) | Memory: 19.5 MB (11.55%) - LeetSync

--- 753-open-the-lock/open-the-lock.py
+++ 753-open-the-lock/open-the-lock.py
@@ -1,38 +1,31 @@
 class Solution:
     def openLock(self, deadends: List[str], target: str) -> int:
-        def get_nei(num):
-            res = []
-            for i in range(4):
-                num_list_plus = list(num)
-                num_list_plus[i] = str((int(num_list_plus[i]) + 1) % 10)
-                res.append(''.join(num_list_plus))
-                num_list_minus = list(num)
-                num_list_minus[i] = str((int(num_list_minus[i]) - 1 + 10) % 10)
-                res.append(''.join(num_list_minus))
-            return res
-        if "0000" in deadends:
-            return -1
-        q = collections.deque([(0, '0000')])
         visited = set(deadends)
+        if target in visited or "0000" in visited: return -1
+        def get_nei(nums):
+            res = []
+            for i in range(len(nums)):
+                digit = (int(nums[i]) + 1) % 10
+                res.append(nums[:i] + str(digit) + nums[i + 1:])
+                digit = (int(nums[i]) - 1) % 10
+                res.append(nums[:i] + str(digit) + nums[i + 1:])
+            return res
+        q = collections.deque([(0, "0000")]) # (count, combo)
         while q:
-            count, num = q.popleft()
-            if num == target:
-                return count
-            count += 1
-            for nei in get_nei(num):
-                if nei in visited:
-                    continue
+            count, combo = q.popleft()
+            if combo == target: return count
+            for nei in get_nei(combo):
+                if nei in visited: continue
                 visited.add(nei)
-                q.append((count, nei))
+                q.append((count + 1, nei))
         return -1
-
 """
-BFS + Math
+shortest path prim's algo (min_heap)
 Input: deadends = ["0201","0101","0102","1212","2002"], target = "0202"
 Output: 6
-Explanation: 
-A sequence of valid moves would be "0000" -> "1000" -> "1100" -> "1200" -> "1201" -> "1202" -> "0202".
-Note that a sequence like "0000" -> "0001" -> "0002" -> "0102" -> "0202" would be invalid,
-because the wheels of the lock become stuck after the display becomes the dead end "0102".
 
+visited = ["0201","0101","0102","1212","2002", "0000", "0009"]
+heap = [(1, 0001), (1, 0009), (1, 0010), (1, 0100), (1, 1000)]
+Time: E * log(v)
+Space: E + V
 """
