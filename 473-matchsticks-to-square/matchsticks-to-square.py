735f1dea90035a93d137df941b6f17d989b7eb93
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-02-09 15:44:47 -0800

    Time: 4155 ms (16.51%) | Memory: 17.7 MB (81.57%) - LeetSync

--- 473-matchsticks-to-square/matchsticks-to-square.py
+++ 473-matchsticks-to-square/matchsticks-to-square.py
@@ -4,22 +4,13 @@ class Solution:
         if length * 4 != sum(nums): return False
         nums.sort(reverse=True)
         sides = [0] * 4
-        def dfs(i):
+        def backtrack(i):
             if i == len(nums): return True
             for j in range(4):
                 if sides[j] + nums[i] <= length:
                     sides[j] += nums[i]
-                    if dfs(i + 1):
+                    if backtrack(i + 1):
                         return True
                     sides[j] -= nums[i]
             return False
-        return dfs(0)
-"""
-{1: 4, 2: 2, 4: 1}
-
-math + backtrack
-how to find one side...
-total / 4 = one side
-pair highest with lowest to get one side?
-
-"""
+        return backtrack(0)
