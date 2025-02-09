db9a583f0bd050e6d6c9582388d3389aa4dcd93d
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-02-08 19:22:02 -0800

    Time: 71 ms (36.57%) | Memory: 17.9 MB (60.98%) - LeetSync

--- 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.py
+++ 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.py
@@ -1,22 +1,29 @@
 class Solution:
     def longestSubstring(self, s: str, k: int) -> int:
+        def get_longest(i):
+            res = 0
+            window = collections.defaultdict(int)
+            l = 0
+            for r in range(len(s)):
+                window[s[r]] += 1
+                while len(window) > i:
+                    window[s[l]] -= 1
+                    if window[s[l]] == 0:
+                        del window[s[l]]
+                    l += 1
+                if min(window.values()) >= k:
+                    res = max(res, r - l + 1)
+            return res
+
         res = 0
         for i in range(1, len(set(s)) + 1):
-            window = collections.defaultdict(int)
-            left = 0
-            for right, char in enumerate(s):
-                window[char] += 1
-                while len(window) > i:
-                    window[s[left]] -= 1
-                    if window[s[left]] == 0:
-                        del window[s[left]]
-                    left += 1
-                is_valid = all([True if v >= k else False for v in window.values()])
-                if is_valid:
-                    res = max(res, right - left + 1)
+            longest = get_longest(i)
+            res = max(res, longest)
         return res
-
 """
-brute force
+backtracking
++/- substring
+
+sliding window + binary search
 
 """
