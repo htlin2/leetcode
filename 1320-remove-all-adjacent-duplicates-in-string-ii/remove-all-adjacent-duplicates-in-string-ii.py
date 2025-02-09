bc0593897d071081539491a58ee82584a4e69b8a
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-02-08 19:34:02 -0800

    Time: 61 ms (21.44%) | Memory: 22.3 MB (61.10%) - LeetSync

--- 1320-remove-all-adjacent-duplicates-in-string-ii/remove-all-adjacent-duplicates-in-string-ii.py
+++ 1320-remove-all-adjacent-duplicates-in-string-ii/remove-all-adjacent-duplicates-in-string-ii.py
@@ -4,27 +4,22 @@ class Solution:
         for char in s:
             if stack and stack[-1][0] == char:
                 stack[-1][-1] += 1
-                if stack[-1][-1] == k:
-                    stack.pop()
             else:
                 stack.append([char, 1])
+            while stack and stack[-1][-1] >= k:
+                stack[-1][-1] -= k
+                if stack[-1][-1] == 0:
+                    stack.pop()
+                while len(stack) >= 2 and stack[-1][0] == stack[-2][0]:
+                    char, count = stack.pop()
+                    stack[-1][-1] += count
         res = ''
         for char, count in stack:
             res += char * count
         return res
-
 """
 stack
-Example 2:
-
 Input: s = "deeedbbcccbdaa", k = 3
 Output: "aa"
-deeedbbcccbdaa
-a: 2
 
-
-Input: s = "pbbcggttciiippooaais", k = 2
-Output: "ps"
-stack
-p:1 , s
 """
