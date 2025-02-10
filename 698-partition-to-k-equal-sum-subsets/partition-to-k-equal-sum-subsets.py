478eb2e2e262614fb7807bbfccff1e901284aed7
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-02-09 16:47:08 -0800

    Time: 7 ms (96.05%) | Memory: 17.9 MB (79.54%) - LeetSync

--- 698-partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.py
+++ 698-partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.py
@@ -4,17 +4,17 @@ class Solution:
         if total % k != 0: return False
         target = total // k
         nums.sort(reverse=True)
-        used = [False] * len(nums)
+        visited = set()
         def bt(i, k_rem, curr_sum):
             if k_rem == 0: return True
             if curr_sum == target: 
                 return bt(0, k_rem - 1, 0)
             for j in range(i, len(nums)):
-                if not used[j] and curr_sum + nums[j] <= target:
-                    used[j] = True
+                if j not in visited and curr_sum + nums[j] <= target:
+                    visited.add(j)
                     if bt(j + 1, k_rem, curr_sum + nums[j]):
                         return True
-                    used[j] = False
+                    visited.remove(j)
                     if curr_sum == 0:
                         break
             return False
