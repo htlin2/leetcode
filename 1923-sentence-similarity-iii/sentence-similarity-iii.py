9dba08d3340226728647ff654de8cab8ef89b514
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-02-10 21:46:27 -0800

    Time: 0 ms (100.00%) | Memory: 17.8 MB (54.71%) - LeetSync

--- 1923-sentence-similarity-iii/sentence-similarity-iii.py
+++ 1923-sentence-similarity-iii/sentence-similarity-iii.py
@@ -1,19 +1,11 @@
 class Solution:
     def areSentencesSimilar(self, s1: str, s2: str) -> bool:
-        if s1 == s2: return True
-        if len(s2) > len(s1):
-            return self.areSentencesSimilar(s2, s1)
-        w1, w2 = s1.split(' '), s2.split(' ')
-        w1_l, w1_r, w2_l, w2_r = 0, len(w1) - 1, 0, len(w2) - 1
-        while w1_l < len(w1) and w2_l < len(w2) and w1[w1_l] == w2[w2_l]:
-            w1_l += 1
-            w2_l += 1
-        while w1_r >= 0 and w2_r >= 0 and w1[w1_r] == w2[w2_r]:
-            w1_r -= 1
-            w2_r -= 1
-        return w1_l > w2_r
-"""
-two pointers
-handle both sides
-handle front or back
-"""
+        q1 = collections.deque(s1.split(' '))
+        q2 = collections.deque(s2.split(' '))
+        while q1 and q2 and q1[0] == q2[0]:
+            q1.popleft()
+            q2.popleft()
+        while q1 and q2 and q1[-1] == q2[-1]:
+            q1.pop()
+            q2.pop()
+        return not q1 or not q2
