12fccc569ebcb4056dc43627b23f7b57ad465c1f
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-02-22 10:50:12 -0800

    Time: 11 ms (88.77%) | Memory: 17.9 MB (81.09%) - LeetSync

--- 698-partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.py
+++ 698-partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.py
@@ -1,21 +1,26 @@
 class Solution:
     def canPartitionKSubsets(self, nums: List[int], k: int) -> bool:
         total = sum(nums)
-        if total % k != 0: return False
-        target = total // k
+        if total % k: return False
         nums.sort(reverse=True)
-        visited = set()
-        def bt(i, k_rem, curr_sum):
+        target = total // k
+        visited = set() # index
+        def dfs(i, curr_sum, k_rem):
             if k_rem == 0: return True
-            if curr_sum == target: 
-                return bt(0, k_rem - 1, 0)
+            if curr_sum == target: return dfs(0, 0, k_rem - 1)
             for j in range(i, len(nums)):
-                if j not in visited and curr_sum + nums[j] <= target:
-                    visited.add(j)
-                    if bt(j + 1, k_rem, curr_sum + nums[j]):
-                        return True
-                    visited.remove(j)
-                    if curr_sum == 0:
-                        break
+                if j in visited or curr_sum + nums[j] > target: continue
+                visited.add(j)
+                if dfs(j + 1, curr_sum + nums[j], k_rem):
+                    return True
+                visited.remove(j)
+                if curr_sum == 0:
+                    break
             return False
-        return bt(0, k, 0)
+        return dfs(0, 0, k)
+
+"""
+backtracking
+total / k to get equal sum
+
+"""
