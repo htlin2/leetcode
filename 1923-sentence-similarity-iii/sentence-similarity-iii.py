829efa26b339243fb917e0b789f4aa41ddd2475c
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-02-22 09:42:13 -0800

    Time: 0 ms (100.00%) | Memory: 17.7 MB (88.84%) - LeetSync

--- 1923-sentence-similarity-iii/sentence-similarity-iii.py
+++ 1923-sentence-similarity-iii/sentence-similarity-iii.py
@@ -1,15 +1,20 @@
 class Solution:
     def areSentencesSimilar(self, s1: str, s2: str) -> bool:
-        if len(s1) > len(s2):
-            return self.areSentencesSimilar(s2, s1)
         w1, w2 = s1.split(' '), s2.split(' ')
-        N1, N2 = len(w1), len(w2)
-        l1, l2 = 0, 0
-        while l1 < N1 and l2 < N2 and w1[l1] == w2[l2]:
-            l1 += 1
-            l2 += 1
-        r1, r2 = N1 - 1, N2 - 1
-        while r1 >= 0 and r2 >= 0 and w1[r1] == w2[r2]:
-            r1 -= 1
-            r2 -= 1
-        return l1 > r1
+        q1, q2 = collections.deque(w1), collections.deque(w2)
+        while q1 and q2 and q1[0] == q2[0]:
+            q1.popleft()
+            q2.popleft()
+        while q1 and q2 and q1[-1] == q2[-1]:
+            q1.pop()
+            q2.pop()
+        return len(q1) == 0 or len(q2) == 0
+"""
+queue
+Input: 
+sentence1 = "My name is Haley"
+sentence2 = "My Haley"
+w1 [name, is]
+w2 []
+
+"""
