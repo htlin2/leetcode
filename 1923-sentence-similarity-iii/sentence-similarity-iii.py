9fc79b67c39e16f64af8e5ec68894f6dbc65c882
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-02-22 10:02:37 -0800

    Time: 0 ms (100.00%) | Memory: 17.8 MB (60.65%) - LeetSync

--- 1923-sentence-similarity-iii/sentence-similarity-iii.py
+++ 1923-sentence-similarity-iii/sentence-similarity-iii.py
@@ -1,20 +1,23 @@
 class Solution:
     def areSentencesSimilar(self, s1: str, s2: str) -> bool:
+        if len(s1) < len(s2):
+            s1, s2 = s2, s1
         w1, w2 = s1.split(' '), s2.split(' ')
-        q1, q2 = collections.deque(w1), collections.deque(w2)
-        while q1 and q2 and q1[0] == q2[0]:
-            q1.popleft()
-            q2.popleft()
-        while q1 and q2 and q1[-1] == q2[-1]:
-            q1.pop()
-            q2.pop()
-        return len(q1) == 0 or len(q2) == 0
+        w1_left, w2_left = 0, 0
+        while w1_left < len(w1) and w2_left < len(w2) and w1[w1_left] == w2[w2_left]:
+            w1_left += 1
+            w2_left += 1
+        w1_right, w2_right = len(w1) - 1, len(w2) - 1
+        while w1_right >= 0 and w2_right >= 0 and w1[w1_right] == w2[w2_right]:
+            w1_right -= 1
+            w2_right -= 1
+        return w2_right < w1_left
 """
-queue
-Input: 
-sentence1 = "My name is Haley"
-sentence2 = "My Haley"
-w1 [name, is]
-w2 []
+two pointers
+Input: sentence1 = "My name is Haley", sentence2 = "My Haley"
+w1 = [My, name, is, Haley]
+          w1_l  w2_r
+w2 = [My, Haley]
+      w2_r  w2_l
 
 """
