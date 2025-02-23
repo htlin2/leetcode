35d9cc5eee53d063e8fa0193a19a9ed4da717099
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-02-23 09:20:22 -0800

    Time: 323 ms (63.11%) | Memory: 17.9 MB (67.55%) - LeetSync

--- 473-matchsticks-to-square/matchsticks-to-square.py
+++ 473-matchsticks-to-square/matchsticks-to-square.py
@@ -2,20 +2,22 @@ class Solution:
     def makesquare(self, nums: List[int]) -> bool:
         total = sum(nums)
         if total % 4 != 0: return False
-        nums.sort(reverse=True)
+        print(total % 4)
         target = total // 4
         visited = set()
-        def bt(i, rem, curr_sum):
-            if rem == 0: return True
+        nums.sort(reverse=True)
+        def bt(i, curr_sum, rem_side):
+            if rem_side == 0: return True
             if curr_sum == target:
-                return bt(0, rem - 1, 0)
+                return bt(0, 0, rem_side - 1)
             for j in range(i, len(nums)):
-                if j not in visited and curr_sum + nums[j] <= target:
-                    visited.add(j)
-                    if bt(j + 1, rem, curr_sum + nums[j]):
-                        return True
-                    visited.remove(j)
-                    if curr_sum == 0:
-                        break
+                if j in visited or curr_sum + nums[j] > target: continue
+                visited.add(j)
+                if bt(j + 1, curr_sum + nums[j], rem_side): return True
+                visited.remove(j)
+                if curr_sum == 0: break
             return False
-        return bt(0, 4, 0)
+        return bt(0, 0, 4)
+"""
+backtracking
+"""
