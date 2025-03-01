e8f3ff29634a8674422e0f5648bcda7c2d1dff1d
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-03-01 12:28:42 -0800

    Time: 19 ms (5.37%) | Memory: 19.1 MB (6.38%) - LeetSync

--- 860-design-circular-queue/design-circular-queue.py
+++ 860-design-circular-queue/design-circular-queue.py
@@ -1,37 +1,47 @@
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+        self.prev = None
+
 class MyCircularQueue:
 
     def __init__(self, k: int):
-        self.hashmap = {}
-        self.head = 0
-        self.curr = 0
-        self.k = k - 1
+        self.head, self.tail = Node(-1), Node(-1)
+        self.head.next, self.tail.prev = self.tail, self.head
+        self.count = 0
+        self.k = k
 
     def enQueue(self, value: int) -> bool:
         if self.isFull(): return False
-        self.hashmap[self.curr] = value
-        self.curr += 1
+        curr_node = Node(value)
+        prev_node = self.tail.prev
+        prev_node.next, curr_node.prev = curr_node, prev_node
+        curr_node.next, self.tail.prev = self.tail, curr_node
+        self.count += 1
         return True
 
     def deQueue(self) -> bool:
         if self.isEmpty(): return False
-        del self.hashmap[self.head]
-        self.head += 1
+        next_node = self.head.next.next
+        self.head.next, next_node.prev = next_node, self.head
+        self.count -= 1
         return True
 
     def Front(self) -> int:
         if self.isEmpty(): return -1
-        return self.hashmap[self.head]
+        return self.head.next.val
 
     def Rear(self) -> int:
         if self.isEmpty(): return -1
-        return self.hashmap[self.curr - 1]
+        return self.tail.prev.val
 
     def isEmpty(self) -> bool:
-        if self.head == self.curr: return True
+        if self.count == 0: return True
         return False
 
     def isFull(self) -> bool:
-        if self.curr > (self.head + self.k): return True
+        if self.count == self.k: return True
         return False
 
 
