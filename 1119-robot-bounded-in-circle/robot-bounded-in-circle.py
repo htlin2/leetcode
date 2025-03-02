3f7f3a4bd7caf97972a1a5904820bb0431f6540d
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-03-02 13:45:34 -0800

    Time: 0 ms (100.00%) | Memory: 17.9 MB (41.00%) - LeetSync

--- 1119-robot-bounded-in-circle/robot-bounded-in-circle.py
+++ 1119-robot-bounded-in-circle/robot-bounded-in-circle.py
@@ -1,26 +1,14 @@
 class Solution:
     def isRobotBounded(self, instructions: str) -> bool:
-        hashmap = {
-            0: (0, 1), # north
-            90: (1, 0), # east
-            180: (0, -1), # south
-            270: (-1, 0), # west
-        }
-        paths = {}
-        instructions *= 4
-        facing = 0
-        x, y = 0, 0
-        for i in range(2):
-            path = [(x, y)]
-            for instruction in instructions:
-                if instruction == 'G':
-                    dx, dy = hashmap[facing]
-                    x += dx
-                    y += dy
-                    path.append((x, y))
-                elif instruction == 'R':
-                    facing = (facing + 90) % 360
-                elif instruction == 'L':
-                    facing = (facing - 90 + 360) % 360
-            paths[i] = path
-        return paths[0] == paths[1]
+        # North, East, South, West
+        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        x, y, d = 0, 0, 0
+        for i in instructions:
+            if i == 'G':
+                x += directions[d][0]
+                y += directions[d][1]
+            elif i == 'R':
+                d = (d + 1) % 4
+            elif i == 'L':
+                d = (d - 1) % 4
+        return (x == 0 and y == 0) or d != 0
