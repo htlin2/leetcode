3afd3023595f7a91e8eb6439d7fc800a723bd73d
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-03-02 20:21:35 -0800

    Time: 0 ms (100.00%) | Memory: 18.1 MB (68.73%) - LeetSync

--- 617-merge-two-binary-trees/merge-two-binary-trees.py
+++ 617-merge-two-binary-trees/merge-two-binary-trees.py
@@ -5,33 +5,16 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def mergeTrees(self, A: Optional[TreeNode], B: Optional[TreeNode]) -> Optional[TreeNode]:
-        if not A: return B
-        if not B: return A
-        if not A and not B: return None
-        A_val = A.val if A else 0
-        B_val = B.val if B else 0
-        node = TreeNode(A_val + B_val)
-
-        A_left_node = A.left if A else None
-        B_left_node = B.left if B else None
-        node.left = self.mergeTrees(A_left_node, B_left_node)
-
-        A_right_node = A.right if A else None
-        B_right_node = B.right if B else None
-        node.right = self.mergeTrees(A_right_node, B_right_node)
+    def mergeTrees(self, r1: Optional[TreeNode], r2: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not r1 and not r2: return None
+        if r1 and not r2: return r1
+        if not r1 and r2: return r2
+        node = TreeNode(r1.val + r2.val)
+        node.left = self.mergeTrees(r1.left, r2.left)
+        node.right = self.mergeTrees(r1.right, r2.right)
         return node
 
 """
-dfs preorder
-base case:
-    if not A: return B
-    if not B: return A
-    if not A and not B: return None
-A = A.val if A else 0
-B = B.val if B else 0
-combined_node = A + B
-combined_node.left = dfs(A.left, B.left)
-combined_node.right = dfs(A.right, B.right)
-return combined_node
+preorder dfs
+only stop when both root have no nodes
 """
