3c3dc71293e744754cc73b30bf7aa91b864799c4
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-03-09 16:50:10 -0700

    Time: 31 ms (27.40%) | Memory: 21.1 MB (86.52%) - LeetSync

--- 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.py
+++ 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.py
@@ -1,10 +1,13 @@
 class Solution:
     def kthSmallest(self, grid: List[List[int]], k: int) -> int:
-        nums = []
-        for row in grid:
-            nums = [*nums, *row]
-        nums.sort()
-        return nums[k - 1]
-"""
-brute force
-"""
+        min_heap = [] # (value, row, col)
+        for col in range(len(grid[0])):
+            to_add = (grid[0][col], 0, col)
+            heapq.heappush(min_heap, to_add)
+        while k > 1:
+            v, r, c = heapq.heappop(min_heap)
+            if r + 1 < len(grid):
+                to_add = (grid[r + 1][c], r + 1, c)
+                heapq.heappush(min_heap, to_add)
+            k -= 1
+        return min_heap[0][0]
