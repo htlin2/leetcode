3b93ad128734aa92cdc7a7a413726a0ec25f4193
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-03-22 21:17:23 -0700

    Time: 0 ms (100.00%) | Memory: 17.8 MB (46.30%) - LeetSync

--- 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
@@ -1,12 +1,8 @@
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
-        N1, N2 = len(str1), len(str2)
-        for i in range(min(N1, N2), 0, -1):
-            if N1 % i or N2 % i:
-                continue
-            curr = str1[:i]
-            s1 = N1 // i * curr
-            s2 = N2 // i * curr
-            if s1 == str1 and s2 == str2:
-                return curr
-        return ''
+        if str1 + str2 != str2 + str1: return ''
+        def gcd(x, y):
+            if y == 0: return x
+            return gcd(y, x % y)
+        end = gcd(len(str1), len(str2))
+        return str1[:end]
