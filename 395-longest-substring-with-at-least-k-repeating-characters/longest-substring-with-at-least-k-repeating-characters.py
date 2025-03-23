8549d8552ba76fefe2ea8523f087abf0ef357ce6
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-03-23 14:39:09 -0700

    Time: 74 ms (36.95%) | Memory: 17.8 MB (72.38%) - LeetSync

--- 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.py
+++ 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.py
@@ -1,29 +1,19 @@
 class Solution:
     def longestSubstring(self, s: str, k: int) -> int:
-        def get_longest(i):
-            res = 0
-            window = collections.defaultdict(int)
-            l = 0
-            for r in range(len(s)):
-                window[s[r]] += 1
-                while len(window) > i:
-                    window[s[l]] -= 1
-                    if window[s[l]] == 0:
-                        del window[s[l]]
-                    l += 1
-                if min(window.values()) >= k:
-                    res = max(res, r - l + 1)
-            return res
-
+        max_uniq_chars = len(set(s))
+        counter = collections.Counter(s)
         res = 0
-        for i in range(1, len(set(s)) + 1):
-            longest = get_longest(i)
-            res = max(res, longest)
+        for i in range(1, max_uniq_chars + 1):
+            window = collections.defaultdict(int)
+            left = 0
+            for right in range(len(s)):
+                window[s[right]] += 1
+                while len(window) > i:
+                    window[s[left]] -= 1
+                    if window[s[left]] == 0:
+                        del window[s[left]]
+                    left += 1
+                is_valid = min(window.values()) >= k
+                if is_valid:
+                    res = max(res, sum(window.values()))
         return res
-"""
-backtracking
-+/- substring
-
-sliding window + binary search
-
-"""
