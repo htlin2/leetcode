67f85497312d0994f2c4011f196ddfb4177cd8a5
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-03-23 15:30:15 -0700

    Time: 116 ms (78.89%) | Memory: 32.4 MB (43.60%) - LeetSync

--- 2419-subarray-with-elements-greater-than-varying-threshold/subarray-with-elements-greater-than-varying-threshold.py
+++ 2419-subarray-with-elements-greater-than-varying-threshold/subarray-with-elements-greater-than-varying-threshold.py
@@ -1,25 +1,37 @@
 class Solution:
     def validSubarraySize(self, nums: List[int], threshold: int) -> int:
-        nums = nums + [0]
+        if not nums or not threshold: return -1
+        nums = [0] + nums + [0]
         stack = []
-        for r in range(len(nums)):
-            next_small = nums[r]
-            while stack and nums[stack[-1]] >= next_small:
-                prev_small_idx = stack.pop()
-                k = r - stack[-1] - 1 if stack else r
-                if nums[prev_small_idx] > threshold / k:
+        for i in range(len(nums)):
+            next_small = nums[i]
+            while stack and nums[stack[-1]] > next_small:
+                highest_idx = stack.pop()
+                prev_small = nums[highest_idx]
+                k = i - stack[-1] - 1 if stack else i
+                if threshold / k < prev_small:
                     return k
-            stack.append(r)
+            stack.append(i)
         return -1
-
 """
 monotonic stack increasing
-previous small, next small
+prev small, next small
 Input: nums = [1,3,4,3,1], threshold = 6
-idx            0,1,2,3,4
 Output: 3
-stack = 1,3
-next_small = 3
-prev_small = 3
-k = 2
+[0, 1, 3, 4, 3, 1, 0]
+ 0, 1, 2, 3, 4, 5, 6
+ 0, 1, 2, 3, 4
+
+
+Input: nums = [6,5,6,5,8], threshold = 7
+Output: 1
+[0, 6, 5, 6, 5, 8]
+ 0, 1, 2, 3, 4, 5 -> index
+             i
+stack = [0]
+prev_small = 5
+next_small = 5
+base (k) = 2
+target = threshold / k = 3.5
+
 """
