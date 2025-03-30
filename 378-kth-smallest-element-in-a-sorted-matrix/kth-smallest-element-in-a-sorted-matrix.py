644f1b8377b21714730f03126db748023e8dc1dc
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-03-30 12:49:14 -0700

    Time: 29 ms (32.74%) | Memory: 21.3 MB (53.37%) - LeetSync

--- 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.py
+++ 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.py
@@ -1,13 +1,29 @@
 class Solution:
     def kthSmallest(self, grid: List[List[int]], k: int) -> int:
-        min_heap = [] # (value, row, col)
-        for col in range(len(grid[0])):
-            to_add = (grid[0][col], 0, col)
-            heapq.heappush(min_heap, to_add)
-        while k > 1:
-            v, r, c = heapq.heappop(min_heap)
+        min_heap = [] # (num, (r, c))
+        for i in range(len(grid[0])):
+            heapq.heappush(min_heap, (grid[0][i], (0, i)))
+
+        for _ in range(k - 1):
+            _num, (r, c) = heapq.heappop(min_heap)
             if r + 1 < len(grid):
-                to_add = (grid[r + 1][c], r + 1, c)
+                num = grid[r + 1][c]
+                to_add = (num, (r + 1, c))
                 heapq.heappush(min_heap, to_add)
-            k -= 1
         return min_heap[0][0]
+"""
+heap + queue
+k = 8
+Input: grid = 
+[
+    [1,5,9],
+    [10,11,13],
+    [12,13,15]
+]
+Output: 13
+Explanation: The elements in the matrix are [1,5,9,10,11,12,13,13,15], and the 8th smallest number is 13
+k = 4
+[ 1, 5, 9]
+[10,11,13]
+[12,]
+"""
