e14270630c024c40bc61aaaf397233b2b78c5df1
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-03-30 13:23:45 -0700

    Time: 1 ms (85.84%) | Memory: 21.2 MB (86.29%) - LeetSync

--- 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.py
+++ 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.py
@@ -14,19 +14,3 @@ class Solution:
             else:
                 right = mid - 1
         return left
-"""
-heap + queue
-k = 8
-Input: grid = 
-[
-    [1,5,9],
-    [10,11,13],
-    [12,13,15]
-]
-Output: 13
-Explanation: The elements in the matrix are [1,5,9,10,11,12,13,13,15], and the 8th smallest number is 13
-k = 4
-[ 1, 5, 9]
-[10,11,13]
-[12,]
-"""
