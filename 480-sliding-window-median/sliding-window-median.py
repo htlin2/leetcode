5251d50b004c6ce46d19bddfdbd3d3d81b0471f8
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-03-30 12:30:01 -0700

    Time: 84 ms (82.73%) | Memory: 32.2 MB (57.98%) - LeetSync

--- 480-sliding-window-median/sliding-window-median.py
+++ 480-sliding-window-median/sliding-window-median.py
@@ -2,52 +2,46 @@ class Solution:
     def medianSlidingWindow(self, nums: List[int], k: int) -> List[float]:
         left = [] # max_heap
         right = [] # min_heap
-        res, heap_dict = [], collections.defaultdict(int)
-        is_even = k % 2 == 0
+        nums_dict = collections.defaultdict(int) # num: count
+        is_odd = k % 2 == 1
+        res = []
         for i in range(k):
-            if not left or nums[i] < -left[0]:
+            if not left or nums[i] <= -left[0]:
                 heapq.heappush(left, -nums[i])
             else:
                 heapq.heappush(right, nums[i])
-            if len(left) - 1 > len(right):
+            if len(left) - len(right) >= 2:
                 heapq.heappush(right, -heapq.heappop(left))
-            if len(left) < len(right):
+            if len(right) - len(left) >= 1:
                 heapq.heappush(left, -heapq.heappop(right))
-        if is_even:
-            median = (-left[0] + right[0]) / 2
-        else:
+        if is_odd:
             median = -left[0]
+        else:
+            median = (-left[0] + right[0]) / 2
         res.append(median)
         for i in range(k, len(nums)):
             prev_num = nums[i - k]
-            heap_dict[prev_num] += 1
+            nums_dict[prev_num] += 1
             balance = -1 if prev_num <= median else 1
             if nums[i] <= median:
-                balance += 1
                 heapq.heappush(left, -nums[i])
+                balance += 1
             else:
-                balance -= 1
                 heapq.heappush(right, nums[i])
-            # balance
+                balance -= 1
             if balance < 0:
                 heapq.heappush(left, -heapq.heappop(right))
-            elif balance > 0:
+            elif 0 < balance:
                 heapq.heappush(right, -heapq.heappop(left))
-            # remove prev
-            while left and heap_dict[-left[0]] > 0:
-                heap_dict[-left[0]] -= 1
+            while left and nums_dict[-left[0]]:
+                nums_dict[-left[0]] -= 1
                 heapq.heappop(left)
-            while right and heap_dict[right[0]] > 0:
-                heap_dict[right[0]] -= 1
+            while right and nums_dict[right[0]]:
+                nums_dict[right[0]] -= 1
                 heapq.heappop(right)
-            if is_even:
-                median = (-left[0] + right[0]) / 2
-            else:
+            if is_odd:
                 median = -left[0]
+            else:
+                median = (-left[0] + right[0]) / 2
             res.append(median)
         return res
-
-"""
-left = -3, -1, 1
-right = 3
-"""
