6cf48eca4362489b9f29f7a15ca5d309d5c2717f
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-03-30 17:32:19 -0700

    Time: 1 ms (85.84%) | Memory: 21 MB (99.51%) - LeetSync

--- 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.py
+++ 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.py
@@ -3,8 +3,13 @@ class Solution:
         left, right = grid[0][0], grid[-1][-1]
         def less_than_mid_counts(mid):
             count = 0
-            for row in grid:
-                count += bisect.bisect_right(row, mid)
+            r, c = len(grid) - 1, 0
+            while r >= 0 and c < len(grid[0]):
+                if grid[r][c] <= mid:
+                    count += r + 1
+                    c += 1
+                else:
+                    r -= 1
             return count
 
         while left <= right:
