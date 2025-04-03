42a96300e7ac00580f723a6e6e6a892c57482b2a
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-04-02 21:43:35 -0700

    Time: 39 ms (65.90%) | Memory: 28.9 MB (90.32%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.py
+++ 41-first-missing-positive/first-missing-positive.py
@@ -5,8 +5,15 @@ class Solution:
             while 1 <= nums[i] <= N and nums[nums[i] - 1] != nums[i]:
                 correct_idx = nums[i] - 1
                 nums[i], nums[correct_idx] = nums[correct_idx], nums[i]
-
-        for i, num in enumerate(nums):
-            if i + 1 != num:
+        for i in range(len(nums)):
+            if nums[i] - 1 != i:
                 return i + 1
         return N + 1
+
+"""
+Input: nums = [3,4,-1,1]
+[ 3, 4,-1, 1]
+  0, 1, 2, 3
+  1,-1 ,3, 4
+           i 
+"""
