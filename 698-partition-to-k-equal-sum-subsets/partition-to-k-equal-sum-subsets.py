a1cca50f2a25bfd9c503a8181a26ce4d1b0ba045
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-04-04 19:44:07 -0700

    Time: 9 ms (88.45%) | Memory: 17.9 MB (81.54%) - LeetSync

--- 698-partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.py
+++ 698-partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.py
@@ -4,23 +4,18 @@ class Solution:
         if total % k: return False
         nums.sort(reverse=True)
         target = total // k
-        visited = set() # index
-        def dfs(i, curr_sum, k_rem):
-            if k_rem == 0: return True
-            if curr_sum == target: return dfs(0, 0, k_rem - 1)
+        visited = set()
+        def dfs(i, curr_sum, k):
+            if k == 0: return True
+            if curr_sum == target: return dfs(0, 0, k - 1)
             for j in range(i, len(nums)):
-                if j in visited or curr_sum + nums[j] > target: continue
-                visited.add(j)
-                if dfs(j + 1, curr_sum + nums[j], k_rem):
-                    return True
-                visited.remove(j)
-                if curr_sum == 0:
-                    break
+                if j in visited: continue
+                if curr_sum + nums[j] <= target:
+                    visited.add(j)
+                    if dfs(j + 1, curr_sum + nums[j], k):
+                        return True
+                    visited.remove(j)
+                    if curr_sum == 0:
+                        break
             return False
         return dfs(0, 0, k)
-
-"""
-backtracking
-total / k to get equal sum
-
-"""
