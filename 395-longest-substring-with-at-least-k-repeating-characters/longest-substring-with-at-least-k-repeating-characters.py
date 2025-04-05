9bb2009769043eddb70e9a8efaf5df31e091fe88
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-04-05 11:45:26 -0700

    Time: 68 ms (39.73%) | Memory: 18 MB (57.27%) - LeetSync

--- 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.py
+++ 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.py
@@ -1,19 +1,34 @@
 class Solution:
     def longestSubstring(self, s: str, k: int) -> int:
-        max_uniq_chars = len(set(s))
-        counter = collections.Counter(s)
         res = 0
-        for i in range(1, max_uniq_chars + 1):
+        def is_valid(mid):
+            nonlocal res
             window = collections.defaultdict(int)
             left = 0
             for right in range(len(s)):
                 window[s[right]] += 1
-                while len(window) > i:
+                while len(window) > mid:
                     window[s[left]] -= 1
                     if window[s[left]] == 0:
                         del window[s[left]]
                     left += 1
-                is_valid = min(window.values()) >= k
-                if is_valid:
-                    res = max(res, sum(window.values()))
+                if min(window.values()) >= k:
+                    total = sum(window.values())
+                    res = max(res, total)
+        for i in range(1, len(set(s)) + 1):
+            is_valid(i)
         return res
+"""
+binary search + sliding window
+Input: s = "ababbc", k = 2
+Output: 5
+Explanation: The longest substring is "ababb", as 'a' is repeated 2 times and 'b' is repeated 3 times.
+
+ a b a b b c
+ l         r
+window = {a: 2, b: 3, c: 1}
+uniq_char = 3 # abc
+binary search range 1 ~ 3
+mid = 2
+
+"""
