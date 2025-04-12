984b50deef6ec6a1a1218bc37178998c9ed3acce
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-04-12 12:24:39 -0700

    Time: 0 ms (100.00%) | Memory: 17.8 MB (75.22%) - LeetSync

--- 778-reorganize-string/reorganize-string.py
+++ 778-reorganize-string/reorganize-string.py
@@ -5,17 +5,20 @@ class Solution:
         for char, count in counter.items():
             heapq.heappush(max_heap, (-count, char))
         res = ''
-        while len(max_heap) >= 2:
+        while max_heap:
             first = heapq.heappop(max_heap)
-            second = heapq.heappop(max_heap)
-            res += first[-1] + second[-1]
+            if res and res[-1] == first[-1]:
+                return ''
+            res += first[-1]
+            second = []
+            if max_heap:
+                second = heapq.heappop(max_heap)
+                res += second[-1]
             if first[0] + 1 != 0:
                 heapq.heappush(max_heap, (first[0] + 1, first[-1]))
-            if second[0] + 1 != 0:
+            if second and second[0] + 1 != 0:
                 heapq.heappush(max_heap, (second[0] + 1, second[-1]))
-        if max_heap and abs(max_heap[0][0]) > 1: return ''
-        if max_heap and res and max_heap[0][-1] == res[-1]: return ''
-        return res + max_heap[0][-1] if max_heap else res
+        return res
 """
 max_heap = (count, val)
 
