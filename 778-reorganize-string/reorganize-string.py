ebc9c930637e4249ba4b4e27a72c38cc4e9352ca
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-04-12 12:20:10 -0700

    Time: 3 ms (48.19%) | Memory: 17.7 MB (93.99%) - LeetSync

--- 778-reorganize-string/reorganize-string.py
+++ 778-reorganize-string/reorganize-string.py
@@ -1,17 +1,22 @@
 class Solution:
     def reorganizeString(self, s: str) -> str:
-        N = len(s)
         counter = collections.Counter(s)
-        key_val_pairs = [[k, v] for k, v in counter.items()]
-        sorted_freq = sorted(key_val_pairs, key=lambda t: t[-1], reverse=True)
-        if sorted_freq[0][-1] > math.ceil(N / 2):
-            return ''
-        ans = [''] * N
-        i = 0
-        for k, v in sorted_freq:
-            for _ in range(v):
-                if i >= N:
-                    i = 1
-                ans[i] = k
-                i += 2
-        return ''.join(ans)
+        max_heap = [] #(count, char)
+        for char, count in counter.items():
+            heapq.heappush(max_heap, (-count, char))
+        res = ''
+        while len(max_heap) >= 2:
+            first = heapq.heappop(max_heap)
+            second = heapq.heappop(max_heap)
+            res += first[-1] + second[-1]
+            if first[0] + 1 != 0:
+                heapq.heappush(max_heap, (first[0] + 1, first[-1]))
+            if second[0] + 1 != 0:
+                heapq.heappush(max_heap, (second[0] + 1, second[-1]))
+        if max_heap and abs(max_heap[0][0]) > 1: return ''
+        if max_heap and res and max_heap[0][-1] == res[-1]: return ''
+        return res + max_heap[0][-1] if max_heap else res
+"""
+max_heap = (count, val)
+
+"""
