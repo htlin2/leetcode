5e74cf7ce8e8010d28d6a6515a57948459e13665
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-04-12 11:27:26 -0700

    Time: 52 ms (19.30%) | Memory: 30.1 MB (49.74%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.py
+++ 287-find-the-duplicate-number/find-the-duplicate-number.py
@@ -1,29 +1,13 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        N = len(nums)
-        slow = fast = 0
-        while True:
-            slow = nums[slow]
-            fast = nums[nums[fast]]
-            if slow == fast: break
-        slow2 = 0
-        while True:
-            slow2 = nums[slow2]
-            slow = nums[slow]
-            if slow2 == slow:
-                return slow
+        for i in range(len(nums)):
+            while nums[i] - 1 != i:
+                if nums[i] == nums[nums[i] - 1]:
+                    return nums[i]
+                next_idx = nums[i] - 1
+                nums[i], nums[next_idx] = nums[next_idx], nums[i]
+        return 0
 """
-[ 1, 3, 4, 2, 2]
-  0, 1, 2, 3, 4
-0 -> 1 -> 3 -> 2 -> 4
-                 <-
-sf  sf   sf   sf     sf
+two pointers - cycle detection
 
-ex 2:
-output 3
-[ 3, 1, 3, 4, 2]
-  0, 1, 2, 3, 4
-0 -> 3 -> 4 -> 2
-     3  <----  2
-              sf 
 """
