0d9f986938a733cca4f0dd99c6f29cc0efd364ee
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-04-12 21:18:49 -0700

    Time: 103 ms (23.02%) | Memory: 19.2 MB (12.52%) - LeetSync

--- 1648-minimum-insertions-to-balance-a-parentheses-string/minimum-insertions-to-balance-a-parentheses-string.py
+++ 1648-minimum-insertions-to-balance-a-parentheses-string/minimum-insertions-to-balance-a-parentheses-string.py
@@ -1,34 +1,26 @@
 class Solution:
     def minInsertions(self, s: str) -> int:
-        stack = []  # We can think of this stack as a way to keep track of unmatched '('.
-        inserts = 0  # Counts the number of insertions needed.
+        inserts = 0
+        stack = []
         i = 0
-        
         while i < len(s):
             if s[i] == '(':
-                stack.append('(')  # Push the open parenthesis onto the stack
-            elif s[i] == ')':
-                # We need to check if we have a pair of ')'
+                stack.append(s[i])
+                i += 1
+            else:
+                # s[i] == ')'
                 if i + 1 < len(s) and s[i + 1] == ')':
-                    # If the next character is also ')', it's a valid double closing for a '('
                     if stack:
-                        stack.pop()  # We can balance with a previous '('
+                        stack.pop()
                     else:
-                        # If there's no '(' to balance, we need an extra '('
                         inserts += 1
-                    i += 1  # Skip the next ')', as we've handled it here.
+                    i += 2
                 else:
-                    # Otherwise, it's a single ')', which means it's incomplete
                     if stack:
-                        stack.pop()  # Balance one '(' with this ')', but we need one more ')'
-                        inserts += 1  # Need to add another ')'
+                        stack.pop()
+                        inserts += 1
+                        i += 1
                     else:
-                        # No '(' to balance, so we need to add both '(' and ')'
                         inserts += 2
-            
-            i += 1
-
-        # If there are any unmatched '(' left in the stack, we need two ')' for each
-        inserts += 2 * len(stack)
-
-        return inserts
+                        i += 1
+        return inserts + len(stack) * 2
