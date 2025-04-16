75fa8301a679466c206d70609e3197e99b602fd1
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-04-15 22:44:50 -0700

    Time: 100 ms (78.56%) | Memory: 74.6 MB (77.78%) - LeetSync

--- 1023-time-based-key-value-store/time-based-key-value-store.py
+++ 1023-time-based-key-value-store/time-based-key-value-store.py
@@ -1,31 +1,22 @@
 class TimeMap:
 
     def __init__(self):
-        self.cache = collections.defaultdict(list) #key: [(time, val)]
+        self.cache = collections.defaultdict(list) # (timestamp, value)
 
     def set(self, key: str, value: str, timestamp: int) -> None:
         self.cache[key].append((timestamp, value))
 
     def get(self, key: str, timestamp: int) -> str:
-        if not key in self.cache: 
-            return ''
         time_values = self.cache[key]
-        N = len(time_values)
-        l, r = 0, N - 1
-        while l <= r:
-            m = (l + r) // 2
-            if time_values[m][0] == timestamp:
-                r = m - 1
-            elif time_values[m][0] < timestamp:
-                l = m + 1
-            else:
-                r = m - 1
-        if 0 <= l < N:
-            if time_values[l][0] == timestamp:
-                return time_values[l][-1]
-        if 0 == l:
+        if not time_values: return ''
+        to_find = timestamp
+        idx = bisect.bisect_right(time_values, (to_find,))
+        if 0 <= idx < len(time_values) and time_values[idx][0] == timestamp:
+            return time_values[idx][-1]
+        if idx - 1 < 0:
             return ''
-        return time_values[l - 1][-1]
+        return time_values[idx - 1][-1]
+
 
 # Your TimeMap object will be instantiated and called as such:
 # obj = TimeMap()
