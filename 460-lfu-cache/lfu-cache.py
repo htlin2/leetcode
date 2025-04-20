293902ddc35c4a609eae46b5115735d374baa1d0
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-04-19 18:10:03 -0700

    Time: 182 ms (30.12%) | Memory: 79.8 MB (76.91%) - LeetSync

--- 460-lfu-cache/lfu-cache.py
+++ 460-lfu-cache/lfu-cache.py
@@ -1,77 +1,71 @@
 class Node:
-    def __init__(self, key=-1, next=None, prev=None):
+    def __init__(self, key=-1, val=-1):
+        self.val = val
         self.key = key
-        self.next = next
-        self.prev = prev
-
-class LL:
-    def __init__(self):
-        head, tail = Node(), Node()
-        head.next, tail.prev = tail, head
-        self.head = head
-        self.tail = tail
-        self.key_node_map = {}
-    
-    def add(self, prev_node, curr_node, next_node):
-        prev_node.next, next_node.prev = curr_node, curr_node
-        curr_node.next, curr_node.prev = next_node, prev_node
-        self.key_node_map[curr_node.key] = curr_node
-        return curr_node.key
-
-    def add_head(self, key):
-        prev_node = self.head
-        curr_node = Node(key)
-        next_node = self.head.next
-        return self.add(prev_node=prev_node, curr_node=curr_node, next_node=next_node)
-
-    def pop(self, key):
-        if key not in self.key_node_map: return -1
-        curr_node = self.key_node_map[key]
-        prev_node, next_node = curr_node.prev, curr_node.next
-        prev_node.next, next_node.prev = next_node, prev_node
-        del self.key_node_map[key]
-        return key
-
-    def pop_tail(self):
-        return self.pop(key=self.tail.prev.key)
-
-    def length(self):
-        return len(self.key_node_map)
+        self.next = None
+        self.prev = None
+        self.count = 1
 
 class LFUCache:
+
     def __init__(self, cap: int):
+        # {key: node}
+        # {count: {head: node, tail: node}}
+        #                       LRU
         self.cap = cap
-        self.lfu_count = 0
-        self.key_val_map = collections.defaultdict(int)
-        self.key_count_map = collections.defaultdict(int)
-        self.count_ll_map = collections.defaultdict(LL)
+        self.key_node_map = {}
+        self.count_node_map = {}
+        self.total_nodes = 0
+    
+    def remove_node(self, node):
+        prev_node, next_node = node.prev, node.next
+        if prev_node.val == -1 and next_node.val == -1:
+            del self.count_node_map[node.count]
+        else:
+            prev_node.next, next_node.prev = next_node, prev_node
+        del self.key_node_map[node.key]
+        self.total_nodes -= 1
 
-    def counter(self, key):
-        count = self.key_count_map[key]
-        self.key_count_map[key] += 1
-        self.count_ll_map[count].pop(key)
-        self.count_ll_map[count + 1].add_head(key)
-
-        if self.lfu_count == count and self.count_ll_map[count].length() == 0:
-            self.lfu_count += 1
-
+    def insert_node(self, node):
+        self.key_node_map[node.key] = node
+        if node.count not in self.count_node_map:
+            head_node, tail_node = Node(-1), Node(-1)
+            head_node.next, tail_node.prev = tail_node, head_node
+            self.count_node_map[node.count] = {'head': head_node, 'tail': tail_node}
+        head_tail_hashmap = self.count_node_map[node.count]
+        head_node = head_tail_hashmap['head']
+        next_node = head_node.next
+        head_node.next, node.prev = node, head_node
+        next_node.prev, node.next = node, next_node
+        self.total_nodes += 1
+        return node
 
     def get(self, key: int) -> int:
-        if key not in self.key_val_map: return -1
-        self.counter(key)
-        return self.key_val_map[key]
+        if key not in self.key_node_map: return -1
+        node = self.key_node_map[key]
+        self.remove_node(node)
+        node.count += 1
+        self.insert_node(node)
+        return node.val
 
-    def put(self, key: int, val: int) -> None:
-        if self.cap == 0: return
-        # remove lfu
-        if key not in self.key_val_map and self.cap == len(self.key_val_map):
-            lfu_key = self.count_ll_map[self.lfu_count].pop_tail()
-            del self.key_val_map[lfu_key]
-            del self.key_count_map[lfu_key]
-
-        self.key_val_map[key] = val
-        self.counter(key)
-        self.lfu_count = min(self.lfu_count, self.key_count_map[key])
+    def put(self, key: int, value: int) -> None:
+        # if key is found:
+        if key in self.key_node_map:
+            # get node from key
+            self.get(key)
+            node = self.key_node_map[key]
+            node.val = value
+            return
+        elif self.total_nodes == self.cap:
+            # if cap is reached:
+            # remove LRU node based on lowest count
+            min_key = min(self.count_node_map)
+            LRU_node = self.count_node_map[min_key]['tail'].prev
+            self.remove_node(LRU_node)
+        # insert new node in key:node
+        node = Node(key, value)
+        # insert new node to count next to head
+        self.insert_node(node)
 
 # Your LFUCache object will be instantiated and called as such:
 # obj = LFUCache(capacity)
