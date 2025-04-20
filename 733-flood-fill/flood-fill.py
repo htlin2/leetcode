d210da904d5bf499ed6021242df7605a247d93c7
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-04-20 12:49:03 -0700

    Time: 0 ms (100.00%) | Memory: 18.1 MB (26.23%) - LeetSync

--- 733-flood-fill/flood-fill.py
+++ 733-flood-fill/flood-fill.py
@@ -1,24 +1,16 @@
 class Solution:
-    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        ROWS, COLS = len(image), len(image[0])
-        directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
-        original_color = image[sr][sc]
+    def floodFill(self, grid: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        ROWS, COLS = len(grid), len(grid[0])
+        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        original_color = grid[sr][sc]
         def dfs(r, c):
-            if r < 0 or c < 0 or r >= ROWS or c >= COLS: return
-            if image[r][c] != original_color or image[r][c] == color: return
-            image[r][c] = color
+            if r >= ROWS or c >= COLS or r < 0 or c < 0:
+                return
+            if grid[r][c] != original_color or grid[r][c] == color:
+                return
+            grid[r][c] = color
             for dr, dc in directions:
                 dfs(r + dr, c + dc)
+            return
         dfs(sr, sc)
-        return image
-"""
-dfs starting from image[r][c]
-base case: return when image[r][c] != original_color
-set image[r][c] = color
-for loop to expand to 4 directions
-    recursively call dfs with 4 directions
-return image
-
-Time: O(m * n)
-Space: O(m * n)?? call stack
-"""
+        return grid
