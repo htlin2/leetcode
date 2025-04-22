340ad9215ef0a6c4f1522035bef8536cd45424b4
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-04-21 19:03:31 -0700

    Time: 11 ms (15.56%) | Memory: 18.5 MB (92.58%) - LeetSync

--- 1028-interval-list-intersections/interval-list-intersections.py
+++ 1028-interval-list-intersections/interval-list-intersections.py
@@ -3,14 +3,30 @@ class Solution:
         res = []
         i, j = 0, 0
         while i < len(first) and j < len(second):
-            start_i, end_i = first[i]
-            start_j, end_j = second[j]
-            closed_start = max(start_i, start_j)
-            closed_end = min(end_i, end_j)
+            i_start, i_end = first[i]
+            j_start, j_end = second[j]
+            closed_start = max(i_start, j_start)
+            closed_end = min(i_end, j_end)
             if closed_start <= closed_end:
                 res.append([closed_start, closed_end])
-            if end_i < end_j:
+            if i_end <= j_end:
                 i += 1
             else:
                 j += 1
         return res
+"""
+interval
+Input: firstList = [[0,2],[5,10],[13,23],[24,25]], secondList = [[1,5],[8,12],[15,24],[25,26]]
+Output: [[1,2],[5,5],[8,10],[15,23],[24,24],[25,25]]
+
+first
+[0,2],[5,10],[13,23],[24,25]
+   i
+
+second
+[1,5],[8,12],[15,24],[25,26]
+   j
+max start, min end
+
+move smaller end to next
+"""
