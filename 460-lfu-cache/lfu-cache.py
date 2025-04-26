febfbe2efb19f179d6a24d447f547cee08eb1bb1
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-04-26 13:16:29 -0700

    Time: 169 ms (38.28%) | Memory: 79.8 MB (86.25%) - LeetSync

--- 460-lfu-cache/lfu-cache.py
+++ 460-lfu-cache/lfu-cache.py
@@ -1,71 +1,63 @@
 class Node:
-    def __init__(self, key=-1, val=-1):
-        self.val = val
+    def __init__(self, key, val):
         self.key = key
-        self.next = None
+        self.val = val
+        self.count = 0
         self.prev = None
-        self.count = 1
+        self.next = None
 
 class LFUCache:
-
     def __init__(self, cap: int):
-        # {key: node}
-        # {count: {head: node, tail: node}}
-        #                       LRU
         self.cap = cap
-        self.key_node_map = {}
-        self.count_node_map = {}
         self.total_nodes = 0
-    
-    def remove_node(self, node):
+        self.key_node_map = {} # key: node
+        self.count_ll_map = {} # count: {head, tail}
+
+    def insert(self, node):
+        self.key_node_map[node.key] = node
+        self.total_nodes += 1
+        node.count += 1
+        if node.count not in self.count_ll_map:
+            head, tail = Node(-1, -1), Node(-1, -1)
+            head.next, tail.prev = tail, head
+            self.count_ll_map[node.count] = {'head': head, 'tail': tail}
+        head = self.count_ll_map[node.count]['head']
+        next_node = head.next
+        head.next, node.prev = node, head
+        node.next, next_node.prev = next_node, node
+        return node
+
+    def remove(self, node):
+        self.total_nodes -= 1
+        del self.key_node_map[node.key]
         prev_node, next_node = node.prev, node.next
         if prev_node.val == -1 and next_node.val == -1:
-            del self.count_node_map[node.count]
-        else:
-            prev_node.next, next_node.prev = next_node, prev_node
-        del self.key_node_map[node.key]
-        self.total_nodes -= 1
-
-    def insert_node(self, node):
-        self.key_node_map[node.key] = node
-        if node.count not in self.count_node_map:
-            head_node, tail_node = Node(-1), Node(-1)
-            head_node.next, tail_node.prev = tail_node, head_node
-            self.count_node_map[node.count] = {'head': head_node, 'tail': tail_node}
-        head_tail_hashmap = self.count_node_map[node.count]
-        head_node = head_tail_hashmap['head']
-        next_node = head_node.next
-        head_node.next, node.prev = node, head_node
-        next_node.prev, node.next = node, next_node
-        self.total_nodes += 1
+            del self.count_ll_map[node.count]
+        prev_node.next, next_node.prev = next_node, prev_node
         return node
 
     def get(self, key: int) -> int:
-        if key not in self.key_node_map: return -1
+        if key not in self.key_node_map:
+            return -1
         node = self.key_node_map[key]
-        self.remove_node(node)
-        node.count += 1
-        self.insert_node(node)
+        self.remove(node)
+        self.insert(node)
         return node.val
 
     def put(self, key: int, value: int) -> None:
-        # if key is found:
         if key in self.key_node_map:
-            # get node from key
-            self.get(key)
             node = self.key_node_map[key]
             node.val = value
-            return
-        elif self.total_nodes == self.cap:
-            # if cap is reached:
-            # remove LRU node based on lowest count
-            min_key = min(self.count_node_map)
-            LRU_node = self.count_node_map[min_key]['tail'].prev
-            self.remove_node(LRU_node)
-        # insert new node in key:node
-        node = Node(key, value)
-        # insert new node to count next to head
-        self.insert_node(node)
+            self.get(node.key)
+        else:
+            if self.total_nodes == self.cap:
+                min_count_key = min(self.count_ll_map)
+                tail_node = self.count_ll_map[min_count_key]['tail']
+                LRU_node = tail_node.prev
+                self.remove(LRU_node)
+            node = Node(key, value)
+            self.insert(node)
+
 
 # Your LFUCache object will be instantiated and called as such:
 # obj = LFUCache(capacity)
