81f9557811dd1c8afdfdc2f01f335c0d7bce1bbd
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-04-26 12:22:36 -0700

    Time: 49 ms (71.69%) | Memory: 29.1 MB (37.65%) - LeetSync

--- 1762-furthest-building-you-can-reach/furthest-building-you-can-reach.py
+++ 1762-furthest-building-you-can-reach/furthest-building-you-can-reach.py
@@ -1,16 +1,23 @@
 class Solution:
     def furthestBuilding(self, heights: List[int], bricks: int, ladders: int) -> int:
         max_heap = []
-        for i in range(len(heights) - 1):
-            delta = heights[i] - heights[i + 1]
-            if delta >= 0:
+        N = len(heights)
+        for i in range(N - 1):
+            if heights[i] >= heights[i + 1]:
                 continue
-            delta *= -1
-            bricks -= delta
-            heapq.heappush(max_heap, -delta)
+            bricks_need = heights[i + 1] - heights[i]
+            heapq.heappush(max_heap, -bricks_need)
+            bricks -= bricks_need
             if bricks < 0:
-                if ladders == 0:
+                if not ladders:
                     return i
                 ladders -= 1
                 bricks += -heapq.heappop(max_heap)
-        return len(heights) - 1
+        return N - 1
+"""
+max_heap = 5
+Input: heights = [4,12,2,7,3,18,20,3,19], bricks = 10, ladders = 1
+Output: 7
+[4,12,2,7,3,18,20,3,19]
+      i 
+"""
