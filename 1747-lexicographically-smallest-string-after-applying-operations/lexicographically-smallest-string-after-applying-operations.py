4059f640c201d98af0cef1c5d3509709395f29f7
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-04-27 12:41:01 -0700

    Time: 870 ms (29.79%) | Memory: 55 MB (6.38%) - LeetSync

--- 1747-lexicographically-smallest-string-after-applying-operations/lexicographically-smallest-string-after-applying-operations.py
+++ 1747-lexicographically-smallest-string-after-applying-operations/lexicographically-smallest-string-after-applying-operations.py
@@ -1,23 +1,23 @@
 class Solution:
     def findLexSmallestString(self, s: str, a: int, b: int) -> str:
         combo = set()
+        res = s
         def dfs(s):
+            nonlocal res
             if s in combo: return
             combo.add(s)
-            # add or skip
-            res = list(s)
+            if s < res:
+                res = s
+            # add
+            temp = list(s)
             for i in range(len(s)):
                 if i % 2 == 1:
                     int_num = (int(s[i]) + a) % 10
-                    res[i] = str(int_num)
-            dfs(''.join(res))
-            # rotate or skip
-            res = list(s)
-            first = res[:b]
-            second = res[-(len(s) - b):]
-            res = second + first
-            dfs(''.join(res))
+                    temp[i] = str(int_num)
+            dfs(''.join(temp))
+            # rotate
+            temp = s[-b:] + s[:-b]
+            dfs(''.join(temp))
             return
         dfs(s)
-        sorted_combo = sorted(list(combo))
-        return sorted_combo[0]
+        return res
