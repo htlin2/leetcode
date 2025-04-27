7a6b1ff19145015b8ce2b2dbb5af2ecc6d203bd7
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-04-27 11:13:07 -0700

    Time: 0 ms (100.00%) | Memory: 18 MB (32.16%) - LeetSync

--- 50-powx-n/powx-n.py
+++ 50-powx-n/powx-n.py
@@ -1,12 +1,16 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        is_positive = n >= 0
+        if n == 0: return 1
+        if n == 1: return x
+        is_negative = True if n < 0 else False
         n = abs(n)
-        ans = 1
-        while n:
-            if n % 2 == 1:
-                n -= 1
-                ans *= x
-            n = n // 2
-            x = x * x
-        return ans if is_positive else 1 / ans
+        curr = self.myPow(x * x, n // 2)
+        if n % 2:
+            curr *= x
+        return 1/curr if is_negative else curr
+"""
+2^10
+4^5
+16^2 * 4
+
+"""
