1a61b973640885d2fe3955a57ea93588cffcd6fc
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-04-27 12:51:49 -0700

    Time: 662 ms (61.70%) | Memory: 19.1 MB (94.33%) - LeetSync

--- 1747-lexicographically-smallest-string-after-applying-operations/lexicographically-smallest-string-after-applying-operations.py
+++ 1747-lexicographically-smallest-string-after-applying-operations/lexicographically-smallest-string-after-applying-operations.py
@@ -1,23 +1,27 @@
 class Solution:
     def findLexSmallestString(self, s: str, a: int, b: int) -> str:
-        combo = set()
+        q = collections.deque([s])
+        visited = set()
+        visited.add(s)
         res = s
-        def dfs(s):
-            nonlocal res
-            if s in combo: return
-            combo.add(s)
-            if s < res:
-                res = s
-            # add
-            temp = list(s)
-            for i in range(len(s)):
-                if i % 2 == 1:
-                    int_num = (int(s[i]) + a) % 10
-                    temp[i] = str(int_num)
-            dfs(''.join(temp))
-            # rotate
-            temp = s[-b:] + s[:-b]
-            dfs(''.join(temp))
-            return
-        dfs(s)
+        while q:
+            for _ in range(len(q)):
+                first = q.popleft()
+                if first < res:
+                    res = first
+                # add
+                first_arr = list(first)
+                for i in range(0, len(s), 2):
+                    if i + 1 < len(s):
+                        str_num = (int(first_arr[i + 1]) + a) % 10
+                        first_arr[i + 1] = str(str_num)
+                first_str = ''.join(first_arr)
+                if first_str not in visited:
+                    q.append(first_str)
+                    visited.add(first_str)
+                # rotate
+                rotated = first[-b:] + first[:-b]
+                if rotated not in visited:
+                    q.append(rotated)
+                    visited.add(rotated)
         return res
