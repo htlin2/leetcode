615b9f78e2d85109d30312481cbcd08397c3006a
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-04-28 21:20:59 -0700

    Time: 100 ms (28.60%) | Memory: 19 MB (17.55%) - LeetSync

--- 1648-minimum-insertions-to-balance-a-parentheses-string/minimum-insertions-to-balance-a-parentheses-string.py
+++ 1648-minimum-insertions-to-balance-a-parentheses-string/minimum-insertions-to-balance-a-parentheses-string.py
@@ -1,6 +1,6 @@
 class Solution:
     def minInsertions(self, s: str) -> int:
-        inserts = 0
+        res = 0
         stack = []
         i = 0
         while i < len(s):
@@ -8,19 +8,20 @@ class Solution:
                 stack.append(s[i])
                 i += 1
             else:
-                # s[i] == ')'
+                # ')'
                 if i + 1 < len(s) and s[i + 1] == ')':
                     if stack:
                         stack.pop()
                     else:
-                        inserts += 1
+                        res += 1
                     i += 2
                 else:
                     if stack:
                         stack.pop()
-                        inserts += 1
+                        res += 1
                         i += 1
                     else:
-                        inserts += 2
+                        res += 2
                         i += 1
-        return inserts + len(stack) * 2
+        res += len(stack) * 2
+        return res
