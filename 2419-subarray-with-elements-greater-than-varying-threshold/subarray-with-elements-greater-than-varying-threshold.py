80255f2afec2a9e5d1d5dc83a21f2392b8250550
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-04-29 20:01:05 -0700

    Time: 116 ms (74.47%) | Memory: 31.4 MB (95.74%) - LeetSync

--- 2419-subarray-with-elements-greater-than-varying-threshold/subarray-with-elements-greater-than-varying-threshold.py
+++ 2419-subarray-with-elements-greater-than-varying-threshold/subarray-with-elements-greater-than-varying-threshold.py
@@ -1,13 +1,16 @@
 class Solution:
     def validSubarraySize(self, nums: List[int], threshold: int) -> int:
         nums.append(0)
-        stack = [] # monotonic stack increasing
-        for i in range(len(nums)):
-            next_smaller = nums[i]
-            while stack and nums[stack[-1]] > next_smaller:
-                idx = stack.pop()
+        stack = []
+        for i, num in enumerate(nums):
+            while stack and nums[stack[-1]] >= num:
+                prev_small_idx = stack.pop()
                 k = i - stack[-1] - 1 if stack else i
-                if nums[idx] > threshold / k:
+                if nums[prev_small_idx] > threshold / k:
                     return k
             stack.append(i)
         return -1
+"""
+monotonic stack increasing
+
+"""
