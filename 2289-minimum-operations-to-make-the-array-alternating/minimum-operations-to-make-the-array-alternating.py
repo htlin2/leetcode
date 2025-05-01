94ad736f37a7e6159b1e7a356214fadc27f1fe5d
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-04-30 18:50:17 -0700

    Time: 135 ms (60.00%) | Memory: 39.1 MB (10.52%) - LeetSync

--- 2289-minimum-operations-to-make-the-array-alternating/minimum-operations-to-make-the-array-alternating.py
+++ 2289-minimum-operations-to-make-the-array-alternating/minimum-operations-to-make-the-array-alternating.py
@@ -1,15 +1,15 @@
 class Solution:
     def minimumOperations(self, nums: List[int]) -> int:
-        odd = collections.Counter(nums[1::2])
-        even = collections.Counter(nums[::2])
-        odd_sorted = sorted(odd.items(), key=lambda x: x[1], reverse=True)
-        even_sorted = sorted(even.items(), key=lambda x: x[1], reverse=True)
-        odd_sorted.append((-1, 0))
-        even_sorted.append((-1, 0))
         N = len(nums)
-        if odd_sorted[0][0] != even_sorted[0][0]:
-            return N - (odd_sorted[0][-1] + even_sorted[0][-1])
+        even_counter = collections.Counter(nums[::2])
+        odd_counter = collections.Counter(nums[1::2])
+        even_tuples = sorted(even_counter.items(), key=lambda x: x[-1], reverse=True)
+        odd_tuples = sorted(odd_counter.items(), key=lambda x: x[-1], reverse=True)
+        even_tuples.append((-1, 0))
+        odd_tuples.append((-1, 0))
+        if even_tuples[0][0] != odd_tuples[0][0]:
+            return N - even_tuples[0][1] - odd_tuples[0][1]
         else:
-            first = odd_sorted[0][-1] + even_sorted[1][-1]
-            second = odd_sorted[1][-1] + even_sorted[0][-1]
+            first = even_tuples[0][1] + odd_tuples[1][1]
+            second = even_tuples[1][1] + odd_tuples[0][1]
             return N - max(first, second)
