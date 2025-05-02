34057e1360a7d43c66cf79706bd4589729d068ed
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-05-01 20:57:35 -0700

    Time: 818 ms (30.99%) | Memory: 18.1 MB (36.04%) - LeetSync

--- 277-find-the-celebrity/find-the-celebrity.py
+++ 277-find-the-celebrity/find-the-celebrity.py
@@ -5,14 +5,12 @@
 class Solution:
     def findCelebrity(self, n: int) -> int:
         candidate = 0
-        # find candidate
         for i in range(1, n):
             if knows(candidate, i):
                 candidate = i
         
-        # verify candidate
         for i in range(n):
             if i == candidate: continue
-            if knows(candidate, i) or not knows(i, candidate):
+            if not knows(i, candidate) or knows(candidate, i):
                 return -1
         return candidate
