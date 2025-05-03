d4c1ff0f75d9f685490c13b2accc16db88a958e7
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-05-02 20:08:14 -0700

    Time: 4 ms (7.09%) | Memory: 17.8 MB (70.17%) - LeetSync

--- 2050-count-good-numbers/count-good-numbers.py
+++ 2050-count-good-numbers/count-good-numbers.py
@@ -1,18 +1,26 @@
 class Solution:
     def countGoodNumbers(self, n: int) -> int:
-        MOD = 10**9 + 7
+        MOD = 10 ** 9 + 7
+        res = 1
+        def pow(base, n):
+            if n == 0:
+                return 1
+            half = pow(base * base % MOD, n // 2) % MOD
+            if n % 2 == 1:
+                half *= base
+            return half % MOD
+        odd = 1 if n % 2 else 0
+        return (pow(5, n // 2 + odd) * pow(4, n // 2)) % MOD
+"""
+n = 1
+5
 
-        def pow(x, n):
-            res = 1
-            while n:
-                is_odd = n % 2 == 1
-                if is_odd:
-                    res = res * x % MOD
-                    n -= 1
-                else:
-                    x = x * x % MOD
-                    n = n // 2
-            return res % MOD
-        odd = n % 2 + n // 2
-        even = n // 2
-        return (pow(5, odd) * pow(4, even)) % MOD
+n = 2
+5 * 4 = 20
+
+n = 3
+5 * 4 * 5 = 100
+
+n = 4
+5 * 4 * 5 * 4 = 400
+"""
