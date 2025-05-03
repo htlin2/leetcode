ecad430dcd5b38f70658ba6999e1fe7af6d399eb
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-05-02 22:07:09 -0700

    Time: 1 ms (92.88%) | Memory: 18.4 MB (49.74%) - LeetSync

--- 437-path-sum-iii/path-sum-iii.py
+++ 437-path-sum-iii/path-sum-iii.py
@@ -5,19 +5,20 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def pathSum(self, root: Optional[TreeNode], target: int) -> int:
-        prefix_hashmap = collections.defaultdict(int)
-        prefix_hashmap[0] += 1
+    def pathSum(self, root: Optional[TreeNode], t: int) -> int:
+        prefix = collections.defaultdict(int)
+        prefix[0] += 1
         res = 0
         def dfs(node, curr):
             nonlocal res
             if not node: return
             curr += node.val
-            delta = curr - target
-            res += prefix_hashmap[delta]
-            prefix_hashmap[curr] += 1
+            delta = curr - t
+            res += prefix[delta]
+            prefix[curr] += 1
             dfs(node.left, curr)
             dfs(node.right, curr)
-            prefix_hashmap[curr] -= 1
+            prefix[curr] -= 1
+            return
         dfs(root, 0)
         return res
