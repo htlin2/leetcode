ae77805fefff7233a57372b0974de4c207e327d1
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-05-03 11:26:07 -0700

    Time: 150 ms (26.61%) | Memory: 18.3 MB (35.08%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -1,24 +1,36 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        counter = collections.Counter(t)
-        window = collections.defaultdict(int)
-        keys = 0
+        t_counter = collections.Counter(t)
+        window_counter = {}
+        matched_chars = 0
         left = 0
         res = ''
         for right in range(len(s)):
-            if s[right] in counter:
-                window[s[right]] += 1
-                if window[s[right]] == counter[s[right]]:
-                    keys += 1
-            while keys == len(counter):
-                if not res or len(res) >= right - left + 1:
-                    substr = s[left:right+1]
-                    res = substr
-                if s[left] in counter:
-                    if window[s[left]] == counter[s[left]]:
-                        keys -= 1
-                    window[s[left]] -= 1
-                    if window[s[left]] == 0:
-                        del window[s[left]]
+            # expand window logic
+            if s[right] in t_counter:
+                if s[right] not in window_counter:
+                    window_counter[s[right]] = 0
+                window_counter[s[right]] += 1
+                if window_counter[s[right]] == t_counter[s[right]]:
+                    matched_chars += 1
+            # shrink window logic
+            while left <= right and matched_chars == len(t_counter):
+                if not res or len(res) >= (right - left + 1):
+                    res = s[left:right + 1]
+                if s[left] in window_counter:
+                    if window_counter[s[left]] == t_counter[s[left]]:
+                        matched_chars -= 1
+                    window_counter[s[left]] -= 1
+                    if window_counter[s[left]] == 0:
+                        del window_counter[s[left]]
                 left += 1
         return res
+"""
+hashmap + sliding window
+Input: s = "ADOBECODEBANC", t = "ABC"
+t_counter = {A: 1, B: 1, C: 1}
+res = ADOBEC
+window_counter = {A: 1, B: 1, C: 1}
+ A D O B E C O D E B A N C
+                     L   R
+"""
