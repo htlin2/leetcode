1aa2e63a49b8b5982082431c3d1f0b1190b4d2f6
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-05-03 11:08:41 -0700

    Time: 11 ms (84.65%) | Memory: 17.8 MB (80.89%) - LeetSync

--- 698-partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.py
+++ 698-partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.py
@@ -1,21 +1,23 @@
 class Solution:
     def canPartitionKSubsets(self, nums: List[int], k: int) -> bool:
         total = sum(nums)
-        if total % k: return False
+        if total % k:
+            return False
         nums.sort(reverse=True)
         target = total // k
         visited = set()
-        def dfs(i, curr_sum, k):
-            if k == 0: return True
-            if curr_sum == target: return dfs(0, 0, k - 1)
+        def dfs(i, curr_sum, rem_k):
+            if rem_k == 0: 
+                return True
+            if curr_sum == target: 
+                return dfs(0, 0, rem_k - 1)
             for j in range(i, len(nums)):
-                if j in visited: continue
-                if curr_sum + nums[j] <= target:
-                    visited.add(j)
-                    if dfs(j + 1, curr_sum + nums[j], k):
-                        return True
-                    visited.remove(j)
-                    if curr_sum == 0:
-                        break
+                if j in visited or curr_sum + nums[j] > target: continue
+                visited.add(j)
+                if dfs(j + 1, curr_sum + nums[j], rem_k):
+                    return True
+                visited.remove(j)
+                if curr_sum == 0:
+                    break
             return False
         return dfs(0, 0, k)
