29523160ea21a3036db20a6024c4b279fff6320d
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-05-02 21:55:48 -0700

    Time: 0 ms (100.00%) | Memory: 17.5 MB (97.88%) - LeetSync

--- 1923-sentence-similarity-iii/sentence-similarity-iii.py
+++ 1923-sentence-similarity-iii/sentence-similarity-iii.py
@@ -1,40 +1,19 @@
 class Solution:
     def areSentencesSimilar(self, s1: str, s2: str) -> bool:
-        s1_s = s1.split(' ')
-        s2_s = s2.split(' ')
-        if len(s1) < len(s2): return self.areSentencesSimilar(s2, s1)
-        s1 = s1_s
-        s2 = s2_s
+        if len(s2) > len(s1):
+            return self.areSentencesSimilar(s2, s1)
+        s1 = s1.split(' ')
+        s2 = s2.split(' ')
         n1, n2 = len(s1), len(s2)
-        s1_l, s1_r = 0, n1 - 1
-        s2_l, s2_r = 0, n2 - 1
-        while s2_l <= s2_r and s1[s1_l] == s2[s2_l]:
-            s1_l += 1
-            s2_l += 1
-        while 0 <= s2_r and s1[s1_r] == s2[s2_r]:
-            s1_r -= 1
-            s2_r -= 1
-        if s2_r < s2_l:
-            return True
-        return False
+        s1_left, s1_right = 0, n1 - 1
+        s2_left, s2_right = 0, n2 - 1
+        while s1_left < n1 and s2_left < n2 and s1[s1_left] == s2[s2_left]:
+            s1_left += 1
+            s2_left += 1
+        while 0 <= s1_right and 0 <= s2_right and s1[s1_right] == s2[s2_right]:
+            s1_right -= 1
+            s2_right -= 1
+        return s2_right < s2_left
 """
 two pointers
-Input: sentence1 = "My name is Haley", sentence2 = "My Haley"
-
-s1 = My, name, is, Haley
-     l               r
-s2 = My, Haley
-     l    r
-
-
-Input: sentence1 = "Eating right now", sentence2 = "Eating"
-s1 = Eating, right, now
-      l              r
-s2 = Eating
-       lr
-
-s1 = Lucccky
-
-s2 = Luky
-
 """
