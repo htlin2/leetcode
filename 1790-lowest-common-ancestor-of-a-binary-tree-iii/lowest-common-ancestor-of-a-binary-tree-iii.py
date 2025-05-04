9f7d67a3224f30015c21f5c513d679c90a2e95f5
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-05-04 10:21:04 -0700

    Time: 54 ms (53.25%) | Memory: 22 MB (5.31%) - LeetSync

---
+++ 1790-lowest-common-ancestor-of-a-binary-tree-iii/lowest-common-ancestor-of-a-binary-tree-iii.py
@@ -0,0 +1,25 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+        self.parent = None
+"""
+
+class Solution:
+    def lowestCommonAncestor(self, p: 'Node', q: 'Node') -> 'Node':
+        root = p
+        while root and root.parent:
+            root = root.parent
+        def dfs(node):
+            if not node: return None
+            if node == p or node == q:
+                return node
+            left = dfs(node.left)
+            right = dfs(node.right)
+            if left and right:
+                return node
+            return left or right
+        return dfs(root)
