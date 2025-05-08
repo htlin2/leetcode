55a637bc1721b08fbdf31902b76a2c4e867d425b
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-05-07 21:07:24 -0700

    Time: 3 ms (47.86%) | Memory: 20.9 MB (7.01%) - LeetSync

--- 261-graph-valid-tree/graph-valid-tree.py
+++ 261-graph-valid-tree/graph-valid-tree.py
@@ -1,61 +1,19 @@
-class UnionFind:
-    def __init__(self, N):
-        self.parents = [i for i in range(N)]
-        self.ranks = [1] * N
-
-    def find(self, n):
-        if self.parents[n] == n: return n
-        self.parents[n] = self.find(self.parents[self.parents[n]])
-        return self.parents[n]
-
-    def union(self, n1, n2):
-        p1, p2 = self.find(n1), self.find(n2)
-        if p1 == p2: return False
-        if self.ranks[p1] >= self.ranks[p2]:
-            self.ranks[p1] += self.ranks[p2]
-            self.parents[p2] = p1
-        else:
-            self.ranks[p2] += self.ranks[p1]
-            self.parents[p1] = p2
-        return True
-
 class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
-        uf = UnionFind(n)
+        adj = collections.defaultdict(set)
         for n1, n2 in edges:
-            if not uf.union(n1, n2):
-                return False
-        for i in range(n):
-            uf.parents[i] = uf.find(i)
-        all_parents = set(uf.parents)
-        return len(all_parents) == 1
+            adj[n1].add(n2)
+            adj[n2].add(n1)
+        visited = set()
+        def has_cycle(node, prev):
+            if node in visited: return True
+            visited.add(node)
+            for nei in adj[node]:
+                if nei == prev: continue
+                if has_cycle(nei, node):
+                    return True
+            return False
 
-"""
-Union Find?
-
-DFS / BFS
-edge case, 2 roots
-1 -> 2 -> 3
-4 -> 5
-not a valid tree
-
-how to detec cycle?
-
-example 2=
-Input: n = 5, edges = [[0,1],[1,2],[2,3],[1,3],[1,4]]
-                                           i
-0 -> 1 -> 2 -> 3
-     1 -> 3 (x) return false
-
-Time: O(n)
-Space: O(n)
-
-[
-    [0,1],
-    [2,3],
-    [1,2]
-]
-0 -> 1
-2 -> 3
-1 -> 2
-"""
+        if has_cycle(0, -1):
+            return False
+        return len(visited) == n
