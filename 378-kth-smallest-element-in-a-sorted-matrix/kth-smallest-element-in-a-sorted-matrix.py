6ad52ba3ec9216b837c3b5358732a10ddd4350db
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-05-08 19:58:46 -0700

    Time: 27 ms (43.90%) | Memory: 21.3 MB (52.37%) - LeetSync

--- 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.py
+++ 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.py
@@ -1,26 +1,22 @@
 class Solution:
     def kthSmallest(self, grid: List[List[int]], k: int) -> int:
-        left, right = grid[0][0], grid[-1][-1]
-        def get_count(mid):
-            count = 0
-            for row in grid:
-                count += bisect.bisect_right(row, mid)
-            return count
-
-        while left <= right:
-            mid = (left + right) // 2
-            count = get_count(mid)
-            if count < k:
-                left = mid + 1
-            else:
-                right = mid - 1
-        return left
-
+        min_heap = [] # (val, row, col)
+        for c in range(len(grid[0])):
+            heapq.heappush(min_heap, (grid[0][c], 0, c))
+        while k:
+            k -= 1
+            val, row, col = heapq.heappop(min_heap)
+            if row + 1 < len(grid):
+                heapq.heappush(min_heap, (grid[row + 1][col], row + 1, col))
+        return val
 """
-1. heap + linked list
-O(n log n + k)
+k = 8
+Input: matrix = [
+    [ 1, 5, 9],
+    [10,11,13],
+    [12,13,15]
+], 
 
-2. binary search
-range = m
-O(n log m)
+min_heap = , , 13
+k = 1
 """
