54991f7d9ea499f511c1a8a3d77d8a7af55917df
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-05-08 19:18:58 -0700

    Time: 748 ms (67.44%) | Memory: 17.9 MB (94.46%) - LeetSync

--- 277-find-the-celebrity/find-the-celebrity.py
+++ 277-find-the-celebrity/find-the-celebrity.py
@@ -8,9 +8,9 @@ class Solution:
         for i in range(1, n):
             if knows(candidate, i):
                 candidate = i
-        
-        for i in range(n):
+
+        for i in range(0, n):
             if i == candidate: continue
-            if not knows(i, candidate) or knows(candidate, i):
+            if knows(candidate, i) or not knows(i, candidate):
                 return -1
         return candidate
