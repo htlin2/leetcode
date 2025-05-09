06fddad6873e4c81c41529bc86fce2048fbd3daa
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-05-08 19:12:51 -0700

    Time: 7 ms (58.14%) | Memory: 20.9 MB (5.19%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.py
+++ 547-number-of-provinces/number-of-provinces.py
@@ -1,32 +1,31 @@
-class UnionFind:
-    def __init__(self, N):
-        self.parents = [i for i in range(N)]
-        self.counts = [1] * N
-
-    def find(self, n):
-        if n == self.parents[n]: return n
-        self.parents[n] = self.find(self.parents[self.parents[n]])
-        return self.parents[n]
-
-    def union(self, n1, n2):
-        p1, p2 = self.find(n1), self.find(n2)
-        if p1 == p2: return False
-        c1, c2 = self.counts[p1], self.counts[p2]
-        if c1 >= c2:
-            self.counts[p1] += self.counts[p2]
-            self.parents[p2] = p1
-        else:
-            self.counts[p2] += self.counts[p1]
-            self.parents[p1] = p2
-        return True
-
 class Solution:
     def findCircleNum(self, grid: List[List[int]]) -> int:
-        N = len(grid)
-        uf = UnionFind(N)
-        for r in range(N):
-            for c in range(r + 1, N):
-                if grid[r][c] == 1:
-                    uf.union(r, c)
-        parents = set([uf.find(i) for i in range(N)])
-        return len(parents)
+        adj = collections.defaultdict(set)
+        for r in range(len(grid)):
+            for c in range(r + 1, len(grid)):
+                if grid[r][c]:
+                    adj[r].add(c)
+                    adj[c].add(r)
+        print(adj)
+        visited = set()
+        def dfs(i):
+            if i in visited: return
+            visited.add(i)
+            for nei in adj[i]:
+                dfs(nei)
+            return
+        res = 0
+        for i in range(len(grid)):
+            if i not in visited:
+                dfs(i)
+                res += 1
+        return res
+"""
+Input: isConnected = [
+    [1,1,0],
+    [1,1,0],
+    [0,0,1]
+]
+Output: 2
+
+"""
