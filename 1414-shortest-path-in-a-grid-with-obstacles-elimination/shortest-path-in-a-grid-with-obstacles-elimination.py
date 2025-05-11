ca22be60ef0919911afa70da12ffe3c241d45eb8
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-05-11 11:17:53 -0700

    Time: 15 ms (93.47%) | Memory: 19.2 MB (80.21%) - LeetSync

--- 1414-shortest-path-in-a-grid-with-obstacles-elimination/shortest-path-in-a-grid-with-obstacles-elimination.py
+++ 1414-shortest-path-in-a-grid-with-obstacles-elimination/shortest-path-in-a-grid-with-obstacles-elimination.py
@@ -4,19 +4,22 @@ class Solution:
         if k >= ROWS + COLS - 2:
             return ROWS + COLS - 2
         directions = [(1, 0), (-1, 0), (0, -1), (0, 1)] # (r, c)
-        q = collections.deque([(0, 0, k, 0)]) # (r, c, rem_k, count)
+        q = collections.deque([(0, 0, k)]) # (r, c, rem_k, count)
         visited = set((0, 0, k))
+        count = 0
         while q:
-            r, c, rem_k, count = q.popleft()
-            if r == ROWS - 1 and c == COLS - 1: return count
-            for dr, dc in directions:
-                nr, nc = dr + r, dc + c
-                if nr < 0 or nc < 0 or nr >= ROWS or nc >= COLS: continue
-                if grid[nr][nc] == 1 and not rem_k: continue
-                new_k = rem_k - 1 if grid[nr][nc] == 1 else rem_k
-                if (nr, nc, new_k) in visited: continue
-                q.append((nr, nc, new_k, count + 1))
-                visited.add((nr, nc, new_k))
+            for _ in range(len(q)):
+                r, c, rem_k = q.popleft()
+                if r == ROWS - 1 and c == COLS - 1: return count
+                for dr, dc in directions:
+                    nr, nc = dr + r, dc + c
+                    if nr < 0 or nc < 0 or nr >= ROWS or nc >= COLS: continue
+                    if grid[nr][nc] == 1 and not rem_k: continue
+                    new_k = rem_k - 1 if grid[nr][nc] == 1 else rem_k
+                    if (nr, nc, new_k) in visited: continue
+                    q.append((nr, nc, new_k))
+                    visited.add((nr, nc, new_k))
+            count += 1
         return -1
 """
 [
