d34a5cf5cdf9c660ed26c301a0d8aec8519f3e12
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-05-11 12:04:27 -0700

    Time: 0 ms (100.00%) | Memory: 17.9 MB (77.22%) - LeetSync

--- 797-rabbits-in-forest/rabbits-in-forest.py
+++ 797-rabbits-in-forest/rabbits-in-forest.py
@@ -2,8 +2,8 @@ class Solution:
     def numRabbits(self, ans: List[int]) -> int:
         res = 0
         counter = collections.Counter(ans)
-        for num, freq in counter.items():
+        for num, count in counter.items():
             group_size = num + 1
-            groups = math.ceil(freq / group_size)
+            groups = math.ceil(count / group_size)
             res += groups * group_size
         return res
