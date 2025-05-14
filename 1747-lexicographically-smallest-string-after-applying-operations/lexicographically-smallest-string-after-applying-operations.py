8f5bdc5b9ca4572ad09af361a973ca69f6aedfa1
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-05-13 18:48:18 -0700

    Time: 725 ms (48.93%) | Memory: 19.5 MB (63.95%) - LeetSync

--- 1747-lexicographically-smallest-string-after-applying-operations/lexicographically-smallest-string-after-applying-operations.py
+++ 1747-lexicographically-smallest-string-after-applying-operations/lexicographically-smallest-string-after-applying-operations.py
@@ -1,33 +1,25 @@
 class Solution:
     def findLexSmallestString(self, s: str, a: int, b: int) -> str:
-        def add(substr):
-            res = list(substr)
-            for i in range(len(res)):
-                if i % 2:
-                    res[i] = str((int(res[i]) + a) % 10)
-            return ''.join(res)
-
-        def rotate(substr):
-            front = substr[:b]
-            back = substr[b:]
-            return back + front
+        q = collections.deque([s])
+        visited = set([s])
         res = s
-        visited = set()
-        def dfs(substr):
-            nonlocal res
-            if int(substr) <= int(res):
-                res = substr
-            if substr in visited: return
-            visited.add(substr)
-            added_substr = add(substr)
-            dfs(added_substr)
-            rotated_substr = rotate(substr)
-            dfs(rotated_substr)
-            return
-        dfs(s)
+        while q:
+            for _ in range(len(q)):
+                first = q.popleft()
+                if int(first) <= int(res):
+                    res = first
+                arr_first = list(first)
+                for i in range(len(res)):
+                    if i % 2:
+                        int_digit = (int(arr_first[i]) + a) % 10
+                        str_digit = str(int_digit)
+                        arr_first[i] = str_digit
+                added_first = ''.join(arr_first)
+                if added_first not in visited:
+                    q.append(added_first)
+                    visited.add(added_first)
+                rotated_first = first[b:] + first[:b]
+                if rotated_first not in visited:
+                    q.append(rotated_first)
+                    visited.add(rotated_first)
         return res
-"""
-backtracking
-add a to odd idx
-rotate s to right by b
-"""
