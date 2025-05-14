6f7b963c6ab01ccea4ab1ea172c21fb46c12d541
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-05-13 21:51:57 -0700

    Time: 103 ms (73.22%) | Memory: 55.6 MB (10.85%) - LeetSync

--- 833-bus-routes/bus-routes.py
+++ 833-bus-routes/bus-routes.py
@@ -1,36 +1,23 @@
 class Solution:
     def numBusesToDestination(self, routes: List[List[int]], source: int, target: int) -> int:
-        adj = collections.defaultdict(set) # stop: route[]
-        q = collections.deque([(source, 0)]) # [stop, count]
+        adj = collections.defaultdict(set) # stop: [bus1, bus2]
         for i in range(len(routes)):
             for stop in routes[i]:
                 adj[stop].add(i)
-        
-        visited_route = set()
-        visited_stop = set()
-        visited_stop.add(source)
+        q = collections.deque([source])
+        visited_bus = set()
+        visited_stop = set([source])
+        count = 0
         while q:
-            stop, count = q.popleft()
-            if stop == target:
-                return count
-            for next_route in adj[stop]:
-                if next_route in visited_route: continue
-                visited_route.add(next_route)
-                for next_stop in routes[next_route]:
-                    if next_stop in visited_stop: continue
-                    visited_stop.add(next_stop)
-                    q.append((next_stop, count + 1))
+            for _ in range(len(q)):
+                stop = q.popleft()
+                if stop == target: return count
+                for next_bus in adj[stop]:
+                    if next_bus in visited_bus: continue
+                    visited_bus.add(next_bus)
+                    for next_stop in routes[next_bus]:
+                        if next_stop in visited_stop: continue
+                        visited_stop.add(next_stop)
+                        q.append(next_stop)
+            count += 1
         return -1
-"""
-source = 1, target = 6
-routes = [
-    [1,2,7],
-    [3,6,7]
-], 
-Output: 2
-1: [0]
-2: [0]
-3: [1]
-6: [1]
-7: [0,1]
-"""
