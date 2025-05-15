9870699261061762e2055485d09bfa1e667998ef
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-05-14 20:50:54 -0700

    Time: 382 ms (60.89%) | Memory: 19.7 MB (78.04%) - LeetSync

--- 744-network-delay-time/network-delay-time.py
+++ 744-network-delay-time/network-delay-time.py
@@ -1,20 +1,18 @@
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        adj = collections.defaultdict(list) # src: [(weight, dst)]
-        for src, dst, weight in times:
-            adj[src].append((weight, dst))
-        if not adj[k]: return -1
-        time = 0
-        visited = set() # n
-        min_heap = [(0, k)] # (weight, dst)
+        adj = collections.defaultdict(list)
+        for src, dst, time in times:
+            adj[src].append((time, dst))
+        min_heap = [(0, k)]
+        hashmap = collections.defaultdict(int)
         while min_heap:
-            weight, dst = heapq.heappop(min_heap)
-            if dst in visited: continue
-            time = max(weight, time)
-            visited.add(dst)
-            for nei_wei, nei_dst in adj[dst]:
-                if nei_dst in visited: continue
-                heapq.heappush(min_heap, (nei_wei + time, nei_dst))
-        if len(visited) != n:
-            return -1
-        return time
+            time, dst = heapq.heappop(min_heap)
+            if dst in hashmap: continue
+            hashmap[dst] = time
+            if len(hashmap) == n:
+                break
+            for nei_time, nei_dst in adj[dst]:
+                if nei_dst in hashmap: continue
+                heapq.heappush(min_heap, (time + nei_time, nei_dst))
+        if len(hashmap) != n: return -1
+        return max(hashmap.values())
