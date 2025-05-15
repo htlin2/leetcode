acf8e3d83f8fefdbc04aa88b49cbf5f9dfa913fa
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-05-14 23:23:17 -0700

    Time: 379 ms (68.55%) | Memory: 19.7 MB (55.59%) - LeetSync

--- 744-network-delay-time/network-delay-time.py
+++ 744-network-delay-time/network-delay-time.py
@@ -1,18 +1,16 @@
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
         adj = collections.defaultdict(list)
-        for src, dst, time in times:
-            adj[src].append((time, dst))
+        for src, dst, weight in times:
+            adj[src].append((weight, dst))
         min_heap = [(0, k)]
-        hashmap = collections.defaultdict(int)
+        shortest = {}
         while min_heap:
-            time, dst = heapq.heappop(min_heap)
-            if dst in hashmap: continue
-            hashmap[dst] = time
-            if len(hashmap) == n:
-                break
-            for nei_time, nei_dst in adj[dst]:
-                if nei_dst in hashmap: continue
-                heapq.heappush(min_heap, (time + nei_time, nei_dst))
-        if len(hashmap) != n: return -1
-        return max(hashmap.values())
+            weight, dst = heapq.heappop(min_heap)
+            if dst in shortest: continue
+            shortest[dst] = weight
+            for nei_weight, nei in adj[dst]:
+                if nei in shortest: continue
+                heapq.heappush(min_heap, (weight + nei_weight, nei))
+        if len(shortest) != n: return -1
+        return max(shortest.values())
