846b4d0d532d0375c56b22872846aecbcf92d97f
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-05-15 20:54:09 -0700

    Time: 4 ms (13.46%) | Memory: 18.1 MB (28.18%) - LeetSync

--- 443-string-compression/string-compression.py
+++ 443-string-compression/string-compression.py
@@ -1,22 +1,16 @@
 class Solution:
     def compress(self, chars: List[str]) -> int:
-        right = 0  # read pointer
-        left = 0   # write pointer
-
+        right = 0
+        left = 0
         while right < len(chars):
+            count = 1
             char = chars[right]
-            count = 0
-
-            # Count occurrences of the same character
-            while right < len(chars) and chars[right] == char:
+            while right + 1 < len(chars) and char == chars[right + 1]:
                 right += 1
                 count += 1
-
-            # Write character
+            right += 1
             chars[left] = char
             left += 1
-
-            # Write count if greater than 1
             if count > 1:
                 for c in str(count):
                     chars[left] = c
