4b000861990d210b6b3041626a77b69601c2698f
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-05-15 21:14:28 -0700

    Time: 707 ms (7.75%) | Memory: 24.3 MB (56.88%) - LeetSync

--- 1414-shortest-path-in-a-grid-with-obstacles-elimination/shortest-path-in-a-grid-with-obstacles-elimination.py
+++ 1414-shortest-path-in-a-grid-with-obstacles-elimination/shortest-path-in-a-grid-with-obstacles-elimination.py
@@ -1,41 +1,24 @@
 class Solution:
     def shortestPath(self, grid: List[List[int]], k: int) -> int:
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
         ROWS, COLS = len(grid), len(grid[0])
-        if k >= ROWS + COLS - 2:
-            return ROWS + COLS - 2
-        directions = [(1, 0), (-1, 0), (0, -1), (0, 1)] # (r, c)
-        q = collections.deque([(0, 0, k)]) # (r, c, rem_k, count)
-        visited = set((0, 0, k))
-        count = 0
-        while q:
-            for _ in range(len(q)):
-                r, c, rem_k = q.popleft()
-                if r == ROWS - 1 and c == COLS - 1: return count
-                for dr, dc in directions:
-                    nr, nc = dr + r, dc + c
-                    if nr < 0 or nc < 0 or nr >= ROWS or nc >= COLS: continue
-                    if grid[nr][nc] == 1 and not rem_k: continue
-                    new_k = rem_k - 1 if grid[nr][nc] == 1 else rem_k
-                    if (nr, nc, new_k) in visited: continue
-                    q.append((nr, nc, new_k))
-                    visited.add((nr, nc, new_k))
-            count += 1
+        min_heap = [(0, 0, 0, k)] # (count, r, c, rem_k)
+        visited = set()
+        while min_heap:
+            count, r, c, rem_k = heapq.heappop(min_heap)
+            if r == ROWS - 1 and c == COLS - 1:
+                return count
+            if (r, c, rem_k) in visited: continue
+            visited.add((r, c, rem_k))
+            for dr, dc in directions:
+                nr, nc = dr + r, dc + c
+                if nr < 0 or nc < 0 or nr >= ROWS or nc >= COLS:
+                    continue
+                if (nr, nc, rem_k) in visited:
+                    continue
+                if grid[nr][nc] == 1 and rem_k:
+                    heapq.heappush(min_heap, (count + 1, nr, nc, rem_k - 1))
+                elif grid[nr][nc] == 0:
+                    heapq.heappush(min_heap, (count + 1, nr, nc, rem_k))
         return -1
-"""
-[
-    [0,0],
-    [1,0],
-    [1,0],
-    [1,0],
-    [1,0],
-    [1,0],
-    [0,0],
-    [0,1],
-    [0,1],
-    [0,1],
-    [0,0],
-    [1,0],
-    [1,0],
-    [0,0]
-    ]
-"""
+                
