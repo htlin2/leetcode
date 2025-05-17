2ea04af6b0292599b73b699cd6b45f277a74f48d
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-05-16 20:19:09 -0700

    Time: 93 ms (80.13%) | Memory: 32.2 MB (61.00%) - LeetSync

--- 480-sliding-window-median/sliding-window-median.py
+++ 480-sliding-window-median/sliding-window-median.py
@@ -1,7 +1,5 @@
 class Solution:
     def medianSlidingWindow(self, nums: List[int], k: int) -> List[float]:
-        is_odd = k % 2 == 1
-        res = []
         left = [] # max_heap
         right = [] # min_heap
         for i in range(k):
@@ -13,25 +11,25 @@ class Solution:
                 heapq.heappush(right, -heapq.heappop(left))
             if len(right) - len(left) >= 1:
                 heapq.heappush(left, -heapq.heappop(right))
-        if is_odd:
+        if k % 2 == 1:
             median = -left[0]
         else:
             median = (-left[0] + right[0]) / 2
-        res.append(median)
+        res = [median]
         balance_dict = collections.defaultdict(int)
         for i in range(k, len(nums)):
             removed_num = nums[i - k]
             balance_dict[removed_num] += 1
             balance = -1 if removed_num <= median else 1
             if nums[i] <= median:
-                balance += 1
                 heapq.heappush(left, -nums[i])
+                balance += 1
             else:
-                balance -= 1
                 heapq.heappush(right, nums[i])
+                balance -= 1
             if balance < 0:
                 heapq.heappush(left, -heapq.heappop(right))
-            if 0 < balance:
+            elif balance > 0:
                 heapq.heappush(right, -heapq.heappop(left))
             while left and balance_dict[-left[0]]:
                 balance_dict[-left[0]] -= 1
@@ -39,9 +37,11 @@ class Solution:
             while right and balance_dict[right[0]]:
                 balance_dict[right[0]] -= 1
                 heapq.heappop(right)
-            if is_odd:
+            if k % 2 == 1:
                 median = -left[0]
             else:
                 median = (-left[0] + right[0]) / 2
             res.append(median)
         return res
+"""
+"""
