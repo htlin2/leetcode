cfb1a5786ce44c2387de89109f38aec7dc60ca2d
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-05-16 20:45:36 -0700

    Time: 164 ms (62.90%) | Memory: 31.8 MB (44.16%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.py
+++ 239-sliding-window-maximum/sliding-window-maximum.py
@@ -1,16 +1,15 @@
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
         res = []
-        max_heap = []
-        for right in range(k):
-            heapq.heappush(max_heap, (-nums[right], right))
-        res.append(-max_heap[0][0])
-
-        left = 0
-        for right in range(k, len(nums)):
-            heapq.heappush(max_heap, (-nums[right], right))
-            while max_heap and max_heap[0][-1] <= left:
-                heapq.heappop(max_heap)
+        q = collections.deque([]) # monotonic decreasing
+        left = -k + 1
+        for right in range(len(nums)):
+            while q and q[0] < left:
+                q.popleft()
+            while q and nums[q[-1]] <= nums[right]:
+                q.pop()
+            q.append(right)
+            if left >= 0:
+                res.append(nums[q[0]])
             left += 1
-            res.append(-max_heap[0][0])
         return res
