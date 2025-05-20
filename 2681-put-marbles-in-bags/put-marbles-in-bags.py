0bfdd9890103818a98c0c42915aed1b1c0fc426a
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-05-19 20:39:00 -0700

    Time: 128 ms (69.70%) | Memory: 30.1 MB (96.87%) - LeetSync

--- 2681-put-marbles-in-bags/put-marbles-in-bags.py
+++ 2681-put-marbles-in-bags/put-marbles-in-bags.py
@@ -3,14 +3,9 @@ class Solution:
         if k == 1: return 0
         nums = []
         for i in range(1, len(weights)):
-            curr_sum = weights[i] + weights[i - 1]
-            nums.append(curr_sum)
+            nums.append(weights[i] + weights[i - 1])
         nums.sort()
         i = k - 1
-        max_score = sum(nums[-i:])
-        min_score = sum(nums[:i])
-        return max_score - min_score
-"""
-Input: weights = [1,3,5,1], k = 2
-4,8,6
-"""
+        max_arr = nums[-i:]
+        min_arr = nums[:i]
+        return sum(max_arr) - sum(min_arr)
