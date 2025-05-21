262b87bb7e2fbf5e28681e3cdd135b00bec99f8f
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-05-20 22:43:17 -0700

    Time: 174 ms (36.55%) | Memory: 40.3 MB (5.40%) - LeetSync

--- 2289-minimum-operations-to-make-the-array-alternating/minimum-operations-to-make-the-array-alternating.py
+++ 2289-minimum-operations-to-make-the-array-alternating/minimum-operations-to-make-the-array-alternating.py
@@ -1,15 +1,20 @@
 class Solution:
     def minimumOperations(self, nums: List[int]) -> int:
-        N = len(nums)
-        even_counter = collections.Counter(nums[::2])
-        odd_counter = collections.Counter(nums[1::2])
-        even_tuples = sorted(even_counter.items(), key=lambda x: x[-1], reverse=True)
-        odd_tuples = sorted(odd_counter.items(), key=lambda x: x[-1], reverse=True)
-        even_tuples.append((-1, 0))
-        odd_tuples.append((-1, 0))
-        if even_tuples[0][0] != odd_tuples[0][0]:
-            return N - even_tuples[0][1] - odd_tuples[0][1]
-        else:
-            first = even_tuples[0][1] + odd_tuples[1][1]
-            second = even_tuples[1][1] + odd_tuples[0][1]
-            return N - max(first, second)
+        even_nums = []
+        odd_nums = []
+        for i in range(len(nums)):
+            if i % 2 == 1:
+                odd_nums.append(nums[i])
+            else:
+                even_nums.append(nums[i])
+        even_counter = collections.Counter(even_nums)
+        odd_counter = collections.Counter(odd_nums)
+        even_sorted = sorted(even_counter.items(), key=lambda x: x[1], reverse=True)
+        odd_sorted = sorted(odd_counter.items(), key=lambda x: x[1], reverse=True)
+        even_sorted.append((-1, 0))
+        odd_sorted.append((-1, 0))
+        if even_sorted[0][0] != odd_sorted[0][0]:
+            return len(nums) - even_sorted[0][-1] - odd_sorted[0][-1]
+        first = even_sorted[0][-1] + odd_sorted[1][-1]
+        second = even_sorted[1][-1] + odd_sorted[0][-1]
+        return len(nums) - max(first, second)
