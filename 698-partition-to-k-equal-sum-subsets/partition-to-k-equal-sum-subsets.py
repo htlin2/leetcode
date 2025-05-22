b82d8bf7724bbc4f77a3a059174bdf1633e2cf86
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-05-21 20:54:54 -0700

    Time: 3 ms (99.34%) | Memory: 17.9 MB (64.49%) - LeetSync

--- 698-partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.py
+++ 698-partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.py
@@ -1,20 +1,17 @@
 class Solution:
     def canPartitionKSubsets(self, nums: List[int], k: int) -> bool:
-        N = len(nums)
         total = sum(nums)
-        target = total // k
-        if target * k != total:
-            return False
+        target = total / k
+        if target * k != total: return False
         nums.sort(reverse=True)
         visited = set()
         def dfs(i, curr_sum, rem_k):
-            if rem_k == 0:
-                return True
+            if rem_k == 0: return True
             if curr_sum == target:
                 return dfs(0, 0, rem_k - 1)
-            for j in range(i, N):
+            for j in range(i, len(nums)):
                 if j in visited: continue
-                if curr_sum + nums[j] <= target:
+                if nums[j] + curr_sum <= target:
                     visited.add(j)
                     if dfs(j + 1, curr_sum + nums[j], rem_k):
                         return True
@@ -23,6 +20,3 @@ class Solution:
                     break
             return False
         return dfs(0, 0, k)
-"""
-backtracking
-"""
