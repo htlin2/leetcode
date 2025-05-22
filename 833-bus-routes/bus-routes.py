8c73bf5270368a67b875a091378b308daa769cf2
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-05-21 21:07:38 -0700

    Time: 79 ms (90.12%) | Memory: 55.5 MB (10.65%) - LeetSync

--- 833-bus-routes/bus-routes.py
+++ 833-bus-routes/bus-routes.py
@@ -1,17 +1,19 @@
 class Solution:
     def numBusesToDestination(self, routes: List[List[int]], source: int, target: int) -> int:
-        adj = collections.defaultdict(set) # stop: [bus1, bus2]
-        for i in range(len(routes)):
-            for stop in routes[i]:
-                adj[stop].add(i)
+        adj = collections.defaultdict(set) # stop: bus[]
+        for bus, route in enumerate(routes):
+            for stop in route:
+                adj[stop].add(bus)
+        if source == target: return 0
+        if source not in adj or target not in adj: return -1
+        res = 0
         q = collections.deque([source])
-        visited_bus = set()
         visited_stop = set([source])
-        count = 0
+        visited_bus = set()
         while q:
             for _ in range(len(q)):
                 stop = q.popleft()
-                if stop == target: return count
+                if stop == target: return res
                 for next_bus in adj[stop]:
                     if next_bus in visited_bus: continue
                     visited_bus.add(next_bus)
@@ -19,5 +21,5 @@ class Solution:
                         if next_stop in visited_stop: continue
                         visited_stop.add(next_stop)
                         q.append(next_stop)
-            count += 1
+            res += 1
         return -1
