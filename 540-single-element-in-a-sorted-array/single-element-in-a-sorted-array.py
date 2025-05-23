837f1ea2ecc80f86c7baefa6b9206f5e677c2516
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-05-22 21:36:36 -0700

    Time: 0 ms (100.00%) | Memory: 24.8 MB (82.77%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
@@ -1,14 +1,20 @@
 class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
-        N = len(nums)
-        left, right = 0, N - 1
+        left, right = 0, len(nums) - 1
         while left <= right:
             mid = (left + right) // 2
-            curr_num = nums[mid]
-            prev_num = nums[mid - 1] if mid - 1 >= 0 else nums[-1]
-            left_length = mid - 1 if prev_num == curr_num else mid
-            if left_length % 2 == 1:
-                right = mid - 1
-            else:
+            prev_num = nums[mid - 1] if mid - 1 >= 0 else float('-inf')
+            next_num = nums[mid + 1] if mid + 1 < len(nums) else float('inf')
+            if nums[mid] != prev_num and nums[mid] != next_num:
+                return nums[mid]
+            left_size = mid
+            if nums[mid] == next_num:
+                left_size += 1
+            if left_size % 2 == 1:
                 left = mid + 1
+            else:
+                right = mid - 1
         return nums[right]
+"""
+binary search
+"""
