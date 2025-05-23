82932435833996ce40229bcadf91bdc0a23bf88c
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-05-22 20:34:11 -0700

    Time: 11 ms (80.37%) | Memory: 18.2 MB (29.12%) - LeetSync

--- 2856-count-complete-subarrays-in-an-array/count-complete-subarrays-in-an-array.py
+++ 2856-count-complete-subarrays-in-an-array/count-complete-subarrays-in-an-array.py
@@ -1,18 +1,31 @@
 class Solution:
     def countCompleteSubarrays(self, nums: List[int]) -> int:
-        uniq_count = len(set(nums))
-        counter = collections.defaultdict(int)
-        left = 0
+        uniq_length = len(set(nums))
+        window = collections.defaultdict(int)
         res = 0
+        left = 0
         for right in range(len(nums)):
-            counter[nums[right]] += 1
-            while len(counter) == uniq_count:
+            window[nums[right]] += 1
+            while len(window) == uniq_length:
                 res += len(nums) - right
-                counter[nums[left]] -= 1
-                if counter[nums[left]] == 0:
-                    del counter[nums[left]]
+                window[nums[left]] -= 1
+                if window[nums[left]] == 0:
+                    del window[nums[left]]
                 left += 1
         return res
 """
 sliding window
+Input: nums = [1,3,1,2,2]
+Output: 4
+uniq_length = 3
+1,3,1,2 -> N (5) - 3 = 2
+312 -> N - 4 + 1 = 2
+
+Input: nums = [5,5,5,5]
+output: 10
+uniq_length = 1
+5 -> 4 - 1 + 1 = 4
+ 5 -> 4 - 2 + 1 = 3
+   5 -> 4 - 3 + 1 = 2
+     5 -> 4 - 4 + 1 = 1
 """
