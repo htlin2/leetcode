b734c37b8e538bbd9957c05e807d4dda2e1db7c1
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-05-23 19:00:32 -0700

    Time: 0 ms (100.00%) | Memory: 24.8 MB (46.08%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
@@ -7,14 +7,11 @@ class Solution:
             next_num = nums[mid + 1] if mid + 1 < len(nums) else float('inf')
             if nums[mid] != prev_num and nums[mid] != next_num:
                 return nums[mid]
-            left_size = mid
-            if nums[mid] == next_num:
-                left_size += 1
+            left_size = mid - 1 if prev_num == nums[mid] else mid
             if left_size % 2 == 1:
-                left = mid + 1
-            else:
+                # odd
                 right = mid - 1
+            else:
+                # even
+                left = mid + 1
         return nums[right]
-"""
-binary search
-"""
