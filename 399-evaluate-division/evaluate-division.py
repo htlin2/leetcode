e71178f524dd6bc829f99ba2dac8df20122e3661
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-05-24 23:16:17 -0700

    Time: 0 ms (100.00%) | Memory: 17.9 MB (73.06%) - LeetSync

--- 399-evaluate-division/evaluate-division.py
+++ 399-evaluate-division/evaluate-division.py
@@ -1,25 +1,27 @@
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
-        adj = collections.defaultdict(set) #
+        adj = collections.defaultdict(list) # a: (b, val)
         for i in range(len(equations)):
             a, b = equations[i]
             val = values[i]
-            adj[a].add((b, val))
-            adj[b].add((a, 1 / val))
-        res = []
-
-        def dfs(src, dst, weight, visited):
-            if src == dst: return weight
-            visited.add(src)
-            for nei_src, nei_weight in adj[src]:
-                if nei_src in visited: continue
-                res = dfs(nei_src, dst, weight * nei_weight, visited)
-                if res != -1:
-                    return res
+            adj[a].append((b, val))
+            adj[b].append((a, 1 / val))
+        def bfs(src, dst):
+            if src not in adj or dst not in adj: return -1
+            q = collections.deque()
+            q.append((src, 1))
+            visited = set([src])
+            while q:
+                first, weight = q.popleft()
+                if first == dst: return weight
+                visited.add(first)
+                for nei_src, nei_weight in adj[first]:
+                    if nei_src in visited: continue
+                    q.append((nei_src, weight * nei_weight))
             return -1
+        
+        res = []
         for q1, q2 in queries:
-            if q1 not in adj or q2 not in adj:
-                res.append(-1)
-            else:
-                res.append(dfs(q1, q2, 1, set()))
+            ans = bfs(q1, q2)
+            res.append(ans)
         return res
