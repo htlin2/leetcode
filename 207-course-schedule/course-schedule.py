ae0af874bca773bcaa3fb3aa06b89b5cdb49585d
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-05-26 16:25:17 -0700

    Time: 13 ms (8.42%) | Memory: 21.1 MB (5.40%) - LeetSync

---
+++ 207-course-schedule/course-schedule.py
@@ -0,0 +1,20 @@
+class Solution:
+    def canFinish(self, N: int, prerequisites: List[List[int]]) -> bool:
+        adj = collections.defaultdict(set)
+        for src, dst in prerequisites:
+            adj[src].add(dst)
+        visited, cycle = set(), set()
+        res = set()
+        def dfs(src):
+            if src in visited: return True
+            if src in cycle: return False
+            cycle.add(src)
+            for nei in adj[src]:
+                if not dfs(nei):
+                    return False
+            visited.add(src)
+            return True
+        for i in range(N):
+            if not dfs(i):
+                return False
+        return len(visited) == N
