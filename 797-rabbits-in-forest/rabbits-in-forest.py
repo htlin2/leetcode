88bed9f359e257df4ad36f1eb3e8b177e57531d6
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-05-26 11:32:18 -0700

    Time: 0 ms (100.00%) | Memory: 17.9 MB (51.16%) - LeetSync

--- 797-rabbits-in-forest/rabbits-in-forest.py
+++ 797-rabbits-in-forest/rabbits-in-forest.py
@@ -1,9 +1,9 @@
 class Solution:
     def numRabbits(self, answers: List[int]) -> int:
-        answers = [a + 1 for a in answers]
-        counter = collections.Counter(answers)
+        nums = [a + 1 for a in answers]
+        counter = collections.Counter(nums)
         res = 0
-        for group_size, count in counter.items():
-            number = math.ceil(count / group_size)
-            res += number * group_size
+        for group_size, group_count in counter.items():
+            num = math.ceil(group_count / group_size)
+            res += group_size * num
         return res
