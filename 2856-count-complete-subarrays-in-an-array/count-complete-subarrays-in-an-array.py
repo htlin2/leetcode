06a48ce6e06c2e35f15c3c15eed15f45051f51c9
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-05-31 13:59:03 -0700

    Time: 11 ms (80.34%) | Memory: 18 MB (47.33%) - LeetSync

--- 2856-count-complete-subarrays-in-an-array/count-complete-subarrays-in-an-array.py
+++ 2856-count-complete-subarrays-in-an-array/count-complete-subarrays-in-an-array.py
@@ -1,31 +1,15 @@
 class Solution:
     def countCompleteSubarrays(self, nums: List[int]) -> int:
-        uniq_length = len(set(nums))
-        window = collections.defaultdict(int)
+        uniq_nums = set(nums)
         res = 0
+        window = collections.defaultdict(int)
         left = 0
         for right in range(len(nums)):
             window[nums[right]] += 1
-            while len(window) == uniq_length:
+            while len(window) == len(uniq_nums):
                 res += len(nums) - right
                 window[nums[left]] -= 1
                 if window[nums[left]] == 0:
                     del window[nums[left]]
                 left += 1
         return res
-"""
-sliding window
-Input: nums = [1,3,1,2,2]
-Output: 4
-uniq_length = 3
-1,3,1,2 -> N (5) - 3 = 2
-312 -> N - 4 + 1 = 2
-
-Input: nums = [5,5,5,5]
-output: 10
-uniq_length = 1
-5 -> 4 - 1 + 1 = 4
- 5 -> 4 - 2 + 1 = 3
-   5 -> 4 - 3 + 1 = 2
-     5 -> 4 - 4 + 1 = 1
-"""
