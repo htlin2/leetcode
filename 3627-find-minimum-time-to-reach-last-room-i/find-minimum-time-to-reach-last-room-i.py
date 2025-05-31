d7f51f936e74c9cb1c9da6442732ee2af2786540
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-05-31 12:49:05 -0700

    Time: 130 ms (66.71%) | Memory: 18.4 MB (41.45%) - LeetSync

--- 3627-find-minimum-time-to-reach-last-room-i/find-minimum-time-to-reach-last-room-i.py
+++ 3627-find-minimum-time-to-reach-last-room-i/find-minimum-time-to-reach-last-room-i.py
@@ -1,22 +1,26 @@
 class Solution:
     def minTimeToReach(self, grid: List[List[int]]) -> int:
-        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
         ROWS, COLS = len(grid), len(grid[0])
         min_heap = [(0, 0, 0)] # time_, r, c
         visited = set()
         while min_heap:
             time_, r, c = heapq.heappop(min_heap)
-            if r == ROWS - 1 and c == COLS - 1:
-                return time_
-            if (r, c) in visited:
-                continue
+            if r == ROWS - 1 and c == COLS - 1: return time_
+            if (r, c) in visited: continue
             visited.add((r, c))
             for dr, dc in directions:
                 nr, nc = dr + r, dc + c
-                if nr < 0 or nc < 0 or nr >= ROWS or nc >= COLS:
-                    continue
-                if (nr, nc) in visited:
-                    continue
+                if nr < 0 or nc < 0 or nr >= ROWS or nc >= COLS: continue
+                if (nr, nc) in visited: continue
                 new_time = max(time_, grid[nr][nc]) + 1
                 heapq.heappush(min_heap, (new_time, nr, nc))
         return -1
+"""
+Input: moveTime = [
+    [0,4],
+    [4,4]
+]
+Output: 6
+
+"""
