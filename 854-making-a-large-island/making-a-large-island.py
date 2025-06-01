8b574bbce2edd4a0dfc930db36b050af6fa6062a
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-06-01 14:51:19 -0700

    Time: 797 ms (72.71%) | Memory: 28 MB (75.95%) - LeetSync

--- 854-making-a-large-island/making-a-large-island.py
+++ 854-making-a-large-island/making-a-large-island.py
@@ -1,39 +1,40 @@
 class Solution:
     def largestIsland(self, grid: List[List[int]]) -> int:
-        directions = [(1, 0), (-1, 0), (0, -1), (0, 1)]
         ROWS, COLS = len(grid), len(grid[0])
-        label_area = collections.defaultdict(int) # label: area
-        def dfs(r, c, label):
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        id_ = 2
+        memo = collections.defaultdict(int) # id_: total_area
+        def dfs(r, c, id_):
             if r < 0 or c < 0 or r >= ROWS or c >= COLS: return 0
-            if grid[r][c] == 0 or grid[r][c] == label: return 0
-            grid[r][c] = label
+            if grid[r][c] != 1: return 0
+            grid[r][c] = id_
             res = 1
-            for nr, nc in directions:
-                res += dfs(nr + r, nc + c, label)
+            for dr, dc in directions:
+                nr, nc = dr + r, dc + c
+                res += dfs(nr, nc, id_)
             return res
-        label = 2
+
         for r in range(ROWS):
             for c in range(COLS):
-                if grid[r][c] == 1:
-                    area = dfs(r, c, label)
-                    label_area[label] = area
-                    label += 1
-
-        if label_area:
-            res = max(label_area.values())
+                if grid[r][c] != 1: continue
+                area = dfs(r, c, id_)
+                memo[id_] = area
+                id_ += 1
+        if memo:
+            res = max(memo.values())
         else:
             res = 0
         for r in range(ROWS):
             for c in range(COLS):
                 if grid[r][c] != 0: continue
-                visited = set()
                 curr_area = 1
+                visited_id = set()
                 for dr, dc in directions:
                     nr, nc = dr + r, dc + c
                     if nr < 0 or nc < 0 or nr >= ROWS or nc >= COLS: continue
-                    label = grid[nr][nc]
-                    if label in visited: continue
-                    curr_area += label_area[label]
-                    visited.add(label)
-                res = max(curr_area, res)
-        return 1 if res == 0 else res
+                    n_id = grid[nr][nc]
+                    if n_id in visited_id: continue
+                    visited_id.add(n_id)
+                    curr_area += memo[n_id]
+                res = max(res, curr_area)
+        return res
