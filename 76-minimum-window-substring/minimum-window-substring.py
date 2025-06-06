fd8ea8f95c954283480123d9871b06b5194aa5dd
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-06-05 21:08:01 -0700

    Time: 143 ms (28.07%) | Memory: 18.4 MB (16.26%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -1,36 +1,24 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
+        if s == t: return s
         t_counter = collections.Counter(t)
-        window_counter = {}
-        matched_chars = 0
         left = 0
+        window_counter = collections.defaultdict(int)
+        balance = 0
         res = ''
         for right in range(len(s)):
-            # expand window logic
             if s[right] in t_counter:
-                if s[right] not in window_counter:
-                    window_counter[s[right]] = 0
                 window_counter[s[right]] += 1
                 if window_counter[s[right]] == t_counter[s[right]]:
-                    matched_chars += 1
-            # shrink window logic
-            while left <= right and matched_chars == len(t_counter):
-                if not res or len(res) >= (right - left + 1):
+                    balance += 1
+            while balance == len(t_counter):
+                if not res or len(res) >= right - left + 1:
                     res = s[left:right + 1]
                 if s[left] in window_counter:
                     if window_counter[s[left]] == t_counter[s[left]]:
-                        matched_chars -= 1
+                        balance -= 1
                     window_counter[s[left]] -= 1
                     if window_counter[s[left]] == 0:
                         del window_counter[s[left]]
                 left += 1
         return res
-"""
-hashmap + sliding window
-Input: s = "ADOBECODEBANC", t = "ABC"
-t_counter = {A: 1, B: 1, C: 1}
-res = ADOBEC
-window_counter = {A: 1, B: 1, C: 1}
- A D O B E C O D E B A N C
-                     L   R
-"""
