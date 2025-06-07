bcad53a727105dacef1f021f4150e6461429a5bf
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-06-07 13:45:06 -0700

    Time: 0 ms (100.00%) | Memory: 17.9 MB (73.48%) - LeetSync

--- 399-evaluate-division/evaluate-division.py
+++ 399-evaluate-division/evaluate-division.py
@@ -1,25 +1,27 @@
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
-        adj = collections.defaultdict(list) # a: (b, val)
+        adj = collections.defaultdict(list)
         for i in range(len(equations)):
             a, b = equations[i]
             val = values[i]
             adj[a].append((b, val))
-            adj[b].append((a, 1 / val))
+            adj[b].append((a, 1/val))
 
-        def dfs(src, dst, weight, visited):
+        def dfs(src, dst, visited, weight):
             if src not in adj or dst not in adj: return -1
             if src == dst: return weight
-            for nei_src, nei_weight in adj[src]:
-                if nei_src in visited: continue
-                visited.add(nei_src)
-                ans = dfs(nei_src, dst, weight * nei_weight, visited)
-                if ans != -1:
-                    return ans
+            if src in visited: return -1
+            visited.add(src)
+            for nei, nei_weight in adj[src]:
+                if nei in visited: continue
+                res = dfs(nei, dst, visited, weight * nei_weight)
+                if res != -1: return res
             return -1
 
         res = []
         for q1, q2 in queries:
-            visited = set()
-            res.append(dfs(q1, q2, 1, visited))
+            ans = dfs(q1, q2, set(), 1)
+            res.append(ans)
         return res
+"""
+"""
