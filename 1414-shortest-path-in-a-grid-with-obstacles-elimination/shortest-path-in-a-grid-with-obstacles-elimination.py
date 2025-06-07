4dc06e52a1a9fae416e58690e5b20f474c464a8e
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-06-07 14:42:14 -0700

    Time: 699 ms (8.29%) | Memory: 24.5 MB (54.58%) - LeetSync

--- 1414-shortest-path-in-a-grid-with-obstacles-elimination/shortest-path-in-a-grid-with-obstacles-elimination.py
+++ 1414-shortest-path-in-a-grid-with-obstacles-elimination/shortest-path-in-a-grid-with-obstacles-elimination.py
@@ -1,21 +1,21 @@
 class Solution:
     def shortestPath(self, grid: List[List[int]], k: int) -> int:
-        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
         ROWS, COLS = len(grid), len(grid[0])
-        min_heap = [(0, 0, 0, k)] # (weight, r, c, rem_k)
-        visited = set()
-        visited.add((0, 0))
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        min_heap = [(0, 0, 0, k)] # weight, r, c, rem_k
+        visited = set() # (r, c, rem_k)
         while min_heap:
             weight, r, c, rem_k = heapq.heappop(min_heap)
             if r == ROWS - 1 and c == COLS - 1:
                 return weight
+            if (r, c, rem_k) in visited: continue
             visited.add((r, c, rem_k))
             for dr, dc in directions:
                 nr, nc = dr + r, dc + c
                 if nr < 0 or nc < 0 or nr >= ROWS or nc >= COLS: continue
-                if grid[nr][nc] == 1 and rem_k == 0: continue
-                new_k = rem_k - 1 if grid[nr][nc] == 1 else rem_k
-                if (nr, nc, new_k) in visited: continue
-                visited.add((nr, nc, new_k))
-                heapq.heappush(min_heap, (weight + 1, nr, nc, new_k))
+                if (nr, nc, rem_k) in visited: continue
+                if grid[nr][nc] == 1 and rem_k:
+                    heapq.heappush(min_heap, (weight + 1, nr, nc, rem_k - 1))
+                elif grid[nr][nc] == 0:
+                    heapq.heappush(min_heap, (weight + 1, nr, nc, rem_k))
         return -1
