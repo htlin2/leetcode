5c720ed1ddffe60a9a2fcb43dfca763d9c04e5da
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-06-07 14:04:03 -0700

    Time: 0 ms (100.00%) | Memory: 17.9 MB (73.48%) - LeetSync

--- 399-evaluate-division/evaluate-division.py
+++ 399-evaluate-division/evaluate-division.py
@@ -7,20 +7,24 @@ class Solution:
             adj[a].append((b, val))
             adj[b].append((a, 1/val))
 
-        def dfs(src, dst, visited):
-            if src not in adj or dst not in adj: return -1
-            if src == dst: return 1
-            if src in visited: return -1
-            visited.add(src)
-            for nei, nei_weight in adj[src]:
-                if nei in visited: continue
-                res = dfs(nei, dst, visited)
-                if res != -1:return res * nei_weight
+        def bfs(src, dst):
+            if src not in adj or dst not in adj:
+                return -1
+            visited = set([src])
+            q = collections.deque([(src, 1)])
+            while q:
+                node, weight = q.popleft()
+                if node == dst:
+                    return weight
+                for nei, nei_weight in adj[node]:
+                    if nei not in adj: return -1
+                    if nei in visited: continue
+                    visited.add(nei)
+                    q.append((nei, weight * nei_weight))
             return -1
-
         res = []
         for q1, q2 in queries:
-            ans = dfs(q1, q2, set())
+            ans = bfs(q1, q2)
             res.append(ans)
         return res
 """
