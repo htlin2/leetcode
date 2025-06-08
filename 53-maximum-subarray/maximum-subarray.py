c40be4f0568e3b0e93b115296d4aee7156900a85
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-06-08 13:37:09 -0700

    Time: 1111 ms (5.04%) | Memory: 45.9 MB (7.09%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -1,21 +1,18 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        res = curr_sum = float('-inf')
-        for n in nums:
-            curr_sum = max(curr_sum + n, n)
-            res = max(curr_sum, res)
-        return res
-
-
-"""
-prefix sum
-sliding window x b/c it has negative
-[-2, 1,-3,4,-1, 2, 1,-5, 4
-greedy
-        -2, 1 -2,4,-3, 5, 6,
-        -2, 1,-1,4, 1, 6         max(curr_sum, curr)
-res     -2, 1, 1,4, 4, 6
-res = 0
- -2,-1,-4,0,-1,-1, 0,-5,-1 = prefix_sum
- -2,
-"""
+        def helper(left, right):
+            if left > right: return float('-inf')
+            mid = (left + right) // 2
+            curr_sum = left_sum = 0
+            for i in range(mid - 1, left - 1, -1):
+                curr_sum += nums[i]
+                left_sum = max(left_sum, curr_sum)
+            curr_sum = right_sum = 0
+            for i in range(mid + 1, right + 1):
+                curr_sum += nums[i]
+                right_sum = max(right_sum, curr_sum)
+            combined = left_sum + right_sum + nums[mid]
+            sub_left = helper(left, mid - 1)
+            sub_right = helper(mid + 1, right)
+            return max(combined, sub_left, sub_right)
+        return helper(0, len(nums) - 1)
