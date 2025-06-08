c743066077841ca23f7489da2a82f441c13a5069
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-06-08 13:15:41 -0700

    Time: 2816 ms (51.50%) | Memory: 21.7 MB (19.90%) - LeetSync

--- 3751-maximum-frequency-after-subarray-operation/maximum-frequency-after-subarray-operation.py
+++ 3751-maximum-frequency-after-subarray-operation/maximum-frequency-after-subarray-operation.py
@@ -2,7 +2,7 @@ class Solution:
     def maxFrequency(self, nums: List[int], k: int) -> int:
         counter = collections.Counter(nums)
         max_increase = 0
-        for t in range(1, 51):
+        for t in counter:
             if t == k: continue
             curr_sum = max_sum = 0
             for num in nums:
@@ -13,7 +13,7 @@ class Solution:
                 curr_sum = max(curr_sum, 0)
                 max_sum = max(max_sum, curr_sum)
             max_increase = max(max_increase, max_sum)
-        return counter.get(k, 0) + max_increase
+        return counter[k] + max_increase
 """
 Input:
 nums = [3, 2, 3, 7, 3, 2]
