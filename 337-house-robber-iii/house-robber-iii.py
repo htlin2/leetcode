b4fa29963d130873cdc04e2f40a82aab802e73b3
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-07-04 16:58:06 -0700

    Time: 11 ms (16.02%) | Memory: 22 MB (12.96%) - LeetSync

--- 337-house-robber-iii/house-robber-iii.py
+++ 337-house-robber-iii/house-robber-iii.py
@@ -7,17 +7,17 @@
 class Solution:
     def rob(self, root: Optional[TreeNode]) -> int:
         memo = collections.defaultdict(int)
-        def dfs(node, curr_sum, skip):
-            if not node: return curr_sum
+        def dfs(node, skip):
+            if not node: return 0
             if (node, skip) in memo: return memo[node, skip]
             if skip:
-                memo[node, skip] = dfs(node.left, curr_sum, False) + dfs(node.right, curr_sum, False)
+                memo[node, skip] = dfs(node.left, False) + dfs(node.right, False)
                 return memo[node, skip]
-            add_sum = dfs(node.left, curr_sum, True) + dfs(node.right, curr_sum, True) + node.val
-            skip_sum = dfs(node.left, curr_sum, False) + dfs(node.right, curr_sum, False)
+            add_sum = dfs(node.left, True) + dfs(node.right, True) + node.val
+            skip_sum = dfs(node.left, False) + dfs(node.right, False)
             memo[node, skip] = max(add_sum, skip_sum)
             return memo[node, skip]
-        return dfs(root, 0, False)
+        return dfs(root, False)
 """
 graph
 3: [4, 5]
