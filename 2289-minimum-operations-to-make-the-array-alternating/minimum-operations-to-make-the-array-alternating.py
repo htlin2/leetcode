9b99df4681df53b056967d039711fdb6c4f7d6b8
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-08-03 15:15:39 -0700

    Time: 119 ms (61.33%) | Memory: 39 MB (10.22%) - LeetSync

--- 2289-minimum-operations-to-make-the-array-alternating/minimum-operations-to-make-the-array-alternating.py
+++ 2289-minimum-operations-to-make-the-array-alternating/minimum-operations-to-make-the-array-alternating.py
@@ -1,20 +1,16 @@
 class Solution:
     def minimumOperations(self, nums: List[int]) -> int:
-        even_nums = []
-        odd_nums = []
-        for i in range(len(nums)):
-            if i % 2 == 1:
-                odd_nums.append(nums[i])
-            else:
-                even_nums.append(nums[i])
-        even_counter = collections.Counter(even_nums)
-        odd_counter = collections.Counter(odd_nums)
-        even_sorted = sorted(even_counter.items(), key=lambda x: x[1], reverse=True)
-        odd_sorted = sorted(odd_counter.items(), key=lambda x: x[1], reverse=True)
-        even_sorted.append((-1, 0))
+        odd = collections.Counter(nums[1::2])
+        even = collections.Counter(nums[::2])
+        # num: count
+        odd_sorted = sorted(odd.items(), key=lambda x: x[1], reverse=True)
+        even_sorted = sorted(even.items(), key=lambda x: x[1], reverse=True)
         odd_sorted.append((-1, 0))
-        if even_sorted[0][0] != odd_sorted[0][0]:
-            return len(nums) - even_sorted[0][-1] - odd_sorted[0][-1]
-        first = even_sorted[0][-1] + odd_sorted[1][-1]
-        second = even_sorted[1][-1] + odd_sorted[0][-1]
-        return len(nums) - max(first, second)
+        even_sorted.append((-1, 0))
+        N = len(nums)
+        if odd_sorted[0][0] != even_sorted[0][0]:
+            return N - (odd_sorted[0][-1] + even_sorted[0][-1])
+        else:
+            first = odd_sorted[0][-1] + even_sorted[1][-1]
+            second = odd_sorted[1][-1] + even_sorted[0][-1]
+            return N - max(first, second)
