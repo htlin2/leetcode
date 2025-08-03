9eb9067d16ccb239a646efe9b5ab6d0c16904523
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-08-02 20:18:50 -0700

    Time: 83 ms (88.56%) | Memory: 32.1 MB (64.56%) - LeetSync

--- 480-sliding-window-median/sliding-window-median.py
+++ 480-sliding-window-median/sliding-window-median.py
@@ -1,6 +1,6 @@
 class Solution:
     def medianSlidingWindow(self, nums: List[int], k: int) -> List[float]:
-        left = [] # max_heap val
+        left = [] # max_heap
         right = [] # min_heap
         res = []
         for i in range(k):
@@ -8,30 +8,34 @@ class Solution:
                 heapq.heappush(left, -nums[i])
             else:
                 heapq.heappush(right, nums[i])
+            # balance
             if len(left) - len(right) >= 2:
                 heapq.heappush(right, -heapq.heappop(left))
-            if len(right) - len(left) >= 1:
+            elif len(right) - len(left) >= 1:
                 heapq.heappush(left, -heapq.heappop(right))
         if k % 2 == 1:
+            # odd
             median = -left[0]
         else:
+            # even
             median = (-left[0] + right[0]) / 2
         res.append(median)
         balance_dict = collections.defaultdict(int)
         for i in range(k, len(nums)):
             prev_num = nums[i - k]
             balance_dict[prev_num] += 1
+            # find balance
             balance = -1 if prev_num <= median else 1
-            if nums[i] <= median:
-                heapq.heappush(left, -nums[i])
+            if not left or nums[i] <= median:
                 balance += 1
+                heapq.heappush(left, -nums[i])
             else:
-                heapq.heappush(right, nums[i])
                 balance -= 1
-            if balance < 0:
-                heapq.heappush(left, -heapq.heappop(right))
-            elif 0 < balance:
+                heapq.heappush(right, nums[i])
+            if balance == 2:
                 heapq.heappush(right, -heapq.heappop(left))
+            elif balance == -2:
+                heapq.heappush(left, -heapq.heappop(right))
             while left and balance_dict[-left[0]]:
                 balance_dict[-left[0]] -= 1
                 heapq.heappop(left)
