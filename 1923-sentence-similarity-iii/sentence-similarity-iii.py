965513f4c12d724a9dec5a622079410df902e498
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-08-07 20:25:22 -0700

    Time: 0 ms (100.00%) | Memory: 17.7 MB (90.91%) - LeetSync

--- 1923-sentence-similarity-iii/sentence-similarity-iii.py
+++ 1923-sentence-similarity-iii/sentence-similarity-iii.py
@@ -1,11 +1,14 @@
 class Solution:
     def areSentencesSimilar(self, s1: str, s2: str) -> bool:
-        q1 = collections.deque(s1.split(' '))
-        q2 = collections.deque(s2.split(' '))
+        s1_a, s2_a = s1.split(' '), s2.split(' ')
+        q1, q2 = collections.deque(s1_a), collections.deque(s2_a)
         while q1 and q2 and q1[0] == q2[0]:
             q1.popleft()
             q2.popleft()
         while q1 and q2 and q1[-1] == q2[-1]:
             q1.pop()
             q2.pop()
-        return not q1 or not q2
+        return True if not q1 or not q2 else False
+"""
+queue or stack?
+"""
