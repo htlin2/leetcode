e5113ff4ab3cb257f9bcb1ea102501b67c0803d5
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-08-07 20:33:16 -0700

    Time: 0 ms (100.00%) | Memory: 17.9 MB (38.96%) - LeetSync

--- 1923-sentence-similarity-iii/sentence-similarity-iii.py
+++ 1923-sentence-similarity-iii/sentence-similarity-iii.py
@@ -1,14 +1,15 @@
 class Solution:
     def areSentencesSimilar(self, s1: str, s2: str) -> bool:
-        s1_a, s2_a = s1.split(' '), s2.split(' ')
-        q1, q2 = collections.deque(s1_a), collections.deque(s2_a)
-        while q1 and q2 and q1[0] == q2[0]:
-            q1.popleft()
-            q2.popleft()
-        while q1 and q2 and q1[-1] == q2[-1]:
-            q1.pop()
-            q2.pop()
-        return True if not q1 or not q2 else False
+        s1, s2 = s1.split(' '), s2.split(' ')
+        s1_l, s1_r = 0, len(s1) - 1
+        s2_l, s2_r = 0, len(s2) - 1
+        while s1_l <= s1_r and s2_l <= s2_r and s1[s1_l] == s2[s2_l]:
+            s1_l += 1
+            s2_l += 1
+        while s1_l <= s1_r and s2_l <= s2_r and s1[s1_r] == s2[s2_r]:
+            s1_r -= 1
+            s2_r -= 1
+        return s1_r < s1_l or s2_r < s2_l
 """
 queue or stack?
 """
