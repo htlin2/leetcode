064c276883a8729b1a135701c9bbf47d2f43099d
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-09-20 15:11:19 -0700

    Time: 7 ms (64.91%) | Memory: 18.1 MB (97.23%) - LeetSync

--- 437-path-sum-iii/path-sum-iii.py
+++ 437-path-sum-iii/path-sum-iii.py
@@ -6,24 +6,27 @@
 #         self.right = right
 class Solution:
     def pathSum(self, root: Optional[TreeNode], target: int) -> int:
-        path_count = collections.defaultdict(int) # path_sum: count
-        path_count[0] = 1
+        memo = collections.defaultdict(int)
+        memo[0] = 1
         res = 0
-        def dfs(node, curr_sum):
+        def dfs(node, memo, curr_sum):
             nonlocal res
             if not node: return
             curr_sum += node.val
             delta = curr_sum - target
-            res += path_count[delta]
-            path_count[curr_sum] += 1
-            dfs(node.left, curr_sum)
-            dfs(node.right, curr_sum)
-            path_count[curr_sum] -= 1
-            return
-        dfs(root, 0)
+            res += memo[delta]
+            memo[curr_sum] += 1
+            dfs(node.left, memo, curr_sum)
+            dfs(node.right, memo, curr_sum)
+            memo[curr_sum] -= 1
+        dfs(root, memo, 0)
         return res
 """
-preorder dfs
-store all accumulated path
-subtract to get current path sum
+prefix sum
+{
+    10: 1
+    15: 1
+    17: 1
+    18: 1
+}
 """
