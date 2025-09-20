4c2ff887f27c02a8c499d2dc67be80e1623eb29c
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-09-20 14:49:03 -0700

    Time: 21 ms (24.64%) | Memory: 20 MB (16.43%) - LeetSync

--- 135-candy/candy.py
+++ 135-candy/candy.py
@@ -1,15 +1,29 @@
 class Solution:
     def candy(self, nums: List[int]) -> int:
-        N = len(nums)
-        prefix = [1] * N
-        postfix = [1] * N
-        for i in range(1, N):
+        prefix = [1]
+        for i in range(1, len(nums)):
             if nums[i] > nums[i - 1]:
-                prefix[i] += prefix[i - 1]
-        for i in range(N - 2, -1, -1):
+                prefix.append(prefix[i - 1] + 1)
+            else:
+                prefix.append(1)
+        postfix = [1] * len(nums)
+        for i in range(len(nums) - 2, -1, -1):
             if nums[i] > nums[i + 1]:
-                postfix[i] += postfix[i + 1]
-        res = 0
-        for i in range(N):
-            res += max(prefix[i], postfix[i])
-        return res
+                postfix[i] = postfix[i + 1] + 1
+        combined = []
+        for i in range(len(nums)):
+            combined.append(max(prefix[i], postfix[i]))
+        return sum(combined)
+"""
+prefix, postfix
+Input: ratings = [1,0,2]
+Output: 5
+
+[ 1, 0, 2]
+  1, 1, 2  prefix compare nums[i] and nums[i - 1]
+  2  1  1  postifx compare nums[i] and nums[i + 1]
+
+[ 1, 2, 2]
+  1  2, 1  prefix
+  1  1  1  postfix 
+"""
