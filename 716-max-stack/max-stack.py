2104d72166f2d2fc8ae4a4f16e5771efc52984bf
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-09-21 14:32:48 -0700

    Time: 256 ms (32.49%) | Memory: 72.6 MB (5.47%) - LeetSync

--- 716-max-stack/max-stack.py
+++ 716-max-stack/max-stack.py
@@ -1,51 +1,45 @@
 class MaxStack:
 
     def __init__(self):
-        self.max_heap = [] # (-val, -id)
-        self.stack = [] # (-val, -id)
-        self.id = 1
+        self.stack = [] # (num, id)
+        self.max_heap = [] # (-num, id)
         self.valid_ids = set()
+        self.id = 0
 
-    def clean_stack(self):
-        while self.stack and -self.stack[-1][-1] not in self.valid_ids:
+    def clean(self):
+        while self.stack and self.stack[-1][-1] not in self.valid_ids:
             self.stack.pop()
-    
-    def clean_heap(self):
+
         while self.max_heap and -self.max_heap[0][-1] not in self.valid_ids:
             heapq.heappop(self.max_heap)
 
     def push(self, x: int) -> None:
-        _tuple = (-x, -self.id)
+        self.clean()
+        self.stack.append([x, self.id])
+        heapq.heappush(self.max_heap, [-x, -self.id])
         self.valid_ids.add(self.id)
-        self.stack.append(_tuple)
-        heapq.heappush(self.max_heap, _tuple)
         self.id += 1
-        return
 
     def pop(self) -> int:
-        self.clean_stack()
-        x, _id = self.stack.pop()
-        _id *= -1
-        x *= -1
+        self.clean()
+        num, _id = self.stack.pop()
         self.valid_ids.remove(_id)
-        return x 
+        return num
 
     def top(self) -> int:
-        self.clean_stack()
-        return -self.stack[-1][0]
+        self.clean()
+        return self.stack[-1][0]
 
     def peekMax(self) -> int:
-        self.clean_heap()
+        self.clean()
         return -self.max_heap[0][0]
 
     def popMax(self) -> int:
-        self.clean_heap()
-        x, _id = heapq.heappop(self.max_heap)
-        x *= -1
-        _id *= -1
-        self.valid_ids.remove(_id)
-        self.clean_stack()
-        return x
+        self.clean()
+        num, _id = heapq.heappop(self.max_heap)
+        num *= -1
+        self.valid_ids.remove(-_id)
+        return num
 
 
 # Your MaxStack object will be instantiated and called as such:
