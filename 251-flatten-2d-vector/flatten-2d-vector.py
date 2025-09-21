8222bf813cf2282426a9129868f38b0aa473281c
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-09-21 16:12:50 -0700

    Time: 7 ms (68.19%) | Memory: 23.9 MB (22.46%) - LeetSync

--- 251-flatten-2d-vector/flatten-2d-vector.py
+++ 251-flatten-2d-vector/flatten-2d-vector.py
@@ -1,38 +1,25 @@
 class Vector2D:
 
     def __init__(self, vec: List[List[int]]):
-        self.vec = vec
-        self.row = 0
-        self.col = 0
-        self.advance()
-
-    def advance(self):
-        while self.row < len(self.vec) and self.col >= len(self.vec[self.row]):
-            self.row += 1
-            self.col = 0
+        def flat(arr):
+            res = []
+            if not arr: return []
+            for num in arr:
+                if isinstance(num, list):
+                    res += flat(num)
+                else:
+                    res.append(num)
+            return res
+        self.q = collections.deque(flat(vec))
 
     def next(self) -> int:
-        num = self.vec[self.row][self.col]
-        self.col += 1
-        self.advance()
-        return num
+        return self.q.popleft()
 
     def hasNext(self) -> bool:
-        return self.row < len(self.vec)
+        return len(self.q) != 0
 
 
 # Your Vector2D object will be instantiated and called as such:
 # obj = Vector2D(vec)
 # param_1 = obj.next()
 # param_2 = obj.hasNext()
-
-"""
-[
-    [
-        [
-            [1, 2], [3], [4]]
-        ], 
-        [], [], [], [], [], [], []
-    ]
-
-"""
