0343ea3c86b48df7c818753cbe49d536afe3a257
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-11-02 17:24:18 -0800

    Time: 0 ms (100.00%) | Memory: 17.7 MB (95.07%) - LeetSync

--- 289-game-of-life/game-of-life.py
+++ 289-game-of-life/game-of-life.py
@@ -1,50 +1,26 @@
 class Solution:
     def gameOfLife(self, grid: List[List[int]]) -> None:
         """
-        Do not return anything, modify grid in-place instead.
+        Do not return anything, modify board in-place instead.
         """
+        directions = [(0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (-1, -1), (1, -1), (-1, 1)]
+        ROWS, COLS = len(grid), len(grid[0])
         copy_grid = [row.copy() for row in grid]
-        ROW, COL = len(grid), len(grid[0])
-        directions = [(1, 0), (-1, 0), (0, 1), (0, -1), (1, 1), (-1, -1), (1, -1), (-1, 1)]
-        for r in range(ROW):
-            for c in range(COL):
-                one = 0
-                is_live = copy_grid[r][c] == 1
+        for r in range(ROWS):
+            for c in range(COLS):
+                counts = 0
                 for dr, dc in directions:
-                    rr, cc = r + dr, c + dc
-                    if rr < 0 or cc < 0 or rr >= ROW or cc >= COL:
+                    rr, cc = dr + r, dc + c
+                    if rr < 0 or cc < 0 or rr >= ROWS or cc >= COLS:
                         continue
-                    if copy_grid[rr][cc] == 1:
-                        one += 1
-                if is_live and (one < 2 or one > 3):
+                    counts += copy_grid[rr][cc]
+                is_live = copy_grid[r][c]
+                if is_live and counts < 2:
                     grid[r][c] = 0
-                elif is_live and (one == 2 or one == 3):
+                elif is_live and counts in [2, 3]:
                     grid[r][c] = 1
-                elif is_live and one > 3:
+                elif is_live and counts > 3:
                     grid[r][c] = 0
-                elif not is_live and one == 3:
+                elif not is_live and counts == 3:
                     grid[r][c] = 1
-        return grid
-"""
-Input: grid = [
-    [0,1,0],
-    [0,0,1],
-    [1,1,1],
-    [0,0,0]
-]
-
-mid: [
-    [0,0,0],
-    [?,0,+],
-    [0,+,+],
-    [0,?,0]
-]
-
-Output: [
-    [0,0,0],
-    [1,0,1],
-    [0,1,1],
-    [0,1,0]
-]
-
-"""
+        return
