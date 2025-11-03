844379980de31a26c801f4ee5af5a07840d87191
Tim Lin <28332496+htlin2@users.noreply.github.com>
2025-11-02 17:43:43 -0800

    Time: 0 ms (100.00%) | Memory: 17.8 MB (48.81%) - LeetSync

--- 289-game-of-life/game-of-life.py
+++ 289-game-of-life/game-of-life.py
@@ -2,25 +2,47 @@ class Solution:
     def gameOfLife(self, grid: List[List[int]]) -> None:
         """
         Do not return anything, modify board in-place instead.
+        {
+            a: 0
+            b: 1
+            c: 0
+            d: 1
+        }
+        {
+            b,a
+            0,0: a
+            0,1: b
+            1,0: c
+            1,1: d
+        }
         """
+        hashmap = {'a': 0, 'b': 1, 'c': 0, 'd': 1}
         directions = [(0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (-1, -1), (1, -1), (-1, 1)]
         ROWS, COLS = len(grid), len(grid[0])
-        copy_grid = [row.copy() for row in grid]
         for r in range(ROWS):
             for c in range(COLS):
                 counts = 0
+                is_live = grid[r][c]
                 for dr, dc in directions:
                     rr, cc = dr + r, dc + c
                     if rr < 0 or cc < 0 or rr >= ROWS or cc >= COLS:
                         continue
-                    counts += copy_grid[rr][cc]
-                is_live = copy_grid[r][c]
+                    key = grid[rr][cc]
+                    if key in hashmap and key in ['c', 'd']:
+                        counts += 1
+                    elif key not in hashmap:
+                        counts += grid[rr][cc]
                 if is_live and counts < 2:
-                    grid[r][c] = 0
+                    grid[r][c] = 'c'
                 elif is_live and counts in [2, 3]:
-                    grid[r][c] = 1
+                    grid[r][c] = 'd'
                 elif is_live and counts > 3:
-                    grid[r][c] = 0
+                    grid[r][c] = 'c'
                 elif not is_live and counts == 3:
-                    grid[r][c] = 1
+                    grid[r][c] = 'b'
+                else:
+                    grid[r][c] = 'a'
+        for r in range(ROWS):
+            for c in range(COLS):
+                grid[r][c] = hashmap[grid[r][c]]
         return
