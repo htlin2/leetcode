4014f434c0045f844550c284a10c05732e37cfe0
Tim Lin <28332496+htlin2@users.noreply.github.com>
2026-07-11 15:54:05 -0700

    Time: 3 ms (53.72%) | Memory: 20.6 MB (7.14%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,33 +1,10 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hashmap = {}
-        for i, n in enumerate(nums):
-            delta = target - n
-            if delta in hashmap:
-                return [hashmap[delta], i]
-            hashmap[n] = i
+        h = collections.defaultdict(int)
+        for i, num in enumerate(nums):
+            h[num] = i
 
-"""
-1. brute force
-[11,15,2,7]
-       i j     
-11 + 15 = 26
-11 + 2 = 13
-11 + 7 = 18
-2 + 7 = 9
-Time: O(n^2)
-Space: O(1)
-
-2. hashmap
-Input: nums = [11,15,2,7], target = 9
-Output: [2,3]
-hashmap = {  number: index
-    11: 0,
-    15: 1,
-    2: 2
-
-}
-return [2,3]
-time: O(n)
-space: O(n)
-"""
+        for i, num in enumerate(nums):
+            delta = target - num
+            if delta in h and h[delta] != i:
+                return [i, h[delta]]
