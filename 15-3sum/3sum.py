fc428e798e21a87cd7d31b4301760486d4109bc3
Tim Lin <28332496+htlin2@users.noreply.github.com>
2026-07-14 18:30:32 -0700

    Time: 2797 ms (5.00%) | Memory: 22 MB (93.95%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -2,25 +2,27 @@ class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         N = len(nums)
         nums.sort()
-        res = []
-        def two_sum(n_i, left, right):
+        res = set()
+        def two_sum(left, right, i):
             while left < right:
-                three_sum = n_i + nums[left] + nums[right]
-                if three_sum == 0:
-                    res.append((n_i, nums[left], nums[right]))
+                triplet = (nums[left], nums[right], nums[i])
+                total = nums[left] + nums[right] + nums[i]
+                if total == 0 and triplet not in res:
+                    res.add(triplet)
                     left += 1
-                    right -= 1
-                    while left < right and nums[left] == nums[left - 1]:
-                        left += 1
-                elif three_sum < 0:
+                elif total < 0:
                     left += 1
                 else:
                     right -= 1
-            return 
+        for i in range(N - 2):
+            two_sum(i + 1, N - 1, i)
+        return list(res)
+"""
+1.brute force
+3 for loops
+T:O(n^3)
+S:O(1)
 
-        for i in range(0, N - 2):
-            if i > 0 and nums[i] == nums[i - 1]:
-                continue
-            n_i = nums[i]
-            two_sum(n_i, i + 1, N - 1)
-        return res
+2.
+
+"""
