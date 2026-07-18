1abcc541f02385d64d1e914e08d345ab64d1a61e
Tim Lin <28332496+htlin2@users.noreply.github.com>
2026-07-17 22:14:55 -0700

    Time: 149 ms (97.24%) | Memory: 20.6 MB (80.22%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.py
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -3,22 +3,17 @@ class Solution:
         left, right = 1, max(piles)
         while left <= right:
             mid = (left + right) // 2
-            total = sum([math.ceil(p / mid) for p in piles])
-            if total <= h:
-                # eat too fast, dec mid
+            speed = sum([math.ceil(p / mid) for p in piles])
+            if speed > h:
+                # eating too slow, speed up, increase mid
+                left = mid + 1
+            elif speed < h:
+                # eating too fast, slow down, decrease mid
                 right = mid - 1
             else:
-                # eat too slow, inc mid
-                left = mid + 1
+                # same as h, slow down
+                right = mid - 1
         return left
 """
 binary search weight left
-find mid (speed per hour)
-calculate total hours needed
-if total == h:
-    eat too fast, decrease mid
-elif total > h:
-    eat too slow, increase mid
-elif total < h:
-    eat too fast, decrease mid
 """
