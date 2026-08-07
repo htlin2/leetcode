3bd13f33d7208b894d65a78efd0b27d6f60fb43c
Tim Lin <28332496+htlin2@users.noreply.github.com>
2026-08-07 16:17:40 -0700

    Time: 39 ms (16.09%) | Memory: 27.7 MB (13.67%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,33 +1,25 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         N = len(nums)
-        postfix = []
-        curr_postfix = 1
-        for i in range(N - 1, -1, -1):
-            curr_postfix *= nums[i]
-            postfix.append(curr_postfix)
-        postfix.reverse()
-        prefix = 1
+        prefix = []
+        curr = 1
         for i in range(N):
-            postfix[i] = postfix[i + 1] if i + 1 < N else 1
-            postfix[i] *= prefix
-            prefix *= nums[i]
-        return postfix
+            curr *= nums[i]
+            prefix.append(curr)
 
-"""
-Input: nums = [1,2,3,4]
-Output: [24,12,8,6]
+        postfix = []
+        curr = 1
+        for i in range(N - 1, -1, -1):
+            curr *= nums[i]
+            postfix.append(curr)
+        postfix.reverse()
 
-prefix = [ 1, 2, 6,24]
-postfix= [24,24,12, 4]
-res    = [24,12, 8, 6] => prefix[i - 1] * postfix[i + 1]
-Time: O(n)
-Space: O(n)
-
-
-Optimized
-postfix= [24,12, 8, 4]
-prefix  =           6
-Time: O(n)
-Space: O(1)
-"""
+        res = []
+        for i in range(N):
+            curr = 1
+            if i + 1 < N:
+                curr *= postfix[i + 1]
+            if i - 1 >= 0:
+                curr *= prefix[i - 1]
+            res.append(curr)
+        return res
