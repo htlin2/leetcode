bbef4d1fbf07c8bd4471351d7e08d07f6dc277a0
Tim Lin <28332496+htlin2@users.noreply.github.com>
2026-08-06 17:37:50 -0700

    Time: 209 ms (12.76%) | Memory: 20.7 MB (42.24%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.py
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -1,15 +1,23 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        left, right = 1, max(piles)
-        while left < right:
-            mid = (left + right) // 2 # speed
-            total_time = sum([math.ceil(p / mid) for p in piles])
-            if total_time == h:
-                right = mid
-            elif total_time < h:
-                # eating too fast, slow down, mid-
-                right = mid
-            else:
-                # eating too slow, speed up, mid+
-                left = mid + 1
+        left, right = 1, sum(piles)
+        while left <= right:
+            speed = (left + right) // 2
+            total_hours = sum([math.ceil(p / speed) for p in piles])
+            if total_hours == h:
+                # decrease speed
+                right = speed - 1
+            elif total_hours > h:
+                # increase speed
+                left = speed + 1
+            elif total_hours < h:
+                # decrease speed
+                right = speed - 1
         return left
+
+"""
+binary search shift left
+speed 1, 2, 3, 4, 5, 6, 7, 8, 9
+hour 10, 9, 8, 8, 8, 7, 7, 7, 7
+h =         8
+"""
